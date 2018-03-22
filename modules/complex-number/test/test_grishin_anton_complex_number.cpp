c077056c8f6c50298c0e966185f2dda1e5de40a5
TheNessi <Macuta46@ya.ru>
2018-03-22 15:36:34 +0300

    add first test

--- modules/complex-number/test/test_grishin_anton_complex_number.cpp
+++ modules/complex-number/test/test_grishin_anton_complex_number.cpp
@@ -4,16 +4,16 @@
 
 #include "include/complex_number.h"
 
-TEST(Grishin_Anton_ComplexNumberTest, Can_Negative_Number) {
+TEST(Grishin_Anton_ComplexNumberTest, Can_get_Re_from_Im) {
 	// Arrange
-	double re = 5.0;
-	double im = 5.0;
+	double re = -1.0;
+	double im = 1;
 
 	// Act
-	ComplexNumber z(re, im);
-	
+	ComplexNumber z1(0.0, im);
+
+	ComplexNumber z = z1 * z1;
 
 	// Assert
 	EXPECT_EQ(re, z.getRe());
-	EXPECT_EQ(im, z.getIm());
 }
