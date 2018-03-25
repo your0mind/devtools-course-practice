d624dae7fd67ab7b64e716bd3236e80f48eb53e6
Alexey Khomenko <hom.ksei@gmail.com>
2018-03-25 13:21:49 +0300

    Added first test for lab1

---
+++ modules/complex-number/test/test_khomenko_aleksei_complex_number.cpp
@@ -0,0 +1,35 @@
+// Copyright 2018 Khomenko Aleksei
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+TEST(Khomenko_Aleksei_ComplexNumberTest, ReEqlRe) {
+    // Arrange
+    double re = -1.0;
+    double im = 1.0;
+    
+    // Act
+    ComplexNumber z(0.0, im);
+    ComplexNumber z1 = z*z;
+    
+    // Assert
+    EXPECT_EQ(re, z1.getRe());
+ 
+}
+
+/*TEST(Khomenko_Aleksei_ComplexNumberTest, Not_Equal) {
+    // Arrange
+    double re = 20.0;
+    double im = 25.0;
+    
+    // Act
+    ComplexNumber z(re, im);
+    ComplexNumber z1(re-5,im-7);
+    
+    // Assert
+    
+    ASSERT_NE(z1, z);
+}*/
+
+
