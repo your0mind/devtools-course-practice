de9852747e0c69cc11ee2da032d14130e69bb35d
Alexey Khomenko <hom.ksei@gmail.com>
2018-03-25 20:32:12 +0300

    Removing Some Spaces

--- modules/complex-number/test/test_khomenko_aleksei_complex_number.cpp
+++ modules/complex-number/test/test_khomenko_aleksei_complex_number.cpp
@@ -4,30 +4,28 @@
 
 #include "include/complex_number.h"
 
-
-TEST(Khomenko_Aleksei_ComplexNumberTest, Re_Eql_Re) {
+TEST(Khomenko_Aleksei_ComplexNumberTest, Re_Eql_Re){
     // Arrange
     double re = -1.0;
     double im = 1.0;
-    
+
     // Act
     ComplexNumber z(0.0, im);
     ComplexNumber z1 = z*z;
-    
+
     // Assert
     EXPECT_EQ(re, z1.getRe());
- 
 }
 
 TEST(Khomenko_Aleksei_ComplexNumberTest, RNot_Equal) {
     // Arrange
     double re = 20.0;
     double im = 25.0;
-    
+
     // Act
     ComplexNumber z(re, im);
     ComplexNumber z1(re-5,im-5);
-    
+
     // Assert
     ASSERT_NE(z1.getRe(), z.getRe());
     ASSERT_NE(z1.getIm(), z.getIm());
@@ -37,13 +35,12 @@ TEST(Khomenko_Aleksei_ComplexNumberTest, Add_Zero) {
     // Arrange
     double re = 20.0;
     double im = 25.0;
-    
-    
+
     // Act
     ComplexNumber z(re, im);
     ComplexNumber z1(0.0, 0.0);
     ComplexNumber z_res = z + z1;
-    
+
     // Assert
     ASSERT_EQ(z_res.getRe(), z.getRe());
     ASSERT_EQ(z_res.getIm(), z.getIm());
@@ -53,12 +50,12 @@ TEST(Khomenko_Aleksei_ComplexNumberTest, Multiplication_by_Neutral_Element) {
     // Arrange
     double re = 20.0;
     double im = 25.0;
-    
+
     // Act
     ComplexNumber z(re, im);
     ComplexNumber z1(1.0, 0.0);
     ComplexNumber z_res = z * z1;
-    
+
     // Assert
     ASSERT_EQ(z_res.getRe(), z.getRe());
     ASSERT_EQ(z_res.getIm(), z.getIm());
@@ -68,19 +65,13 @@ TEST(Khomenko_Aleksei_ComplexNumberTest, Distribution) {
     // Arrange
     double re = 20.0;
     double im = 20.0;
-    
+
     // Act
     ComplexNumber z1(re, im), z2(re, im), z3(re, im);
-    ComplexNumber z_res1(0.0, 0.0);
-    ComplexNumber z_res2(0.0, 0.0);
-    z_res1 = z1*(z2+z3);
-    z_res2 = z1*z2+z1*z3;
-    
+    ComplexNumber z_res1 = z1*(z2+z3);
+    ComplexNumber z_res2 = z1*z2+z1*z3;
+
     // Assert
     ASSERT_EQ(z_res1.getRe(), z_res2.getRe());
     ASSERT_EQ(z_res1.getRe(), z_res2.getRe());
 }
-
-
-
-
