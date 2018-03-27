4515bf1deda905ebf69b24728d759c056dad08ae
TheNessi <Macuta46@ya.ru>
2018-03-27 12:06:27 +0300

    add third test

--- modules/complex-number/test/test_grishin_anton_complex_number.cpp
+++ modules/complex-number/test/test_grishin_anton_complex_number.cpp
@@ -32,5 +32,17 @@ TEST(Grishin_Anton_ComplexNumberTest, Mult_commutative) {
 	EXPECT_EQ(z3, z4);
 }
 
+TEST(Grishin_Anton_ComplexNumberTest, Mult_distributive) {
+	// Arrange
+	ComplexNumber z1(15.0, 20.0);
+	ComplexNumber z2(27.0, 30.0);
+	ComplexNumber z3(5.0, 7.0);
 
+	// Act
+	ComplexNumber z4 = z1*(z2 + z3);
+	ComplexNumber z5 = z1*z2 + z1*z3;
+
+	// Assert
+	EXPECT_EQ(z4, z5);
+}
 
