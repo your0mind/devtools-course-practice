84ff1dc67148edd92b7206887a2ed917dd3fc3c3
Alexey Gladyshev <wotpricol@mail.ru>
2018-03-27 12:32:03 +0300

    Add second test (distributivity) for complex_number

--- modules/complex-number/test/test_GLADYSHEV_ALEXEY_complex_number.cpp
+++ modules/complex-number/test/test_GLADYSHEV_ALEXEY_complex_number.cpp
@@ -4,6 +4,8 @@
 
 #include "include/complex_number.h"
 
+double epsilon = 0.00001;
+
 TEST(Gladyshev_Alexey_ComplexNumberTest, Correct_Associativity) {
 	// Arrange
 	ComplexNumber z1(0.2, -0.3), z2(12, 17), z3(78.897, -47);
@@ -14,4 +16,16 @@ TEST(Gladyshev_Alexey_ComplexNumberTest, Correct_Associativity) {
 
 	// Assert
 	EXPECT_TRUE(firstZ == secondZ);
+}
+TEST(Gladyshev_Alexey_ComplexNumberTest, Correct_Work_Of_Distributivity) {
+	// Arrange
+	ComplexNumber z1(0.2, -0.3), z2(12, 17), z3(78.897, -47);
+
+	// Act
+	ComplexNumber firstZ = z1 * (z2 + z3);
+	ComplexNumber secondZ = z1 * z2 + z1 * z3;
+
+	// Assert
+	EXPECT_NEAR(firstZ.getRe(), secondZ.getRe(), epsilon);
+	EXPECT_NEAR(firstZ.getIm(), secondZ.getIm(), epsilon);
 }
