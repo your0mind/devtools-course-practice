e3c968dd170704aa44d0d922b27dd611c18255da
TheNessi <Macuta46@ya.ru>
2018-03-27 12:35:43 +0300

    update tests

--- modules/complex-number/test/test_grishin_anton_complex_number.cpp
+++ modules/complex-number/test/test_grishin_anton_complex_number.cpp
@@ -6,56 +6,56 @@
 
 double epsilon = 0.001;
 TEST(Grishin_Anton_ComplexNumberTest, Can_get_Re_from_Im) {
- // Arrange
- double re = -1.0;
- double im = 1;
+    // Arrange
+    double re = -1.0;
+    double im = 1;
 
- // Act
- ComplexNumber z1(0.0, im);
+    // Act
+    ComplexNumber z1(0.0, im);
 
- ComplexNumber z = z1 * z1;
+    ComplexNumber z = z1 * z1;
 
- // Assert
- EXPECT_NEAR(re, z.getRe(), epsilon);
+    // Assert
+    EXPECT_NEAR(re, z.getRe(), epsilon);
 }
 
 TEST(Grishin_Anton_ComplexNumberTest, Mult_commutative) {
- // Arrange
- ComplexNumber z1(7.0, 6.0);
- ComplexNumber z2(5.0, 4.0);
+    // Arrange
+    ComplexNumber z1(7.0, 6.0);
+    ComplexNumber z2(5.0, 4.0);
 
 // Act
- ComplexNumber z3 = z2 * z1;
- ComplexNumber z4 = z1 * z2;
+    ComplexNumber z3 = z2 * z1;
+    ComplexNumber z4 = z1 * z2;
 
- // Assert
- EXPECT_EQ(z3, z4);
+    // Assert
+    EXPECT_EQ(z3, z4);
 }
 
 TEST(Grishin_Anton_ComplexNumberTest, Mult_distributive) {
- // Arrange
- ComplexNumber z1(15.0, 20.0);
- ComplexNumber z2(27.0, 30.0);
- ComplexNumber z3(5.0, 7.0);
+    // Arrange
+    ComplexNumber z1(15.0, 20.0);
+    ComplexNumber z2(27.0, 30.0);
+    ComplexNumber z3(5.0, 7.0);
 
- // Act
- ComplexNumber z4 = z1*(z2+z3);
- ComplexNumber z5 = z1*z2+z1*z3;
+    // Act
+    ComplexNumber z4 = z1*(z2+z3);
+    ComplexNumber z5 = z1*z2+z1*z3;
 
- // Assert
- EXPECT_EQ(z4, z5);
+    // Assert
+    EXPECT_EQ(z4, z5);
 }
 
 TEST(Grishin_Anton_ComplexNumberTest, Can_Diff_By_Add) {
- // Arrange
- ComplexNumber z1(1.0, 2.0);
- ComplexNumber z2(3.0, 6.0);
- ComplexNumber z3(0.0, 0.0);
- // Act
- ComplexNumber z4 = z1 - z2;
- z2 = z3 - z2;
- ComplexNumber z5 = z1 + z2;
+    // Arrange
+    ComplexNumber z1(1.0, 2.0);
+    ComplexNumber z2(3.0, 6.0);
+    ComplexNumber z3(0.0, 0.0);
+    // Act
+    ComplexNumber z4 = z1 - z2;
+    z2 = z3 - z2;
+    ComplexNumber z5 = z1 + z2;
 
- // Assert
- EXPECT_EQ(z4, z5);
+    // Assert
+    EXPECT_EQ(z4, z5);
 }
