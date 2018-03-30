8ce83fc1cc63ed3b65fffec18d6627a6c85c4f62
KST <tarakan.php@yandex.ru>
2018-03-30 11:53:36 +0300

    fix tab

--- modules/complex-number/test/test_tarakanov_kirill_complex_number.cpp
+++ modules/complex-number/test/test_tarakanov_kirill_complex_number.cpp
@@ -3,8 +3,7 @@
 #include <gtest/gtest.h>
 #include "include/complex_number.h"
 
-TEST(Tarakanov_Kirill_ComplexNumberTest, Can_SetRe_and_SetIm) {
-    
+TEST(Tarakanov_Kirill_ComplexNumberTest, Can_SetRe_and_SetIm) {   
   // Arrange
   ComplexNumber a(0.0, 0.0);
 
@@ -21,70 +20,68 @@ TEST(Tarakanov_Kirill_ComplexNumberTest, Can_SetRe_and_SetIm) {
 }
 
 TEST(Tarakanov_Kirill_ComplexNumberTest, Plus_Two_Complex_Numbers) {
-    // Arrange
-    ComplexNumber a(5.0, 3.0);
-    ComplexNumber b(2.0, 1.0);
+  // Arrange
+  ComplexNumber a(5.0, 3.0);
+  ComplexNumber b(2.0, 1.0);
 	
-    // Act
-    ComplexNumber res = a + b;
+  // Act
+  ComplexNumber res = a + b;
 	
-    // Assert
-    ComplexNumber true_res(7.0, 4.0);
-    EXPECT_EQ(true_res, res);	
+  // Assert
+  ComplexNumber true_res(7.0, 4.0);
+  EXPECT_EQ(true_res, res);	
 }
 
 TEST(Tarakanov_Kirill_ComplexNumberTest, Minus_Two_Complex_Numbers) {
-    // Arrange
-    ComplexNumber a(5.0, 3.0);
-    ComplexNumber b(2.0, 1.0);
+  // Arrange
+  ComplexNumber a(5.0, 3.0);
+  ComplexNumber b(2.0, 1.0);
 
-    // Act
-    ComplexNumber res = a - b;
+  // Act
+  ComplexNumber res = a - b;
 
-    // Assert
-    ComplexNumber true_res(3.0, 2.0);
-    EXPECT_EQ(true_res, res);
+  // Assert
+  ComplexNumber true_res(3.0, 2.0);
+  EXPECT_EQ(true_res, res);
 }
 
-TEST(Tarakanov_Kirill_ComplexNumberTest, Multi_Two_Complex_Numbers){
-
-    // Arrange
-    ComplexNumber a(3.0, 4.0);
-    ComplexNumber b(6.0, 5.0);
+TEST(Tarakanov_Kirill_ComplexNumberTest, Multi_Two_Complex_Numbers) {
+  // Arrange
+  ComplexNumber a(3.0, 4.0);
+  ComplexNumber b(6.0, 5.0);
 	
-    // Act
-    ComplexNumber res = a * b;
+  // Act
+  ComplexNumber res = a * b;
 	
-    // Assert
-    ComplexNumber true_res(-2, 39.0);
-    EXPECT_EQ(true_res, res);	
+  // Assert
+  ComplexNumber true_res(-2, 39.0);
+  EXPECT_EQ(true_res, res);	
 }
 
 TEST(Tarakanov_Kirill_ComplexNumberTest, Property_OF_Associativity) {
-    // Arrange
-    ComplexNumber a(1.0, 8.0);
-    ComplexNumber b(5.0, 1.0);
-    ComplexNumber c(10.0, -4.0);
+  // Arrange
+  ComplexNumber a(1.0, 8.0);
+  ComplexNumber b(5.0, 1.0);
+  ComplexNumber c(10.0, -4.0);
 
-    // Act
-    ComplexNumber res1 = a + (b + c);
-    ComplexNumber res2 = (a + b) + c;
+  // Act
+  ComplexNumber res1 = a + (b + c);
+  ComplexNumber res2 = (a + b) + c;
 
-    // Assert
-    EXPECT_EQ(res1, res2);
+  // Assert
+  EXPECT_EQ(res1, res2);
 }
 
-TEST(Tarakanov_Kirill_ComplexNumberTest, Property_OF_Distributivity)
-{
-    // Arrange
-    ComplexNumber a(1.0, 8.0);
-    ComplexNumber b(5.0, 1.0);
-    ComplexNumber c(10.0, -4.0);
+TEST(Tarakanov_Kirill_ComplexNumberTest, Property_OF_Distributivity) {
+  // Arrange
+  ComplexNumber a(1.0, 8.0);
+  ComplexNumber b(5.0, 1.0);
+  ComplexNumber c(10.0, -4.0);
 
-    // Act
-    ComplexNumber res1 = a * (b + c);
-    ComplexNumber res2 = a * b + a * c;
+  // Act
+  ComplexNumber res1 = a * (b + c);
+  ComplexNumber res2 = a * b + a * c;
 
-    // Assert
-    EXPECT_EQ(res1, res2);
+  // Assert
+  EXPECT_EQ(res1, res2);
 }
