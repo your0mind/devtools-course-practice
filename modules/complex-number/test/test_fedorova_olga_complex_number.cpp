821b913cbdf71c827d1b9fc5c39666b4d6100baa
FedorovaOlga <36547568+FedorovaOlga@users.noreply.github.com>
2018-03-30 12:44:01 +0300

    updated tests
    my 6 tests were wtitten

--- modules/complex-number/test/test_fedorova_olga_complex_number.cpp
+++ modules/complex-number/test/test_fedorova_olga_complex_number.cpp
@@ -4,15 +4,84 @@
 
 #include "include/complex_number.h"
 
-TEST(Korniakov_Kirill_ComplexNumberTest, Can_Create_Zero) {
-	// Arrange
-	double re = 0.0;
-	double im = 0.0;
+TEST(Fedorova_Olga_ComplexNumberTest, Correct_Multilication)
+{
+    double r = 2.0;
+    double i = 1.0;
 
-	// Act
-	ComplexNumber z(re, im);
+    ComplexNumber c(r, i);
+    ComplexNumber c_mult = c * c;
+    ComplexNumber true_res(3.0, 4.0);
 
-	// Assert
-	EXPECT_EQ(re, z.getRe());
-	EXPECT_EQ(im, z.getIm());
+    EXPECT_EQ(true_res, c_mult);
+}
+
+TEST(Fedorova_Olga_ComplexNumberTest, Correct_Setting)
+{
+    double r = 1.01;
+    double i = 2.02;
+
+    ComplexNumber a(r, i);
+    ComplexNumber d(0.0, 0.0);
+
+    d.setRe(r);
+    d.setIm(i);
+
+    EXPECT_EQ(a, d);
+}
+
+TEST(Fedorova_Olga_ComplexNumberTest, Can_Create_With_NegativeNumbers_And_MultiplyThem)
+{
+    double r = -1.01;
+    double i = -2.02;
+
+    ComplexNumber z(r, i);
+    ComplexNumber z_mult = z*z;
+    ComplexNumber true_mult(-3.0603, 4.0804);
+
+    EXPECT_EQ(r, z.getRe());
+    EXPECT_EQ(i, z.getIm());
+    EXPECT_EQ(true_mult, z_mult);
+}
+
+TEST(Fedorova_Olga_ComplexNumberTest, Can_Create_Copy)
+{
+    double r = 1.01;
+    double i = 2.02;
+
+    ComplexNumber z(r, i);
+    ComplexNumber z_copy(z);
+
+    EXPECT_EQ(z, z_copy);
+}
+
+TEST(Fedorova_Olga_ComplexNumberTest, Cant_Devide_by_Zero)
+{
+    double r = 1.01;
+    double i = 2.02;
+
+    ComplexNumber z(r, i);
+    ComplexNumber zero(0, 0);
+    ComplexNumber res;
+
+    EXPECT_ANY_THROW(res = z / zero);
+}
+
+TEST(Fedorova_Olga_ComplexNumberTest, Correct_NotEqual)
+{
+    double r = 1.01;
+    double i = 2.02;
+
+    ComplexNumber a(r, i);
+    ComplexNumber b(3, 4);
+    ComplexNumber res(-1, -1);
+
+    if (a != b)
+    {
+        res.setRe(1);
+        res.setIm(1);
+    }
+	
+    EXPECT_EQ(1, res.getRe());	
+    EXPECT_EQ(1, res.getIm());
 }
