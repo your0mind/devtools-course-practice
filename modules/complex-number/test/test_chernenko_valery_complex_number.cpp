9d2fea155b62b8403453a8de4174309a6d2ab1cb
unknown <v02055020v@gmail.com>
2018-04-02 17:32:36 +0300

    Updated tests

--- modules/complex-number/test/test_chernenko_valery_complex_number.cpp
+++ modules/complex-number/test/test_chernenko_valery_complex_number.cpp
@@ -4,7 +4,8 @@
 
 #include "include/complex_number.h"
 
-TEST(Chernenko_Valery_ComplexNumberTest, Can_Create_Negative_Number) {
+TEST(Chernenko_Valery_ComplexNumberTest, 
+Can_Create_Negative_Number) {
     double re = -5.7;
     double im = -9.5;
 
@@ -14,7 +15,8 @@ TEST(Chernenko_Valery_ComplexNumberTest, Can_Create_Negative_Number) {
     EXPECT_EQ(im, z.getIm());
 }
 
-TEST(Chernenko_Valery_ComplexNumberTest, Order_Of_Actions_addition_and_subtraction_Is_Not_Important) {
+TEST(Chernenko_Valery_ComplexNumberTest, 
+Order_Of_Actions_addition_and_subtraction_Is_Not_Important) {
     double re1 = 5.2;
     double re2 = 7.8;
     double re3 = 3.65;
@@ -35,7 +37,8 @@ TEST(Chernenko_Valery_ComplexNumberTest, Order_Of_Actions_addition_and_subtracti
 	EXPECT_EQ(0, 0);
 }
 
-TEST(Chernenko_Valery_ComplexNumberTest, Order_Of_Actions_multiplication_and_division_Is_Not_Important) {
+TEST(Chernenko_Valery_ComplexNumberTest, 
+Order_Of_Actions_multiplication_and_division_Is_Not_Important) {
     double re1 = 8.0;
     double re2 = 10.0;
     double re3 = 2.0;
@@ -56,7 +59,8 @@ TEST(Chernenko_Valery_ComplexNumberTest, Order_Of_Actions_multiplication_and_div
     EXPECT_EQ(exz1, exz2);
 }
 
-TEST(Chernenko_Valery_ComplexNumberTest, The_Operation_addition_Is_Commutative) {
+TEST(Chernenko_Valery_ComplexNumberTest, 
+The_Operation_addition_Is_Commutative) {
     double re1 = 1;
     double re2 = 2;
     double im1 = 1;
@@ -71,7 +75,8 @@ TEST(Chernenko_Valery_ComplexNumberTest, The_Operation_addition_Is_Commutative) 
     EXPECT_EQ(exz1, exz2);
 }
 
-TEST(Chernenko_Valery_ComplexNumberTest, The_Operation_division_Is_Not_Commutative) {
+TEST(Chernenko_Valery_ComplexNumberTest, 
+The_Operation_division_Is_Not_Commutative) {
     double re1 = 1;
     double re2 = 2;
     double im1 = 1;
