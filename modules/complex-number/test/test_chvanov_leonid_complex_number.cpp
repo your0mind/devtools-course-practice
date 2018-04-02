e661abd7c535f3b9a734c00765f04950739ced8b
Leonid2202 <leonidchvanov@mail.ru>
2018-04-02 10:32:33 +0300

    Added 1st test

---
+++ modules/complex-number/test/test_chvanov_leonid_complex_number.cpp
@@ -0,0 +1,18 @@
+// Copyright 2018 Chvanov Leonid
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+TEST(Chvanov_Leonid_ComplexNumberTest, Check_Commutative_Property_Of_Addition) {
+    // Arrange
+	ComplexNumber z1(5.0, 6.0);
+	ComplexNumber z2(15.0, 16.0);
+
+    // Act
+	ComplexNumber sum1(z1 + z2);
+	ComplexNumber sum2(z2 + z1);
+
+    // Assert
+	EXPECT_EQ(sum1, sum2);
+}
