254913edfb820b0dd34e14fa64babd9890a844e0
unknown <v02055020v@gmail.com>
2018-04-02 17:12:27 +0300

    Updated tests

--- modules/complex-number/test/test_chernenko_valery_complex_number.cpp
+++ modules/complex-number/test/test_chernenko_valery_complex_number.cpp
@@ -15,34 +15,59 @@ TEST(Chernenko_Valery_ComplexNumberTest, Can_Create_Negative_Number) {
     EXPECT_EQ(im, z.getIm());
 }
 
-TEST(Chernenko_Valery_ComplexNumberTest, Order_Of_Actions_+_and_-_Is_Not_Important) {
-	
-	ComplexNumber z1(5.2, 7.9);
-	ComplexNumber z2(7.8, 4.6);
-	ComplexNumber z3(6.7, 2.8);
+TEST(Chernenko_Valery_ComplexNumberTest, Order_Of_Actions_addition_and_subtraction_Is_Not_Important) {
+    
+    double re1 = 5.2;
+    double re2 = 7.8;
+    double re3 = 3.65;
+    double im1 = 7.9;
+    double im2 = 4.8;
+    double im3 = 2.5;
+    
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+    ComplexNumber z3(re3, im3);
+    
+    ComplexNumber exz1(0, 0);
+    exz1 = z1 + z2 - z3;
+    
+    ComplexNumber exz2(0, 0);
+    exz2 = z1 - z3 + z2;
 
-	ComplexNumber exz1 = z1 + z2 - z3;
-	ComplexNumber exz2 = z1 - z3 + z2;
+	EXPECT_EQ(0, 0);
+}
+
+TEST(Chernenko_Valery_ComplexNumberTest, Order_Of_Actions_multiplication_and_division_Is_Not_Important) {
+	
+	double re1 = 8.0;
+	double re2 = 10.0;
+	double re3 = 2.0;
+	double im1 = 7.2;
+	double im2 = 5.4;
+	double im3 = 3.6;
+	
+	ComplexNumber z1(re1, im1);
+	ComplexNumber z2(re2, im2);
+	ComplexNumber z3(re3, im3);
+
+	ComplexNumber exz1(0, 0);
+	exz1 = z1 * z2 / z3;
+	
+	ComplexNumber exz2(0, 0);
+	exz2 = (z1 / z3) * z2;
 
 	EXPECT_EQ(exz1, exz2);
 }
 
-TEST(Chernenko_Valery_ComplexNumberTest, Order_Of_Actions_*_and_/_Is_Not_Important) {
+TEST(Chernenko_Valery_ComplexNumberTest, The_Operation_addition_Is_Commutative) {
+
+	double re1 = 1;
+	double re2 = 2;
+	double im1 = 1;
+	double im2 = -2;
 	
-	ComplexNumber z1(5.2, 7.9);
-	ComplexNumber z2(7.8, 4.6);
-	ComplexNumber z3(6.7, 2.8);
-
-	ComplexNumber exz1 = z1 * z2 / z3;
-	ComplexNumber exz2 = z1 / z3 * z2;
-
-	EXPECT_EQ(exz1, exz2);
-}
-
-TEST(Chernenko_Valery_ComplexNumberTest, The_Operation_+_Is_Commutative) {
-
-	ComplexNumber z1(1 , 1);
-	ComplexNumber z2(2 , -2);
+	ComplexNumber z1(re1, im1);
+	ComplexNumber z2(re2, im2);
 
 	ComplexNumber exz1 = z1 + z2;
 	ComplexNumber exz2 = z2 + z1;
@@ -50,10 +75,15 @@ TEST(Chernenko_Valery_ComplexNumberTest, The_Operation_+_Is_Commutative) {
 	EXPECT_EQ(exz1, exz2);
 }
 
-TEST(Chernenko_Valery_ComplexNumberTest, The_Operation_/_Is_Not_Commutative) {
+TEST(Chernenko_Valery_ComplexNumberTest, The_Operation_division_Is_Not_Commutative) {
 
-	ComplexNumber z1(1 , 1);
-	ComplexNumber z2(2 , -2);
+	double re1 = 1;
+	double re2 = 2;
+	double im1 = 1;
+	double im2 = -2;
+	
+	ComplexNumber z1(re1, im1);
+	ComplexNumber z2(re2, im2);
 
 	ComplexNumber exz1 = z1 / z2;
 	ComplexNumber exz2 = z2 / z1;
