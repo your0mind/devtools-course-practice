1cb34051f22ccd1b1e51bb13d9f73e15243da12c
Leonid2202 <leonidchvanov@mail.ru>
2018-04-02 10:39:23 +0300

    Added 2nd test

--- modules/complex-number/test/test_chvanov_leonid_complex_number.cpp
+++ modules/complex-number/test/test_chvanov_leonid_complex_number.cpp
@@ -16,3 +16,17 @@ TEST(Chvanov_Leonid_ComplexNumberTest, Check_Commutative_Property_Of_Addition) {
     // Assert
 	EXPECT_EQ(sum1, sum2);
 }
+
+TEST(Chvanov_Leonid_ComplexNumberTest, Check_Distributive_Property_Of_Addition) {
+	// Arrange
+	ComplexNumber z1(5.0, 6.0);
+	ComplexNumber z2(15.0, 16.0);
+	ComplexNumber z3(115.0, 116.0);
+
+	// Act
+	ComplexNumber sum1(z1 * (z2 + z3));
+	ComplexNumber sum2(z1 * z2  + z1 * z3);
+
+	// Assert
+	EXPECT_EQ(sum1, sum2);
+}
