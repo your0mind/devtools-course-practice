d15ccd2e58f1fbdf55dd1da98465a2f35464bf0a
unknown <v02055020v@gmail.com>
2018-04-02 16:41:32 +0300

    Updated Tests

--- modules/complex-number/test/test_chernenko_valery_complex_number.cpp
+++ modules/complex-number/test/test_chernenko_valery_complex_number.cpp
@@ -6,8 +6,8 @@
 
 TEST(Chernenko_Valery_ComplexNumberTest, Can_Create_Negative_Number) {
 
-    Double re = -5.7;
-    Double im = -9.5;
+    double re = -5.7;
+    double im = -9.5;
 
     ComplexNumber z(re, im);
 
@@ -16,47 +16,47 @@ TEST(Chernenko_Valery_ComplexNumberTest, Can_Create_Negative_Number) {
 }
 
 TEST(Chernenko_Valery_ComplexNumberTest, Order_Of_Actions_+_and_-_Is_Not_Important) {
-    
-    ComplexNumber z1(5.2, 7.9);
-    ComplexNumber z2(7.8, 4.6);
-    ComplexNumber z3(6.7, 2.8);
+	
+	ComplexNumber z1(5.2, 7.9);
+	ComplexNumber z2(7.8, 4.6);
+	ComplexNumber z3(6.7, 2.8);
 
-    ComplexNumber exz1 = z1 + z2 - z3;
-    ComplexNumber exz2 = z1 - z3 + z2;
+	ComplexNumber exz1 = z1 + z2 - z3;
+	ComplexNumber exz2 = z1 - z3 + z2;
 
-    EXPECT_EQ(exz1, exz2);
+	EXPECT_EQ(exz1, exz2);
 }
 
 TEST(Chernenko_Valery_ComplexNumberTest, Order_Of_Actions_*_and_/_Is_Not_Important) {
-    
-    ComplexNumber z1(5.2, 7.9);
-    ComplexNumber z2(7.8, 4.6);
-    ComplexNumber z3(6.7, 2.8);
+	
+	ComplexNumber z1(5.2, 7.9);
+	ComplexNumber z2(7.8, 4.6);
+	ComplexNumber z3(6.7, 2.8);
 
-    ComplexNumber exz1 = z1 * z2 / z3;
-    ComplexNumber exz2 = z1 / z3 * z2;
+	ComplexNumber exz1 = z1 * z2 / z3;
+	ComplexNumber exz2 = z1 / z3 * z2;
 
-    EXPECT_EQ(exz1, exz2);
+	EXPECT_EQ(exz1, exz2);
 }
 
 TEST(Chernenko_Valery_ComplexNumberTest, The_Operation_+_Is_Commutative) {
 
-    ComplexNumber z1(1 , 1);
-    ComplexNumber z2(2 , -2);
+	ComplexNumber z1(1 , 1);
+	ComplexNumber z2(2 , -2);
 
-    ComplexNumber exz1 = z1 + z2;
-    ComplexNumber exz2 = z2 + z1;
+	ComplexNumber exz1 = z1 + z2;
+	ComplexNumber exz2 = z2 + z1;
 
-    EXPECT_EQ(exz1, exz2);
+	EXPECT_EQ(exz1, exz2);
 }
 
 TEST(Chernenko_Valery_ComplexNumberTest, The_Operation_/_Is_Not_Commutative) {
 
-    ComplexNumber z1(1 , 1);
-    ComplexNumber z2(2 , -2);
+	ComplexNumber z1(1 , 1);
+	ComplexNumber z2(2 , -2);
 
-    ComplexNumber exz1 = z1 / z2;
-    ComplexNumber exz2 = z2 / z1;
+	ComplexNumber exz1 = z1 / z2;
+	ComplexNumber exz2 = z2 / z1;
 
-    EXPECT_NE(exz1, exz2);
+	EXPECT_NE(exz1, exz2);
 }
