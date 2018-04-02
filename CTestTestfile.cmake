dca73b01d3d87e0ecaa77ae61d034209dc87f377
Trintal <iliyalev@yandex.ru>
2018-04-02 19:34:08 +0300

    ...

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Trintal/Documents/GitHub/devtools-course-practice
-# Build directory: C:/Users/Trintal/Documents/GitHub/devtools-course-practice
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("3rdparty/gtest")
-subdirs("modules/complex-number")
--- modules/complex-number/application/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Trintal/Documents/GitHub/devtools-course-practice/modules/complex-number/application
-# Build directory: C:/Users/Trintal/Documents/GitHub/devtools-course-practice/modules/complex-number/application
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
