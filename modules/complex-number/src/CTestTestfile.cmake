a293347b279f44fa5ba65aa431a609f3ec7541d1
Trintal <iliyalev@yandex.ru>
2018-04-02 19:48:35 +0300

    one more time

--- modules/complex-number/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Trintal/Documents/GitHub/devtools-course-practice/modules/complex-number/src
-# Build directory: C:/Users/Trintal/Documents/GitHub/devtools-course-practice/modules/complex-number/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
