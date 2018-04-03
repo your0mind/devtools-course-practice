c1375b141d6529b3978d2fcdcafbd7130235a434
sergeyt4444 <37845238+sergeyt4444@users.noreply.github.com>
2018-04-03 17:59:31 +0300

    Update test_Churakov_Sergey_complex_number.cpp

--- modules/complex-number/test/test_Churakov_Sergey_complex_number.cpp
+++ modules/complex-number/test/test_Churakov_Sergey_complex_number.cpp
@@ -6,31 +6,31 @@
 
 
 TEST(Churakov_Sergey_ComplexNumberTest, Can_Divide_By_One) {
-	ComplexNumber z(5.5, 10.5);
-	ComplexNumber one(1.0, 0);
-	EXPECT_EQ(z, z / one);
+    ComplexNumber z(5.5, 10.5);
+    ComplexNumber one(1.0, 0);
+    EXPECT_EQ(z, z / one);
 }
 
 TEST(Churakov_Sergey_ComplexNumberTest, Can_Mult_By_One) {
-	ComplexNumber z(5.5, 10.5);
-	ComplexNumber one(1.0, 0);
-	EXPECT_EQ(z, z * one);
+    ComplexNumber z(5.5, 10.5);
+    ComplexNumber one(1.0, 0);
+    EXPECT_EQ(z, z * one);
 }
 
 TEST(Churakov_Sergey_ComplexNumberTest, Can_Add_Zero) {
-	ComplexNumber z(5.5, 10.5);
-	ComplexNumber zero(0, 0);
-	EXPECT_EQ(z, z + zero);
+    ComplexNumber z(5.5, 10.5);
+    ComplexNumber zero(0, 0);
+    EXPECT_EQ(z, z + zero);
 }
 
 TEST(Churakov_Sergey_ComplexNumberTest, Can_Substract_Zero) {
-	ComplexNumber z(5.5, 10.5);
-	ComplexNumber zero(0, 0);
-	EXPECT_EQ(z, z - zero);
+    ComplexNumber z(5.5, 10.5);
+    ComplexNumber zero(0, 0);
+    EXPECT_EQ(z, z - zero);
 }
 
 TEST(Churakov_Sergey_ComplexNumberTest, Can_Mult_By_Zero) {
-	ComplexNumber z(5.5, 10.5);
-	ComplexNumber zero(0, 0);
-	EXPECT_EQ(z*zero, zero);
+    ComplexNumber z(5.5, 10.5);
+    ComplexNumber zero(0, 0);
+    EXPECT_EQ(z*zero, zero);
 }
