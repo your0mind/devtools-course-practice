78fb41824ceae11c64df9c5e3f13f532bf8ec90b
Konstantin <dr.kon5151@yandex.ru>
2018-04-04 00:07:25 +0300

    updated tests

--- modules/complex-number/test/test_kuznetsov_konstantin_complex_number.cpp
+++ modules/complex-number/test/test_kuznetsov_konstantin_complex_number.cpp
@@ -4,7 +4,7 @@
 
 #include "include/complex_number.h"
 
-TEST(Kuznetsov_Konstantin_ComplexNumberTest, New_Complex_Number_Is_Set_To_Zero) {
+TEST(Kuznetsov_Konstantin_ComplexNumberTest, New_Number_Is_Set_To_Zero) {
     ComplexNumber z;
 
     EXPECT_EQ(0.0, z.getRe());
@@ -15,7 +15,7 @@ TEST(Kuznetsov_Konstantin_ComplexNumberTest, Throws_When_Divided_By_Zero) {
     ComplexNumber z1(1.0, 1.0);
     ComplexNumber z2(2.0, 2.0);
     ComplexNumber z3(2.0, 2.0);
-	
+
     ASSERT_ANY_THROW(z1 / (z2 - z3));
 }
 
@@ -31,7 +31,7 @@ TEST(Kuznetsov_Konstantin_ComplexNumberTest, Can_Assign_Complex_Numbers) {
     EXPECT_NE(0.0, z1.getIm());
 }
 
-TEST(Kuznetsov_Konstantin_ComplexNumberTest, Can_Create_Number_Using_An_Operation) {
+TEST(Kuznetsov_Konstantin_ComplexNumberTest, Can_Create_Number_Using_Sum) {
     double re1 = 1.0;
     double im1 = 1.0;
     double re2 = 2.0;
@@ -43,7 +43,7 @@ TEST(Kuznetsov_Konstantin_ComplexNumberTest, Can_Create_Number_Using_An_Operatio
     ASSERT_NO_THROW(ComplexNumber z(z1 + z2));
 }
 
-TEST(Kuznetsov_Konstantin_ComplexNumberTest, Can_Get_Re_And_Im_Using_Conjugate) {
+TEST(Kuznetsov_Konstantin_ComplexNumberTest, Can_Get_Re_And_Im_Using_Conj) {
     double re = 2.0;
     double im = 3.0;
 
