0021b4acc8318afdc19fe00b53075a27b08937f9
Roman Kudalin <th3.f3nom3n@gmail.com>
2018-04-03 20:27:59 +0300

    deleted whitespaces

--- modules/complex-number/test/test_kudalin_roman_complex_number.cpp
+++ modules/complex-number/test/test_kudalin_roman_complex_number.cpp
@@ -9,54 +9,54 @@ TEST(Kudalin_Roman_ComplexNumberTest, Addition_Is_Associative) {
     ComplexNumber z1(15.0, 23.0);
     ComplexNumber z2(9.4, 17.9);
     ComplexNumber z3(13.3, 5.5);
-    
+
     // Act
     ComplexNumber sum1 = z1 + (z2 + z3);
     ComplexNumber sum2 = (z1 + z2) + z3;
-    
+
     // Assert
     EXPECT_EQ(sum1, sum2);
 }
 
-TEST(Kudalin_Roman_ComplexNumberTest, 
-     Addition_With_Zero_Returns_Original_Number) {
-    
+TEST(Kudalin_Roman_ComplexNumberTest,
+Addition_With_Zero_Returns_Original_Number) {
+
     // Arrange
     ComplexNumber z1(15.0, 23.0);
     ComplexNumber z2(0.0, 0.0);
-    
+
     // Act
     ComplexNumber z = z1 + z2;
-    
+
     // Assert
     EXPECT_EQ(z, z1);
 }
 
-TEST(Kudalin_Roman_ComplexNumberTest, 
-     Multiplication_By_Zero_Returns_Zero) {
-    
+TEST(Kudalin_Roman_ComplexNumberTest,
+Multiplication_By_Zero_Returns_Zero) {
+
     // Arrange
     ComplexNumber z1(15.0, 23.0);
     ComplexNumber z2(0.0, 0.0);
-    
+
     // Act
     ComplexNumber z = z1 * z2;
-    
+
     // Assert
     ComplexNumber expected_z(0.0, 0.0);
     EXPECT_EQ(expected_z, z);
 }
 
-TEST(Kudalin_Roman_ComplexNumberTest, 
-     Addition_With_Complex_Conjugate_Returns_A_Real_Number) {
-    
+TEST(Kudalin_Roman_ComplexNumberTest,
+Addition_With_Complex_Conjugate_Returns_A_Real_Number) {
+
     // Assert
     ComplexNumber z1(15.0, 23.0);
     ComplexNumber z2(15.0, -23.0);
-    
+
     // Act
     ComplexNumber z = z1 + z2;
-    
+
     // Assert
     double exp_res = 30.0;
     EXPECT_EQ(z.getRe(), exp_res);
@@ -66,10 +66,10 @@ TEST(Kudalin_Roman_ComplexNumberTest,
 TEST(Kudalin_Roman_ComplexNumberTest, Division_By_Itself_Returns_1) {
     // Assert
     ComplexNumber z1(15.0, 23.0);
-    
+
     // Act
     ComplexNumber z = z1 / z1;
-    
+
     // Assert
     double exp_re = 1.0;
     double exp_im = 0.0;
