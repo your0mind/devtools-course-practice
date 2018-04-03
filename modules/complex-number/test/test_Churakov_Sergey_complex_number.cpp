41f9769d2f3031ca54fea86ed9b85babff993d6e
sergeyt4444 <sergeyt4444@rambler.ru>
2018-04-03 13:35:07 +0300

    added 5 new tests for complex_number

---
+++ modules/complex-number/test/test_Churakov_Sergey_complex_number.cpp
@@ -0,0 +1,36 @@
+// Copyright 2018 Churakov Sergey
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+
+TEST(Churakov_Sergey_ComplexNumberTest, Can_Divide_By_One) {
+	ComplexNumber z(5.5, 10.5);
+	ComplexNumber one(1.0, 0);
+	EXPECT_EQ(z, z / one);
+}
+
+TEST(Churakov_Sergey_ComplexNumberTest, Can_Mult_By_One) {
+	ComplexNumber z(5.5, 10.5);
+	ComplexNumber one(1.0, 0);
+	EXPECT_EQ(z, z * one);
+}
+
+TEST(Churakov_Sergey_ComplexNumberTest, Can_Add_Zero) {
+	ComplexNumber z(5.5, 10.5);
+	ComplexNumber zero(0, 0);
+	EXPECT_EQ(z, z + zero);
+}
+
+TEST(Churakov_Sergey_ComplexNumberTest, Can_Substract_Zero) {
+	ComplexNumber z(5.5, 10.5);
+	ComplexNumber zero(0, 0);
+	EXPECT_EQ(z, z - zero);
+}
+
+TEST(Churakov_Sergey_ComplexNumberTest, Can_Mult_By_Zero) {
+	ComplexNumber z(5.5, 10.5);
+	ComplexNumber zero(0, 0);
+	EXPECT_EQ(z*zero, zero);
+}
