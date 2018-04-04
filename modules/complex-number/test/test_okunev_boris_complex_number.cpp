17a43517b4ca61e6bdf21f017941e7d5d4afc46c
Boris Okunev <hait.08@mail.ru>
2018-04-04 21:38:06 +0300

    Added test

---
+++ modules/complex-number/test/test_okunev_boris_complex_number.cpp
@@ -0,0 +1,95 @@
+// Copyright 2018 Okunev Boris
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+TEST(Okunev_Boris_ComplexNumberTest, Two_Same_Complex_Are_Equal) {
+    // Arrange
+    double re = 1.0;
+    double im = 2.0;
+
+    double re1 = 1.0;
+    double im1 = 2.0;
+
+    // Act
+    ComplexNumber z(re, im);
+    ComplexNumber z1(re, im);
+
+    // Assert
+    EXPECT_TRUE(z == z1);
+}
+
+TEST(Okunev_Boris_ComplexNumberTest, Two_Different_Complex_Are_Not_Equal) {
+    // Arrange
+    double re = 1.0;
+    double im = 2.0;
+
+    double re1 = 2.0;
+    double im1 = 3.0;
+
+    // Act
+    ComplexNumber z(re, im);
+
+    // Assert
+    EXPECT_FALSE(z == z1);
+}
+
+TEST(Okunev_Boris_ComplexNumberTest, No_Throw_Sum_Two_Complex) {
+    // Arrange
+    double re = 1.0;
+    double im = 2.0;
+
+    double re1 = 2.0;
+    double im1 = 3.0;
+
+    // Act
+    ComplexNumber z(re, im);
+
+    // Assert
+    EXPECT_NO_THROW(z + z1);
+}
+
+TEST(Okunev_Boris_ComplexNumberTest, Sum_Two_Complex_Correct) {
+    // Arrange
+    double re = 1.0;
+    double im = 2.0;
+
+    double re1 = 2.0;
+    double im1 = 3.0;
+
+    // Act
+    ComplexNumber z(re, im);
+    ComplexNumber expect(3, 5);
+
+    // Assert
+    EXPECT_EQ(expect, z + z1);
+}
+
+TEST(Okunev_Boris_ComplexNumberTest, Can_Set_Re) {
+    // Arrange
+    double re = 1.0;
+    double im = 2.0;
+
+    double re1 = 5;
+
+    // Act
+    ComplexNumber z(re, im);
+    z.setRe(re1);
+    ComplexNumber expect(re1, im);
+
+    // Assert
+    EXPECT_EQ(expect, z);
+}
+
+TEST(Okunev_Boris_ComplexNumberTest, Can_Get_Im) {
+    // Arrange
+    double re = 1.0;
+    double im = 2.0;
+
+    // Act
+    ComplexNumber z(re, im);
+
+    // Assert
+    EXPECT_EQ(im, z.getIm());
+}
