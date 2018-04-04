3bbe314ce38d6481ffce0748dd02c9873e52c85c
SylaiseElvenan <sveta03011997@gmail.com>
2018-04-04 13:28:24 +0300

    Added new tests №1 for lab1

---
+++ modules/complex-number/test/test_kulikova_sveta_complex_number.cpp
@@ -0,0 +1,26 @@
+// Copyright 2018 Kulikova Svetlana
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+TEST(Test1, Addition) {
+	// Arrange
+	double re1 = 1.2;
+	double im1 = 3.4;
+	double re2 = 5.6;
+	double im2 = 7.8;
+
+	// Act
+	ComplexNumber a(re1, im1);
+	ComplexNumber b(re2, im2);
+	ComplexNumber c = a + b;
+
+	// Assert
+	EXPECT_EQ(re1, a.getRe());
+	EXPECT_EQ(im1, a.getIm());
+	EXPECT_EQ(re2, b.getRe());
+	EXPECT_EQ(im2, b.getIm());
+	EXPECT_EQ(re1 + re2, c.getRe());
+	EXPECT_EQ(im1 + im2, c.getIm());
+}
