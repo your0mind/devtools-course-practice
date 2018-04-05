49919e04108a4c80b8a1608dbef0c23814da4794
leonid2202 <37924296+leonid2202@users.noreply.github.com>
2018-04-05 11:28:32 +0300

    Update test_chvanov_leonid_complex_number.cpp

--- modules/complex-number/test/test_chvanov_leonid_complex_number.cpp
+++ modules/complex-number/test/test_chvanov_leonid_complex_number.cpp
@@ -18,52 +18,52 @@ TEST(Chvanov_Leonid_ComplexNumberTest, Check_Commutative_Property_Of_Addition) {
 }
 
 TEST(Chvanov_Leonid_ComplexNumberTest, Check_Commutative_Property_Of_Multiplication) {
-	// Arrange
+    // Arrange
     ComplexNumber z1(5.0, 6.0);
     ComplexNumber z2(15.0, 16.0);
 
-	// Act
+    // Act
     ComplexNumber prod1(z1 * z2);
     ComplexNumber prod2(z2 * z1);
 
-	// Assert
+    // Assert
     EXPECT_EQ(prod1, prod2);
 }
 
 TEST(Chvanov_Leonid_ComplexNumberTest, Check_Distributive_Property_Of_Addition) {
-	// Arrange
+    // Arrange
     ComplexNumber z1(5.0, 6.0);
     ComplexNumber z2(15.0, 16.0);
     ComplexNumber z3(115.0, 116.0);
 
-	// Act
+    // Act
     ComplexNumber sum1(z1 * (z2 + z3));
     ComplexNumber sum2(z1 * z2  + z1 * z3);
 
-	// Assert
+    // Assert
     EXPECT_EQ(sum1, sum2);
 }
 
 TEST(Chvanov_Leonid_ComplexNumberTest, ComplexNumber_Divided_By_Itself_Returns_1) {
-	// Arrange
+    // Arrange
     ComplexNumber z(5.0, 6.0);
     ComplexNumber expected_res(1.0, 0.0);
 
-	// Act
+    // Act
     ComplexNumber q(z / z);
 
-	// Assert
+    // Assert
     EXPECT_EQ(expected_res, q);
 }
 
 TEST(Chvanov_Leonid_ComplexNumberTest, 
-    Multiplication_By_Neutral_Element_Does_Not_Change_ComplexNumber) {
-	// Arrange
+     Multiplication_By_Neutral_Element_Does_Not_Change_ComplexNumber) {
+    // Arrange
     ComplexNumber z(5.0, 6.0);
     ComplexNumber n(1.0, 0.0);
 
-	// Act
+    // Act
 
-	// Assert
+    // Assert
     EXPECT_EQ(z, z*n);
 }
