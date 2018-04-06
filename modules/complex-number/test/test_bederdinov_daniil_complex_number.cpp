8ed84993064768ddf5ae59a2f7670e36731719f0
Daniil_Bederdinov <palidos@yandex.ru>
2018-04-06 16:27:02 +0300

    First attempt

--- modules/complex-number/test/test_bederdinov_daniil_complex_number.cpp
+++ modules/complex-number/test/test_bederdinov_daniil_complex_number.cpp
@@ -1,18 +1,63 @@
-// Copyright 2018 DD
+// Copyright 2018 Bederdinov Daniil
 
 #include <gtest/gtest.h>
 
 #include "include/complex_number.h"
 
-TEST(Bederdinov_Daniil_ComplexNumberTest, Can_Create_Zer0) {
+TEST(Bederdinov_Daniil_ComplexNumberTest, Can_Multiply_By_Zero) {
     // Arrange
-    double re = 0.0;
-    double im = 0.0;
+    ComplexNumber z(7.1488, 13.22);
 
     // Act
-    ComplexNumber z(re, im);
+    ComplexNumber z1 = z * 0;
 
     // Assert
-    EXPECT_EQ(re, z.getRe());
-    EXPECT_EQ(im, z.getIm());
+    EXPECT_EQ(0,z1);
+}
+
+TEST(Bederdinov_Daniil_ComplexNumberTest,
+    Can_Subtract_Complex_Nubmer_From_Itself) {
+    // Arrange
+    ComplexNumber z(7.1483, 13.32);
+
+    // Act
+    ComplexNumber z1 = z - z;
+
+    // Assert
+    EXPECT_EQ(0, z1);
+}
+
+TEST(Bederdinov_Daniil_ComplexNumberTest,
+    Multiplification_By_One_Changes_Nothing) {
+    // Arrange
+    ComplexNumber z(7.1483, 13.32);
+
+    // Act
+    ComplexNumber z1 = z * 1;
+
+    // Assert
+    EXPECT_EQ(z1, z1);
+}
+
+TEST(Bederdinov_Daniil_ComplexNumberTest,
+    Return_One_When_Divided_By_Itself) {
+    // Arrange
+    ComplexNumber z(7.1483, 13.32);
+
+    // Act
+    ComplexNumber z1 = z / z;
+
+    // Assert
+    EXPECT_EQ(1, z1);
+}
+
+TEST(Bederdinov_Daniil_ComplexNumberTest, Can_Add_Zero) {
+    // Arrange
+    ComplexNumber z(7.1483, 13.32);
+
+    // Act
+    ComplexNumber z1 = z + 0;
+
+    // Assert
+    EXPECT_EQ(z1, z);
 }
