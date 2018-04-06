6b777494739f740f7af3fff087d67d661fad4f5b
Daniil_Bederdinov <palidos@yandex.ru>
2018-04-06 16:57:40 +0300

    Second attempt

--- modules/complex-number/test/test_bederdinov_daniil_complex_number.cpp
+++ modules/complex-number/test/test_bederdinov_daniil_complex_number.cpp
@@ -2,61 +2,66 @@
 
 #include <gtest/gtest.h>
 
-#include "include/complex_number.h"
+#include "complex_number.h"
 
 TEST(Bederdinov_Daniil_ComplexNumberTest, Can_Multiply_By_Zero) {
     // Arrange
-    ComplexNumber z(7.1488, 13.22);
+    ComplexNumber z1(7.1488, 13.22);
+    ComplexNumber z2(0, 0);
 
     // Act
-    ComplexNumber z1 = z * 0;
+    ComplexNumber z = z1 * z2;
 
     // Assert
-    EXPECT_EQ(0,z1);
+    EXPECT_EQ(z2,z);
 }
 
 TEST(Bederdinov_Daniil_ComplexNumberTest,
     Can_Subtract_Complex_Nubmer_From_Itself) {
     // Arrange
-    ComplexNumber z(7.1483, 13.32);
+    ComplexNumber z1(7.1483, 13.32);
+    ComplexNumber z2(0, 0);
 
     // Act
-    ComplexNumber z1 = z - z;
+    ComplexNumber z = z1 - z1;
 
     // Assert
-    EXPECT_EQ(0, z1);
+    EXPECT_EQ(z2, z);
 }
 
 TEST(Bederdinov_Daniil_ComplexNumberTest,
     Multiplification_By_One_Changes_Nothing) {
     // Arrange
-    ComplexNumber z(7.1483, 13.32);
+    ComplexNumber z1(7.1483, 13.32);
+    ComplexNumber z2(1,0);
 
     // Act
-    ComplexNumber z1 = z * 1;
+    ComplexNumber z = z1 * z2;
 
     // Assert
-    EXPECT_EQ(z1, z1);
+    EXPECT_EQ(z, z1);
 }
 
 TEST(Bederdinov_Daniil_ComplexNumberTest,
     Return_One_When_Divided_By_Itself) {
     // Arrange
-    ComplexNumber z(7.1483, 13.32);
+    ComplexNumber z1(7.1483, 13.32);
+    ComplexNumber z2(1, 0);
 
     // Act
-    ComplexNumber z1 = z / z;
+    ComplexNumber z = z1 / z1;
 
     // Assert
-    EXPECT_EQ(1, z1);
+    EXPECT_EQ(z2, z);
 }
 
 TEST(Bederdinov_Daniil_ComplexNumberTest, Can_Add_Zero) {
     // Arrange
-    ComplexNumber z(7.1483, 13.32);
+    ComplexNumber z1(7.1483, 13.32);
+    ComplexNumber z2(0, 0);
 
     // Act
-    ComplexNumber z1 = z + 0;
+    ComplexNumber z = z1 + z2;
 
     // Assert
     EXPECT_EQ(z1, z);
