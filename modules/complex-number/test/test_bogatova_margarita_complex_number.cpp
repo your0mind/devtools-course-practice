9f64866bbcd2a8ff9d0adcd870c557c7e2d1c88c
BogatovaMargarita <33071094+BogatovaMargarita@users.noreply.github.com>
2018-04-06 20:28:05 +0300

    Update test_bogatova_margarita_complex_number.cpp

--- modules/complex-number/test/test_bogatova_margarita_complex_number.cpp
+++ modules/complex-number/test/test_bogatova_margarita_complex_number.cpp
@@ -4,9 +4,9 @@
 
 #include "include/complex_number.h"
 
-TEST(Bogatova_Margarita_ComplexNumberTest, 
+TEST(Bogatova_Margarita_ComplexNumberTest,
      can_create_complex_number_with_negative_values) {
-    ASSERT_NO_THROW(ComplexNumber c(-1.0,-4.0));
+    ASSERT_NO_THROW(ComplexNumber c(-1.0, -4.0));
 }
 
 TEST(Bogatova_Margarita_ComplexNumberTest, set_and_get_some_number) {
@@ -21,13 +21,11 @@ TEST(Bogatova_Margarita_ComplexNumberTest, set_and_get_some_number) {
 
 TEST(Bogatova_Margarita_ComplexNumberTest, equality_operator_return_false) {
     ComplexNumber c1(6.0, 8.0), c2(4.0, 9.0);
-    
     EXPECT_FALSE(c1 == c2);
 }
 
 TEST(Bogatova_Margarita_ComplexNumberTest, inequality_operator_return_true) {
     ComplexNumber c1(6.0, 8.0), c2(6.0, 8.0);
-    
     EXPECT_FALSE(c1 != c2);
 }
 
@@ -37,7 +35,6 @@ TEST(Bogatova_Margarita_ComplexNumberTest, multiple_equating) {
     c1 = c2 = c3;
     
     EXPECT_TRUE(c1 == c2 && c1 == c3 && c3 == c2);
-    
 }
 
 TEST(Bogatova_Margarita_ComplexNumberTest, addition_is_commutative) {
@@ -50,10 +47,10 @@ TEST(Bogatova_Margarita_ComplexNumberTest, addition_is_commutative) {
 }
 
 TEST(Bogatova_Margarita_ComplexNumberTest, subtraction_is_not_commutative) {
-	ComplexNumber c1(6.0, 8.0), c2(4.0, 9.0);
-	ComplexNumber res1, res2;
-	res1 = c1 - c2;
-	res2 = c2 - c1;
-
-	EXPECT_NE(res1, res2);
+    ComplexNumber c1(6.0, 8.0), c2(4.0, 9.0);
+    ComplexNumber res1, res2;
+    res1 = c1 - c2;
+    res2 = c2 - c1;
+    
+    EXPECT_NE(res1, res2);
 }
