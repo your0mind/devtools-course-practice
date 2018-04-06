2e9d798edee6461baf9a7a18b3d891163a979bb4
VarvaraPozdeeva <37691481+VarvaraPozdeeva@users.noreply.github.com>
2018-04-06 10:47:47 +0300

    Update tests file

--- modules/complex-number/test/test_pozdeeva_varvara_complex_number.cpp
+++ modules/complex-number/test/test_pozdeeva_varvara_complex_number.cpp
@@ -4,7 +4,7 @@
 
 #include "include/complex_number.h"
 
-TEST(Pozdeeva_Varvara_ComplexNumberTest, comparison_not_equal_return_false){   
+TEST(Pozdeeva_Varvara_ComplexNumberTest, comparison_not_equal_return_false) {   
     // Arrange
     ComplexNumber z1(26.0, 14.0);
     ComplexNumber z2(26.1, 14.1);
@@ -12,7 +12,7 @@ TEST(Pozdeeva_Varvara_ComplexNumberTest, comparison_not_equal_return_false){
     // Act & Assert
     EXPECT_FALSE(z1 == z2);
 }
-TEST(Pozdeeva_Varvara_ComplexNumberTest, comparison_equal_return_true){
+TEST(Pozdeeva_Varvara_ComplexNumberTest, comparison_equal_return_true) {
          // Arrange
     ComplexNumber z1(26.0, 14.0);
     ComplexNumber z2(26.0, 14.0);
@@ -20,7 +20,7 @@ TEST(Pozdeeva_Varvara_ComplexNumberTest, comparison_equal_return_true){
     // Act & Assert
     EXPECT_TRUE(z1 == z2);
 }
-TEST(Pozdeeva_Varvara_ComplexNumberTest, comparison_equal_return_false){
+TEST(Pozdeeva_Varvara_ComplexNumberTest, comparison_equal_return_false) {
          // Arrange
     ComplexNumber z1(26.0, 14.0);
     ComplexNumber z2(26.0, 14.0);
@@ -28,17 +28,17 @@ TEST(Pozdeeva_Varvara_ComplexNumberTest, comparison_equal_return_false){
     // Act & Assert
     EXPECT_FALSE(z1 != z2);
 }
-TEST(Pozdeeva_Varvara_ComplexNumberTest, can_multiplied_by_zero){
+TEST(Pozdeeva_Varvara_ComplexNumberTest, can_multiplied_by_zero) {
          // Arrange
     ComplexNumber z1(2.0, 4.0);
-    ComplexNumber z2(0.0, 0.0);    
+    ComplexNumber z2(0.0, 0.0);
     // Acc
     z1 = z1*z2;
 
     // Assert
     EXPECT_EQ(z2, z1);
 }
-TEST(Pozdeeva_Varvara_ComplexNumberTest, correct_order_of_operations){
+TEST(Pozdeeva_Varvara_ComplexNumberTest, correct_order_of_operations) {
          // Arrange
     ComplexNumber z1(2.0, 4.0);
     ComplexNumber z2(6.0, 4.0);
