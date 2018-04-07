cf6c6abf8f12812f8f3e28974831299f27bb3d3e
GusevAlexandr <gusevsasha1996@mail.ru>
2018-04-07 22:41:58 +0300

    updated

--- modules/complex-number/test/test_gusev_alexandr_complex_number.cpp
+++ modules/complex-number/test/test_gusev_alexandr_complex_number.cpp
@@ -18,58 +18,59 @@ TEST(Gusev_Alexandr_ComplexNumberTest, Create_Object_Class) {
 }
 
 TEST(Gusev_Aleksandr_ComplexNumberTest, Sum_Test) {
-  // Arrange
+// Arrange
   double re1 = 5.1;
   double im1 = 6.2;
   double re2 = 3.0;
   double im2 = 2.0;
   ComplexNumber c1(re1, im1);
   ComplexNumber c2(re2, im2);
-  // Act
+// Act
   ComplexNumber c = c1 + c2;
-  // Assert
+// Assert
   EXPECT_EQ(c.getRe(), 8.1);
   EXPECT_EQ(c.getIm(), 8.2);
 }
 TEST(Gusev_Aleksandr_ComplexNumberTest, Substruction) {
-  // Arrange
+// Arrange
   double re1 = 3.0;
   double im1 = 5.0;
   double re2 = 4.0;
   double im2 = 5.0;
   ComplexNumber c1(re1, im1);
   ComplexNumber c2(re2, im2);
-  // Act
+// Act
   ComplexNumber c = c1 - c2;
-  // Assert
+// Assert
   EXPECT_EQ(c.getRe(), -1.0);
   EXPECT_EQ(c.getIm(), 0.0);
 }
 TEST(Gusev_Aleksandr_ComplexNumberTest, Multiplication) {
-  // Arrange 
+// Arrange 
   double re1 = 4.0;
   double im1 = 5.0;
   double re2 = 2.0;
   double im2 = 3.0;
-  // Act
+// Act
   ComplexNumber c1(re1, im1);
   ComplexNumber c2(re2, im2);
   ComplexNumber c = c1 * c2;
-  // Assert
+// Assert
   EXPECT_EQ(c.getRe(), -7.0);
   EXPECT_EQ(c.getIm(), 22.0);
 }
 TEST(Gusev_Aleksandr_ComplexNumberTest, Commutativity_Sum_Property) {
-  // Arrange
+// Arrange
   double re1 = 4.2;
   double im1 = 5.2;
   double re2 = 2.3;
   double im2 = 3.3;
-  // Act
+// Act
   ComplexNumber c1(re1, im1);
   ComplexNumber c2(re2, im2);
   ComplexNumber sum12 = c1 + c2;
   ComplexNumber sum21 = c2 + c1;
-  // Assert
+// Assert
   ASSERT_TRUE(sum12 == sum21);
 }
+
