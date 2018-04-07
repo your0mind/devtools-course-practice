11420dc0947c8a01d35f5543c80e4e01251d0c2f
Imadzuma <mar-gosha98@mail.ru>
2018-04-07 18:33:37 +0300

    Добавленно ААА и еще пара тестов

--- modules/complex-number/test/test_Mezina_Margarita_complex_number.cpp
+++ modules/complex-number/test/test_Mezina_Margarita_complex_number.cpp
@@ -3,30 +3,96 @@
 #include <gtest/gtest.h>
 #include "include/complex_number.h"
 
-TEST(Mezina_Margarita_ComplexNumber, Can_Create_Complex_Number) {
-    EXPECT_NO_THROW(ComplexNumber cn(3, 4));
-}
-
 TEST(Mezina_Margarita_ComplexNumber, Can_Get_Real_Number) {
-    ComplexNumber cn(3, 4);
-    EXPECT_EQ(cn.getRe(), 3);
+    // Arrange
+    double re = 3;
+    double im = 4;
+
+    // Act
+    ComplexNumber cn(re, im);
+
+    // Assert
+    EXPECT_EQ(cn.getRe(), re);
 }
 
 TEST(Mezina_Margarita_ComplexNumber, Can_Get_Imaginary_Number) {
-    ComplexNumber cn(3, 4);
-    EXPECT_EQ(cn.getIm(), 4);
+    // Arrange
+    double re = 3;
+    double im = 4;
+
+    // Act
+    ComplexNumber cn(re, im);
+
+    // Assert
+    EXPECT_EQ(cn.getIm(), im);
 }
 
 TEST(Mezina_Margarita_ComplexNumber, Can_Plus_Number) {
-    ComplexNumber cn1(3, 4);
-    ComplexNumber cn2(-1, -2);
-    ComplexNumber cn3(2, 2);
+    // Arrange
+    double re1 = 3;
+    double im1 = 4;
+    double re2 = -1;
+    double im2 = -2;
+    double re3 = 2;
+    double im3 = 2;
+
+    // Act
+    ComplexNumber cn1(re1, im1);
+    ComplexNumber cn2(re2, im2);
+    ComplexNumber cn3(re3, im3);
+
+    // Assert
     EXPECT_EQ(cn3, cn1+cn2);
 }
 
 TEST(Mezina_Margarita_ComplexNumber, Can_Minus_Number) {
-    ComplexNumber cn1(3, 4);
-    ComplexNumber cn2(-1, -2);
-    ComplexNumber cn3(4, 6);
+    // Arrange
+    double re1 = 3;
+    double im1 = 4;
+    double re2 = -1;
+    double im2 = -2;
+    double re3 = 4;
+    double im3 = 6;
+
+    // Act
+    ComplexNumber cn1(re1, im1);
+    ComplexNumber cn2(re2, im2);
+    ComplexNumber cn3(re3, im3);
+
+    // Assert
     EXPECT_EQ(cn3, cn1 - cn2);
 }
+
+TEST(Mezina_Margarita_ComplexNumber, Can_Create_Complex_Number) {
+    // Arrange
+    double re1 = 3;
+    double im1 = 4;
+    double re2 = -1;
+    double im2 = -2;
+    double re3 = 5;
+    double im3 = -10;
+
+    // Act
+    ComplexNumber cn1(re1, im1);
+    ComplexNumber cn2(re2, im2);
+    ComplexNumber cn3(re3, im3);
+
+    // Assert
+    EXPECT_EQ(cn3, cn1 * cn2);
+}
+
+TEST(Mezina_Margarita_ComplexNumber, i_in_square_is_minus_one) {
+    // Arrange
+    double re1 = 0;
+    double im1 = 1;
+    double re2 = -1;
+    double im2 = 0;
+
+    // Act
+    ComplexNumber cn1(re1, im1);
+    ComplexNumber cn2(re2, im2);
+
+    // Assert
+    EXPECT_EQ(cn2, cn1 * cn1);
+}
+
