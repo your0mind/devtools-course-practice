6e0e78a840d7a9c7a1fb316afba4bffdb04901f5
AleksandrPanov <sasha1_1997@mail.ru>
2018-04-08 00:08:53 +0400

    changed the format of tests, complex numbers are declared in the Arrange section

--- modules/complex-number/test/test_panov_aleksander_complex_number.cpp
+++ modules/complex-number/test/test_panov_aleksander_complex_number.cpp
@@ -5,62 +5,61 @@
 #include "include/complex_number.h"
 
 TEST(Panov_Aleksander_ComplexNumberTest, Can_Create_Copy) {
-	// Arrange
+    // Arrange
     double re = -1.0;
     double im = 1.0;
-	
-	// Act
     ComplexNumber z(re, im);
+    
+    // Act
     ComplexNumber copy_z(z);
-	
-	// Assert
+
+    // Assert
     EXPECT_EQ(copy_z, z);
 }
 
 TEST(Panov_Aleksander_ComplexNumberTest, Can_Multiplication_Real) {
-	// Arrange
+    // Arrange
     double re1 = 3.0, re2 = 4.0;
-	
-	// Act
-    ComplexNumber a(re1, 0.0), b(re2, 0.0);
-    ComplexNumber res(re1*re2, 0.0);
-	
-	// Assert
-    EXPECT_EQ(a*b, res);
+    ComplexNumber a(re1, 0.0), b(re2, 0.0), correct_res(re1*re2, 0.0);
+
+    // Act
+    ComplexNumber res = a*b;
+
+    // Assert
+    EXPECT_EQ(res, correct_res);
 }
 
 TEST(Panov_Aleksander_ComplexNumberTest, Can_Multiplication_Imaginary) {
-	// Arrange
+    // Arrange
     double im1 = 3.0, im2 = 4.0;
-	
-	// Act
-    ComplexNumber a(0.0, im1), b(0.0, im2);
-    ComplexNumber res(-im1*im2, 0.0);
-	
-	// Assert
-    EXPECT_EQ(a*b, res);
+    ComplexNumber a(0.0, im1), b(0.0, im2), correct_res(-im1 * im2, 0.0);
+
+    // Act
+    ComplexNumber res = a*b;
+
+    // Assert
+    EXPECT_EQ(res, correct_res);
 }
 
 TEST(Panov_Aleksander_ComplexNumberTest, Can_Division_Real) {
-	// Arrange
+    // Arrange
     double re1 = 12.0, re2 = 3.0;
+    ComplexNumber a(re1, 0.0), b(re2, 0.0), correct_res(re1 / re2, 0.0);
+
+    // Act
+    ComplexNumber res = a / b;
 	
-	// Act
-    ComplexNumber a(re1, 0.0), b(re2, 0.0);
-    ComplexNumber res(re1/re2, 0.0);
-	
-	// Assert
-    EXPECT_EQ(a/b, res);
+    // Assert
+    EXPECT_EQ(res, correct_res);
 }
 
 TEST(Panov_Aleksander_ComplexNumberTest, Can_Division_Imaginary) {
-	// Arrange
-    ComplexNumber z1(0.0, 4.0);
-    ComplexNumber z2(0.0, 2.0);
-	
-	// Act
-    ComplexNumber res(2.0, 0.0);
-	
-	// Assert
-    EXPECT_EQ(z1 / z2, res);
+    // Arrange
+    ComplexNumber z1(0.0, 4.0), z2(0.0, 2.0), correct_res(2.0, 0.0);
+    
+    // Act
+    ComplexNumber res  = z1 / z2;
+    
+    // Assert
+    EXPECT_EQ(res, correct_res);
 }
