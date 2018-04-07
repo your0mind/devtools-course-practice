818bc550da0f18d50c1af2134c74ad7c6cb20e9c
Alisa Novikova <alisa.nov72@gmail.com>
2018-04-07 16:47:27 +0300

    Update test_novikova_alisa_complex_number.cpp

--- build/modules/complex-number/test/test_novikova_alisa_complex_number.cpp
+++ build/modules/complex-number/test/test_novikova_alisa_complex_number.cpp
@@ -5,16 +5,15 @@
 #include "include/complex_number.h"
 
 TEST(Novikova_Alisa_ComplexNumberTest, Can_Create_Not_Zero) {
-    // Arrange
-    double re = 3.14;
-    double im = 15.9;
-
-    // Act
-    ComplexNumber z(re, im);
-
-    // Assert
-    EXPECT_EQ(re, z.getRe());
-    EXPECT_EQ(im, z.getIm());
+	// Arrange
+	double re = 3.14;
+	double im = 15.9;
+	
+	// Act
+	ComplexNumber z(re, im);
+	// Assert
+	EXPECT_EQ(re, z.getRe());
+	EXPECT_EQ(im, z.getIm());
 }
 TEST(Novikova_Alisa_ComplexNumberTest, Can_Division_Equal_Complex_Numbers){
 	// Arrange
@@ -32,7 +31,7 @@ TEST(Novikova_Alisa_ComplexNumberTest, Equal_Sums_With_Not_Equal_Comlex_Numbers)
 	// Arrange
 	ComplexNumber z1(23.4, 15.1);
 	ComplexNumber z2(18.7, 9.8);
-
+	
 	// Act
 	ComplexNumber sum1 = z1 + z2;
 	ComplexNumber sum2 = z2 + z1;
@@ -45,6 +44,7 @@ TEST(Novikova_Alisa_ComplexNumberTest, Can_Difference_Complex_Number_And_Zero){
 	ComplexNumber z1(-9.0, 13.0);
 	ComplexNumber z2(0.0, 0.0);
 
+	
 	// Act
 	ComplexNumber dif = z2 - z1;
 
