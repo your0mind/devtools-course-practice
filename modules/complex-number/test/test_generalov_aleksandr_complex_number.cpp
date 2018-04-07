8ecc1c67bfa8f9959563adc068a848ee0c46bcf2
Aleksandr <aleksandr-generalov19@yandex.ru>
2018-04-07 20:00:33 +0300

    correct code

--- modules/complex-number/test/test_generalov_aleksandr_complex_number.cpp
+++ modules/complex-number/test/test_generalov_aleksandr_complex_number.cpp
@@ -4,27 +4,14 @@
 #include "gtest/gtest.h"
 #include "include/complex_number.h"
 
-TEST(Generalov_Aleksandr_ComplexNumberTest, Can_Create_Zero) {
-    // Arrange
-    double re = 0.0;
-    double im = 0.0;
-
-    // Act
-    ComplexNumber z(re, im);
-
-    // Assert
-    EXPECT_EQ(re, z.getRe());
-    EXPECT_EQ(im, z.getIm());
-}
-
 TEST(Generalov_Aleksandr_ComplexNumberTest, Comparison_Operator) {
     // Arrange
     double re = 4.2;
     double im = 5.1;
     ComplexNumber* a = new ComplexNumber();
+    ComplexNumber z(re, im);
 
     // Act
-    ComplexNumber z(re, im);
     a = &z;
 
     // Assert
@@ -61,9 +48,9 @@ TEST(Generalov_Aleksandr_ComplexNumberTest, Max_Sum_Test) {
     // Arrange
     double max = std::numeric_limits<double>::max();
     double ub = std::numeric_limits<double>::infinity();
+    ComplexNumber z1(max, max);
 
     // Act
-    ComplexNumber z1(max, max);
     ComplexNumber z = z1 + z1;
 
     // Assert
@@ -75,11 +62,11 @@ TEST(Generalov_Aleksandr_ComplexNumberTest, Difference_Test) {
     // Arrange
     double max = std::numeric_limits<double>::max();
     double min = std::numeric_limits<double>::lowest();
-    double ub = std::numeric_limits<double>::infinity();
-
-    // Act
+    double ub = std::numeric_limits<double>::infinity();    
     ComplexNumber z1(max, max);
     ComplexNumber z2(min, min);
+
+    // Act
     ComplexNumber z = z1 - z2;
 
     // Assert
