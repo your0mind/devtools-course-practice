3c9835edf1bdb5e121115891a640c43912e522a1
GusevAlexandr <gusevsasha1996@mail.ru>
2018-04-07 16:13:29 +0300

    delete tab

--- modules/complex-number/test/test_gusev_alexandr_complex_number.cpp
+++ modules/complex-number/test/test_gusev_alexandr_complex_number.cpp
@@ -5,94 +5,91 @@
 #include "include/complex_number.h"
 
 TEST(Gusev_Alexandr_ComplexNumberTest, Can_Create_Zero) {
-	// Arrange
-	double re = 0.0;
-	double im = 0.0;
+  // Arrange
+  double re = 0.0;
+  double im = 0.0;
+  // Act
+  ComplexNumber z(re, im);
 
-	// Act
-	ComplexNumber z(re, im);
-
-	// Assert
-	EXPECT_EQ(re, z.getRe());
-	EXPECT_EQ(im, z.getIm());
+  // Assert
+  EXPECT_EQ(re, z.getRe());
+  EXPECT_EQ(im, z.getIm());
 }
 
 TEST(Gusev_Alexandr_ComplexNumberTest, Create_Object_Class) {
-	// Arrange
-	double re = 3.0;
-	double im = 6.3;
-	ComplexNumber* c = new ComplexNumber();
+  // Arrange
+  double re = 3.0;
+  double im = 6.3;
+  ComplexNumber* c = new ComplexNumber(); 
+  // Act
+  ComplexNumber z(re, im);
+  c = &z;
 
-	// Act
-	ComplexNumber z(re, im);
-	c = &z;
-
-	// Assert
-	EXPECT_EQ(c->getRe(), z.getRe());
-	EXPECT_EQ(c->getIm(), z.getIm());
+  // Assert
+  EXPECT_EQ(c->getRe(), z.getRe());
+  EXPECT_EQ(c->getIm(), z.getIm());
 }
 
 TEST(Gusev_Aleksandr_ComplexNumberTest, Sum_Test) {
-	// Arrange
-	double re1 = 5.1;
-	double im1 = 6.2;
-	double re2 = 3.0;
-	double im2 = 2.0;
-	ComplexNumber c1(re1, im1);
-	ComplexNumber c2(re2, im2);
-	// Act
+  // Arrange
+  double re1 = 5.1;
+  double im1 = 6.2;
+  double re2 = 3.0;
+  double im2 = 2.0;
+  ComplexNumber c1(re1, im1);
+  ComplexNumber c2(re2, im2);
+  // Act
 
-	ComplexNumber c = c1 + c2;
-
-	// Assert
-	EXPECT_EQ(c.getRe(), 8.1);
-	EXPECT_EQ(c.getIm(), 8.2);
+  ComplexNumber c = c1 + c2;
+  // Assert
+  EXPECT_EQ(c.getRe(), 8.1);
+  EXPECT_EQ(c.getIm(), 8.2);
 }
 TEST(Gusev_Aleksandr_ComplexNumberTest, Substruction) {
-	// Arrange
-	double re1 = 3.0;
-	double im1 = 5.0;
-	double re2 = 4.0;
-	double im2 = 5.0;
-	ComplexNumber c1(re1, im1);
-	ComplexNumber c2(re2, im2);
-	// Act
+  // Arrange
+  double re1 = 3.0;
+  double im1 = 5.0;
+  double re2 = 4.0;
+  double im2 = 5.0;
+  ComplexNumber c1(re1, im1);
+  ComplexNumber c2(re2, im2);
+  // Act
 
-	ComplexNumber c = c1 - c2;
+  ComplexNumber c = c1 - c2;
 
-	// Assert
-	EXPECT_EQ(c.getRe(), -1.0);
-	EXPECT_EQ(c.getIm(), 0.0);
+  // Assert
+  EXPECT_EQ(c.getRe(), -1.0);
+  EXPECT_EQ(c.getIm(), 0.0);
 }
 TEST(Gusev_Aleksandr_ComplexNumberTest, Multiplication) {
-	// Arrange
-	double re1 = 4.0;
-	double im1 = 5.0;
-	double re2 = 2.0;
-	double im2 = 3.0;
+  // Arrange 
+  double re1 = 4.0;
+  double im1 = 5.0;
+  double re2 = 2.0;
+  double im2 = 3.0;
 
-	// Act
-	ComplexNumber c1(re1, im1);
-	ComplexNumber c2(re2, im2);
-	ComplexNumber c = c1 * c2;
+  // Act
+  ComplexNumber c1(re1, im1);
+  ComplexNumber c2(re2, im2);
+  ComplexNumber c = c1 * c2;
 
-	// Assert
-	EXPECT_EQ(c.getRe(), -7.0);
-	EXPECT_EQ(c.getIm(), 22.0);
+  // Assert
+  EXPECT_EQ(c.getRe(), -7.0);
+  EXPECT_EQ(c.getIm(), 22.0);
 }
 TEST(Gusev_Aleksandr_ComplexNumberTest, Commutativity_Sum_Property) {
-	// Arrange
-	double re1 = 4.2;
-	double im1 = 5.2;
-	double re2 = 2.3;
-	double im2 = 3.3;
+  // Arrange
+  double re1 = 4.2;
+  double im1 = 5.2;
+  double re2 = 2.3;
+  double im2 = 3.3;
 
-	// Act
-	ComplexNumber c1(re1, im1);
-	ComplexNumber c2(re2, im2);
-	ComplexNumber sum12 = c1 + c2;
-	ComplexNumber sum21 = c2 + c1;
+  // Act
+  ComplexNumber c1(re1, im1);
+  ComplexNumber c2(re2, im2);
+  ComplexNumber sum12 = c1 + c2;
+  ComplexNumber sum21 = c2 + c1;
 
-	// Assert
-	ASSERT_TRUE(sum12 == sum21);
+  // Assert
+  ASSERT_TRUE(sum12 == sum21);
 }
