0a3e5e8168abb64ddb3943bde725077865d89604
Boris Ermachenko <cbkf12.ru@yandex.ru>
2018-04-09 00:04:19 +0300

    third update

--- modules/complex-number/test/test_ermachenko_boris_complex_number.cpp
+++ modules/complex-number/test/test_ermachenko_boris_complex_number.cpp
@@ -1,7 +1,7 @@
 // Copyright 2017 Korniakov Kirill
 #include <gtest/gtest.h>
 #include "include/complex_number.h"
-TEST(Ermachenko_Boris_ComplexNumberTest, can_equate_some_complexnumber){
+TEST(Ermachenko_Boris_ComplexNumberTest, can_equate_some_complexnumber) {
     // Arrange
     ComplexNumber z1(25.0, 14.0);
     ComplexNumber z2(25.1, 14.1);
@@ -12,7 +12,7 @@ TEST(Ermachenko_Boris_ComplexNumberTest, can_equate_some_complexnumber){
     // Assert
     EXPECT_TRUE(z4 == z1);
 }
-TEST(Ermachenko_Boris_ComplexNumberTest, check_correct_order_of_operations){
+TEST(Ermachenko_Boris_ComplexNumberTest, check_correct_order_of_operations) {
     // Arrange
     ComplexNumber z1(27.0, 64.0);
     ComplexNumber z2(6.0, 4.0);
@@ -25,7 +25,7 @@ TEST(Ermachenko_Boris_ComplexNumberTest, check_correct_order_of_operations){
     // Assert
     EXPECT_EQ(res1, res2);
 }
-TEST(Ermachenko_Boris_ComplexNumberTest, check_multiply_distributivel){
+TEST(Ermachenko_Boris_ComplexNumberTest, check_multiply_distributivel) {
     // Arrange
     ComplexNumber z1(27.0, 64.0);
     ComplexNumber z2(6.0, 4.0);
@@ -38,7 +38,7 @@ TEST(Ermachenko_Boris_ComplexNumberTest, check_multiply_distributivel){
     // Assert
     EXPECT_TRUE(res1 == res2);
 }
-TEST(Ermachenko_Boris_ComplexNumberTest, check_multiply_associatively){
+TEST(Ermachenko_Boris_ComplexNumberTest, check_multiply_associatively) {
     // Arrange
     ComplexNumber z1(27.0, 64.0);
     ComplexNumber z2(6.0, 4.0);
@@ -54,7 +54,7 @@ TEST(Ermachenko_Boris_ComplexNumberTest, check_multiply_associatively){
     EXPECT_TRUE(res1 == res2);
     EXPECT_TRUE(res2 == res3);
 }
-TEST(Ermachenko_Boris_ComplexNumberTest, can_multiplied_by_zero){
+TEST(Ermachenko_Boris_ComplexNumberTest, can_multiplied_by_zero) {
     // Arrange
     ComplexNumber z1(10.0, 9.0);
     ComplexNumber z2(0.0, 0.0);
