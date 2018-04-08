8b43a350eeb550b590d18f234e34c430cbb31dc5
mistakLive <mistake.live@mail.ru>
2018-04-08 10:50:12 +0400

    fix

--- modules/complex-number/test/test_shurygin_dmitriy_complex_number.cpp
+++ modules/complex-number/test/test_shurygin_dmitriy_complex_number.cpp
@@ -17,18 +17,18 @@ TEST(Shurygin_Dmitriy_ComplexNumberTest, Can_Create_Zero) {
 TEST(Shurygin_Dmitriy_ComplexNumberTest, Is_Equal) {
     ComplexNumber z1(1.0, 2.0);
     ComplexNumber z2(1.0, 2.0);
-	
+
     ComplexNumber z = z1 - z2;
-	
-    EXPECT_EQ(0, z.getRe());
+    
+	EXPECT_EQ(0, z.getRe());
     EXPECT_EQ(0, z.getIm());
 }
 
 TEST(Shurygin_Dmitriy_ComplexNumberTest, Can_Get_Im) {
     double re = 3;
     double im = -1.23;
-	
-    ComplexNumber z(re, im);
+
+	ComplexNumber z(re, im);
 
     EXPECT_EQ(im, z.getIm());
     EXPECT_EQ(re, z.getRe());
