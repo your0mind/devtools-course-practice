b837cdf81fd1dab3c602e62fa35b43e8229ba7aa
mistakLive <mistake.live@mail.ru>
2018-04-08 11:09:42 +0400

    add tests

--- modules/complex-number/test/test_shurygin_dmitriy_complex_number.cpp
+++ modules/complex-number/test/test_shurygin_dmitriy_complex_number.cpp
@@ -14,7 +14,7 @@ TEST(Shurygin_Dmitriy_ComplexNumberTest, Can_Create_Zero) {
     EXPECT_EQ(im, z.getIm());
 }
 
-TEST(Shurygin_Dmitriy_ComplexNumberTest, Is_Equal) {
+TEST(Shurygin_Dmitriy_ComplexNumberTest, dif_is_zero) {
     ComplexNumber z1(1.0, 2.0);
     ComplexNumber z2(1.0, 2.0);
 
@@ -23,7 +23,7 @@ TEST(Shurygin_Dmitriy_ComplexNumberTest, Is_Equal) {
     EXPECT_EQ(0, z.getIm());
 }
 
-TEST(Shurygin_Dmitriy_ComplexNumberTest, Can_Get_Im) {
+TEST(Shurygin_Dmitriy_ComplexNumberTest, Can_Get_Im_and_Re) {
     double re = 3;
     double im = -1.23;
 
@@ -32,3 +32,29 @@ TEST(Shurygin_Dmitriy_ComplexNumberTest, Can_Get_Im) {
     EXPECT_EQ(re, z.getRe());
 }
 
+TEST(Shurygin_Dmitriy_ComplexNumberTest, Can_Copy) {
+    double re = 3;
+    double im = -1.23;
+    
+    ComplexNumber z(re, im);
+    ComplexNumber copy_z(z);
+    
+    EXPECT_EQ(z, copy_z);
+}
+
+TEST(Shurygin_Dmitriy_ComplexNumberTest, Division_by_himself) {
+    double re = 3;
+    double im = -1.23;
+    
+    ComplexNumber z(re, im);
+	ComplexNumber z1(1.0, 0);
+	ComplexNumber res = z / z;
+    EXPECT_EQ(res, z1);
+}
+
+TEST(Shurygin_Dmitiy_ComplexNumberTest, cant_divide_zero) {
+    ComplexNumber z1(1.0, 1.0);
+    ComplexNumber z2;
+    
+    ASSERT_ANY_THROW(z1 / z2);
+}
