677aae920b2c317ee8d64b1d4c490701af279d9d
mistakLive <mistake.live@mail.ru>
2018-04-08 10:33:05 +0400

    del spaces

--- modules/complex-number/test/test_shurygin_dmitriy_complex_number.cpp
+++ modules/complex-number/test/test_shurygin_dmitriy_complex_number.cpp
@@ -5,31 +5,31 @@
 #include "include/complex_number.h"
 
 TEST(Shurygin_Dmitriy_ComplexNumberTest, Can_Create_Zero) {
-	double re = 0.0;
-	double im = 0.0;
+    double re = 0.0;
+    double im = 0.0;
 
-	ComplexNumber z(re, im);
+    ComplexNumber z(re, im);
 
-	EXPECT_EQ(re, z.getRe());
-	EXPECT_EQ(im, z.getIm());
+    EXPECT_EQ(re, z.getRe());
+    EXPECT_EQ(im, z.getIm());
 }
 
 TEST(Shurygin_Dmitriy_ComplexNumberTest, Is_Equal) {
-	ComplexNumber z1(1.0, 2.0);
-	ComplexNumber z2(1.0, 2.0);
+    ComplexNumber z1(1.0, 2.0);
+    ComplexNumber z2(1.0, 2.0);
 	
-	ComplexNumber z = z1 - z2;
+    ComplexNumber z = z1 - z2;
 	
-	EXPECT_EQ(0, z.getRe());
-	EXPECT_EQ(0, z.getIm());
+    EXPECT_EQ(0, z.getRe());
+    EXPECT_EQ(0, z.getIm());
 }
 
 TEST(Shurygin_Dmitriy_ComplexNumberTest, Can_Get_Im) {
-	double re = 3;
-	double im = -1.23;
+    double re = 3;
+    double im = -1.23;
 	
-	ComplexNumber z(re, im);
+    ComplexNumber z(re, im);
 
-	EXPECT_EQ(im, z.getIm());
+    EXPECT_EQ(im, z.getIm());
 }
 
