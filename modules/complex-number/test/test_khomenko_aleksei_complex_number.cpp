2ca8b66bcc5e6912a25c4b38c8ae5756c3bd0658
Alexey Khomenko <hom.ksei@gmail.com>
2018-04-08 11:05:20 +0300

    fix section

--- modules/complex-number/test/test_khomenko_aleksei_complex_number.cpp
+++ modules/complex-number/test/test_khomenko_aleksei_complex_number.cpp
@@ -8,21 +8,19 @@ TEST(Khomenko_Aleksei_ComplexNumberTest, Can_Get_Re_from_Im) {
     // Arrange
     double re = -1.0;
     double im = 1.0;
+    ComplexNumber z(0.0, im);
 
     // Act
-    ComplexNumber z(0.0, im);
-    ComplexNumber z1 = z*z;
+   ComplexNumber z1 = z*z;
 
     // Assert
     EXPECT_NEAR(re, z1.getRe(), 0.001);
 }
 
 TEST(Khomenko_Aleksei_ComplexNumberTest, Can_Substruct_Const_At_Init) {
-    // Arrange
+    // Arrange & Act
     double re = 20.0;
     double im = 25.0;
-
-    // Act
     ComplexNumber z(re, im);
     ComplexNumber z1(re-5, im-5);
 
@@ -33,12 +31,10 @@ TEST(Khomenko_Aleksei_ComplexNumberTest, Can_Substruct_Const_At_Init) {
 
 TEST(Khomenko_Aleksei_ComplexNumberTest, Can_Add_Zero) {
     // Arrange
-    double re = 20.0;
-    double im = 25.0;
+    ComplexNumber z(20.0, 25.0);
+    ComplexNumber z1(0.0, 0.0);
 
     // Act
-    ComplexNumber z(re, im);
-    ComplexNumber z1(0.0, 0.0);
     ComplexNumber z_res = z + z1;
 
     // Assert
@@ -47,12 +43,10 @@ TEST(Khomenko_Aleksei_ComplexNumberTest, Can_Add_Zero) {
 
 TEST(Khomenko_Aleksei_ComplexNumberTest, Can_Mult_by_Neutral_Ele) {
     // Arrange
-    double re = 20.0;
-    double im = 25.0;
+    ComplexNumber z(20.0, 25.0);
+    ComplexNumber z1(1.0, 0.0);
 
     // Act
-    ComplexNumber z(re, im);
-    ComplexNumber z1(1.0, 0.0);
     ComplexNumber z_res = z * z1;
 
     // Assert
@@ -63,9 +57,9 @@ TEST(Khomenko_Aleksei_ComplexNumberTest, Can_check_the_Distribution) {
     // Arrange
     double re = 20.0;
     double im = 20.0;
+    ComplexNumber z1(re, im), z2(re, im), z3(re, im);
 
     // Act
-    ComplexNumber z1(re, im), z2(re, im), z3(re, im);
     ComplexNumber z_res1 = z1*(z2+z3);
     ComplexNumber z_res2 = z1*z2+z1*z3;
 
