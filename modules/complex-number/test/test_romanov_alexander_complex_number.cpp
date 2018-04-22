6c9d13beee4820007703ec547f2918fe92ad173b
Alexander Romanov <al.romanov2012@yandex.ru>
2018-04-22 19:37:37 +0300

    Reworked tests impl

--- modules/complex-number/test/test_romanov_alexander_complex_number.cpp
+++ modules/complex-number/test/test_romanov_alexander_complex_number.cpp
@@ -5,27 +5,36 @@
 #include "include/complex_number.h"
 
 TEST(Romanov_Alexander_ComplexNumberTest, Can_init_complex_number) {
-    ComplexNumber z(1.0, -1.0);
+    double re = 1.0;
+    double im = -1.0;
+
+    ComplexNumber z(re, im);
+
     EXPECT_EQ(z.getRe(), 1.0);
     EXPECT_EQ(z.getIm(), -1.0);
 }
 TEST(Romanov_Alexander_ComplexNumberTest, cant_divide_zero) {
     ComplexNumber z1(1.0, 1.0);
     ComplexNumber z2;
-    ASSERT_ANY_THROW(z1 / z2);
+    
+    EXPECT_ANY_THROW(z1 / z2);
 }
 TEST(Romanov_Alexander_ComplexNumberTest, able_equal_copying) {
     ComplexNumber z1(1.0, 2.0);
     ComplexNumber z2 = z1;
-    ASSERT_EQ(z1, z2);
+
+    EXPECT_EQ(z2.getRe(), 1.0);
+    EXPECT_EQ(z2.getIm(), 2.0);
 }
 TEST(Romanov_Alexander_ComplexNumberTest, able_distribution_law) {
     ComplexNumber z1(1.0, 1.0);
     ComplexNumber z2(2.0, 2.0);
     ComplexNumber z3(3.0, 3.0);
+
     ComplexNumber res1 = z1 * (z2 + z3);
     ComplexNumber res2 = z1 * z2 + z1 * z3;
-    EXPECT_TRUE(res1 == res2);
+
+    EXPECT_EQ(res1, res2);
 }
 TEST(Romanov_Alexander_ComplexNumberTest, can_count_correctly) {
     ComplexNumber z1(1.0, 2.0);
@@ -33,5 +42,6 @@ TEST(Romanov_Alexander_ComplexNumberTest, can_count_correctly) {
     ComplexNumber z3(2.0, 2.0);
     ComplexNumber z4(1.0, 0.0);
     ComplexNumber res(6.0, 6.0);
-    ASSERT_EQ(res, (z1 + z2) * z3 / z4);
+
+    EXPECT_EQ(res, (z1 + z2) * z3 / z4);
 }
