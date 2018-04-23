36a6911efda2787082601c2188c7b4a33d954aa0
heriou <38137582+heriou@users.noreply.github.com>
2018-04-23 19:59:23 +0300

    Update test_kozorez_alexander_complex_number.cpp

--- modules/complex-number/test/test_kozorez_alexander_complex_number.cpp
+++ modules/complex-number/test/test_kozorez_alexander_complex_number.cpp
@@ -1,40 +1,56 @@
 // Copyright 2018 Kozorez Alexander
+
 #include <gtest/gtest.h>
+
 #include "include/complex_number.h"
+
 TEST(Kozorez_Alex_ComplexNumberTest, 
      complex_plus_zero_is_equal_to_this_complex){
-     
+    
+    // Act
     ComplexNumber z(666.0, 22.0), z1(0.0,0.0);
     ComplexNumber sum = z + z1;
-     
+    
+    // Assert 
     EXPECT_EQ(z, sum);
 }
 
 TEST(Kozorez_Alex_ComplexNumberTest, multi_by_zero_is_zero) {
-     
+    // Arrange 
     ComplexNumber z(666.0, 22.0), z1(0.0, 0.0);
-    ComplexNumber multi = z * z1;
     ComplexNumber zero(0.0, 0.0);
      
+    // Act
+    ComplexNumber multi = z * z1;
+    
+    // Assert
     EXPECT_EQ(multi, zero);
 }
 
 TEST(Kozorez_Alex_ComplexNumberTest, sum_commutication) {
+    // Arrange 
     ComplexNumber z(66.0, 22.0), z1(-66.0, -22.0);
      
+    // Assert 
     EXPECT_EQ(z+z1, z1+z);
 }
 
 TEST(Kozorez_Alex_ComplexNumberTest, sum_of_equals_by_abs_is_zero) {
+    // Arrange 
     ComplexNumber z(666.0, 22.0), z1(-666.0, -22.0);
-    ComplexNumber zero1 = z + z1;
     ComplexNumber zero(0.0, 0.0);
-     
+    
+    // Act
+    ComplexNumber zero1 = z + z1;
+    
+    // Assert 
     EXPECT_EQ(zero, zero1);
 }
 
 TEST(Kozorez_Alex_ComplexNumberTest, multiply_comm) {
+    // Arrange 
     ComplexNumber z(66.0, 22.0), z1(-66.0, -22.0);
      
+    // Assert 
     EXPECT_EQ(z*z1, z1*z);
 }
