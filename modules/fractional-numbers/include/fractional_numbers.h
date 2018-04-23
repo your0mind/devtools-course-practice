fa293ece27373f19dce2f1f1e778fd56bb863cfc
Alexey Khomenko <hom.ksei@gmail.com>
2018-04-23 18:47:51 +0300

    new style fix

--- modules/fractional-numbers/include/fractional_numbers.h
+++ modules/fractional-numbers/include/fractional_numbers.h
@@ -9,26 +9,26 @@
 #ifndef MODULES_FRACTIONAL_NUMBERS_INCLUDE_FRACTIONAL_NUMBERS_H_
 #define MODULES_FRACTIONAL_NUMBERS_INCLUDE_FRACTIONAL_NUMBERS_H_
 
-class Rational {
-public:
+ class Rational {
+ public:
 Rational();
 Rational(const int chislit, const int znamenat);
 Rational(const Rational& r);
-    
+
 Rational operator + (const Rational& s) const;
 Rational operator - (const Rational& s) const;
 Rational operator * (const Rational& s) const;
 Rational operator / (const Rational& s) const;
-    
+
 Rational& operator= (const Rational& s);
 bool operator == (const Rational& r) const;
 int getChislitel() const;
 int getZnamenatel() const;
 void setChislitel(const int chislitel);
 void setZnamenatel(const int znamenatel);
-    
-private:
-int Nod (int, int) const;
+
+ private:
+int Nod(int, int) const;
 int a, b;  // a-числитель b-знаменатель
 };
 
