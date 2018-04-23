4baff0376447bc5495339b91d9c98537b442fcf7
Alexey Khomenko <hom.ksei@gmail.com>
2018-04-23 18:43:48 +0300

    fix style

--- modules/fractional-numbers/include/fractional_numbers.h
+++ modules/fractional-numbers/include/fractional_numbers.h
@@ -1,5 +1,5 @@
 //
-//  fractional_numbers.hpp
+//  fractional_numbers.h
 //  fractional_numbers
 //
 //  Created by Alexey Khomenko on 23.04.18.
@@ -10,26 +10,26 @@
 #define MODULES_FRACTIONAL_NUMBERS_INCLUDE_FRACTIONAL_NUMBERS_H_
 
 class Rational {
- public:
-   Rational();
-   Rational(const int chislit, const int znamenat);
-   Rational(const Rational& r);
+public:
+Rational();
+Rational(const int chislit, const int znamenat);
+Rational(const Rational& r);
     
-   Rational operator + (const Rational& s) const;
-   Rational operator - (const Rational& s) const;
-   Rational operator * (const Rational& s) const;
-   Rational operator / (const Rational& s) const;
+Rational operator + (const Rational& s) const;
+Rational operator - (const Rational& s) const;
+Rational operator * (const Rational& s) const;
+Rational operator / (const Rational& s) const;
     
-   Rational& operator= (const Rational& s);
-   bool operator == (const Rational& r) const;
-   int getChislitel() const;
-   int getZnamenatel() const;
-   void setChislitel(const int chislitel);
-   void setZnamenatel(const int znamenatel);
+Rational& operator= (const Rational& s);
+bool operator == (const Rational& r) const;
+int getChislitel() const;
+int getZnamenatel() const;
+void setChislitel(const int chislitel);
+void setZnamenatel(const int znamenatel);
     
 private:
-   int Nod (int, int) const;
-   int a,b; // a-числитель b-знаменатель
+int Nod (int, int) const;
+int a, b;  // a-числитель b-знаменатель
 };
 
-#endif /* MODULES_FRACTIONAL_NUMBERS_INCLUDE_FRACTIONAL_NUMBERS_H_ */
+#endif  // MODULES_FRACTIONAL_NUMBERS_INCLUDE_FRACTIONAL_NUMBERS_H_
