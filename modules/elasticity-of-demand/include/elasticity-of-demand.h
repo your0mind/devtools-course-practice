34fa8c5315afaa6c6813df403e11dad91d4315da
Oleg <ogretoleg@yandex.ru>
2018-04-24 19:11:49 +0300

    create empty

--- modules/elasticity-of-demand/include/elasticity-of-demand.h
+++ modules/elasticity-of-demand/include/elasticity-of-demand.h
@@ -1,33 +1,10 @@
 // Copyright 2018 Lalykin Oleg
 
-#ifndef MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_NUMBER_H_
-#define MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_NUMBER_H_
+#ifndef MODULES_ELASTICITY_OF_DEMAND_INCLUDE_ELASTICITY_OF_DEMAND_H_
+#define MODULES_ELASTICITY_OF_DEMAND_INCLUDE_ELASTICITY_OF_DEMAND_H_
 
-class ComplexNumber {
- public:
-    ComplexNumber();
-    ComplexNumber(const double real, const double imaginary);
-    ComplexNumber(const ComplexNumber& z);
+class Elasticity {
 
-    ComplexNumber& operator=(const ComplexNumber& z);
+}
 
-    double getRe() const;
-    double getIm() const;
-    void setRe(const double real);
-    void setIm(const double imaginary);
-
-    ComplexNumber operator + (const ComplexNumber& z) const;
-    ComplexNumber operator - (const ComplexNumber& z) const;
-    ComplexNumber operator * (const ComplexNumber& z) const;
-    ComplexNumber operator / (const ComplexNumber& z) const;
-
-    bool operator == (const ComplexNumber& z) const;
-    bool operator != (const ComplexNumber& z) const;
-
- private:
-    bool equalsZero(const ComplexNumber& z) const;
-    double real_;
-    double imaginary_;
-};
-
-#endif  // MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_NUMBER_H_
+#endif  //  MODULES_ELASTICITY_OF_DEMAND_INCLUDE_ELASTICITY_OF_DEMAND_H_
