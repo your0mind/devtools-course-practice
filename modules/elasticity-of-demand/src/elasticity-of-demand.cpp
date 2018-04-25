1cf0375d955f6dcb4213b943e7cc1cf81a09975a
Oleg <ogretoleg@yandex.ru>
2018-04-25 22:33:08 +0300

    lines, spaces in cpp

--- modules/elasticity-of-demand/src/elasticity-of-demand.cpp
+++ modules/elasticity-of-demand/src/elasticity-of-demand.cpp
@@ -11,25 +11,34 @@ Elasticity::Elasticity(): coeff(0), ans("") {}
 double Elasticity::GetCoeff() { return coeff; }
 std::string Elasticity::GetAns() { return ans; }
 
-double Elasticity::PriceElasticity(double Q1, double Q2, double P1, double P2) {
-    // Q1 and Q2 — initial and current volume of demand; 
-    //P1 and Ð2 — initial and current price
+double Elasticity::PriceElasticity(double Q1,
+    double Q2,
+    double P1,
+    double P2) {
+    // Q1 and Q2 - initial and current volume of demand;
+    // P1 and P2 - initial and current price
     coeff =
     fabs(((Q2 - Q1)/ (Q2 + Q1)) *
     ((P2 + P1)/(P2 - P1)));
     return coeff;
 }
-double Elasticity::IncomeElasticity(double Q1, double Q2, double Y1, double Y2) {
+double Elasticity::IncomeElasticity(double Q1,
+    double Q2, 
+    double Y1, 
+    double Y2) {
      // Q1 and Q2 - initial and new volumes of demand;
-     //Y1 and Y2 - initial and new income levels
+     // Y1 and Y2 - initial and new income levels
      coeff =
     ((Q2 - Q1) / (Q2 + Q1)) *
     ((Y2 + Y1) / (Y2 - Y1));
     return coeff;
 }
-double Elasticity::CrossElasticity(double Q1X, double Q2X, double P1Y, double P2Y) {
+double Elasticity::CrossElasticity(double Q1X,
+    double Q2X,
+    double P1Y,
+    double P2Y) {
     // Q1X and Q2X - initial and new volumes of demand for goods X; 
-    //P2Y and P1Y - initial and new price of goods Y
+    // P2Y and P1Y - initial and new price of goods Y
     coeff =
     ((Q2X - Q1X) / (Q2X + Q1X)) *
     ((P2Y + P1Y) / (P2Y - P1Y));
