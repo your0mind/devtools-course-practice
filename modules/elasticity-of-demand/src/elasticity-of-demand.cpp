7255f160dd5d9dc25d152e67c126b4c1521c1264
Oleg <ogretoleg@yandex.ru>
2018-04-25 21:49:06 +0300

    fix formats in cpp

--- modules/elasticity-of-demand/src/elasticity-of-demand.cpp
+++ modules/elasticity-of-demand/src/elasticity-of-demand.cpp
@@ -2,47 +2,46 @@
 
 #include "include/elasticity-of-demand.h"
 #include <math.h>
-Elasticity::Elasticity(): coeff(0),ans("") {}
+Elasticity::Elasticity(): coeff(0), ans("") {}
 
 
 double Elasticity::GetCoeff() { return coeff; }
 std::string Elasticity::GetAns() { return ans; }
 
-// коэффициент ценовой эластичности спро≠са
-double Elasticity::PriceElasticity(double Q1, double Q2, double P1, double P2)
-{
-    //Q1 и Q2 Ч первоначальный и текущий объем спроса; P1 и –2 Ч первоначальна€ и текуща€ цена
-    coeff = fabs(((Q2 - Q1)/ (Q2 + Q1) ) * ((P2 + P1)/(P2 - P1)));
+
+double Elasticity::PriceElasticity(double Q1, double Q2, double P1, double P2) {
+    //Q1 and Q2 Ч initial and current volume of demand; P1 and –2 Ч initial and current price
+    coeff = fabs(
+    ((Q2 - Q1)/ (Q2 + Q1) ) * 
+    ((P2 + P1)/(P2 - P1))
+    );
     return coeff;
 };
-// коэффициент эластичности спроса по доходу
-double Elasticity::IncomeElasticity(double Q1, double Q2, double Y1, double Y2)
-{
-   //Q1 и Q2 Ч первоначальный и новый объемы спроса; Y1и Y2 Ч первоначальный и новый уровни дохода
-   coeff = ((Q2 - Q1) / (Q2 + Q1)) * ((Y2 + Y1) / (Y2 - Y1));
+double Elasticity::IncomeElasticity(double Q1, double Q2, double Y1, double Y2) {
+   //Q1 and Q2 - initial and new volumes of demand; Y1 and Y2 - initial and new income levels
+   coeff = 
+    ((Q2 - Q1) / (Q2 + Q1)) *
+     ((Y2 + Y1) / (Y2 - Y1));
    return coeff;
 };
-// коэффициент перекрестной эластичности
-double Elasticity::CrossElasticity(double Q1X, double Q2X, double P1Y, double P2Y)
-{
-   //Q1X и Q2X Ч первоначальный и новый объемы спроса на то≠вар ’; –2Y и –1Y Ч первоначальна€ и нова€ цена товара Y
-   coeff = ((Q2X - Q1X) / (Q2X + Q1X)) * ((P2Y + P1Y) / (P2Y - P1Y));
+double Elasticity::CrossElasticity(double Q1X, double Q2X, double P1Y, double P2Y) {
+    //Q1X and Q2X - initial and new volumes of demand for goods X; P2Y and P1Y - initial and new price of goods Y
+    coeff = 
+    ((Q2X - Q1X) / (Q2X + Q1X)) *
+    ((P2Y + P1Y) / (P2Y - P1Y));
    return coeff;
 };
-void Elasticity::PriceAnswer()
-{
-    if (coeff > 1) ans = "demand is elastic";//спрос эластичен
-    if (coeff < 1) ans = "demand is inelastic";//спрос неэластичен
-    if (coeff == 1) ans = "unit elasticity";//единична€ эластичность
+void Elasticity::PriceAnswer() {
+    if (coeff > 1) ans = "demand is elastic";
+    if (coeff < 1) ans = "demand is inelastic";
+    if (coeff == 1) ans = "unit elasticity";
 }
-void Elasticity::IncomeAnswer()
-{
-   if (coeff > 0) ans = "normal goods";//нормальные товары
-   if (coeff <= 0) ans = "inferior goods";//товары низ≠шей категории  
+void Elasticity::IncomeAnswer() {
+   if (coeff > 0) ans = "normal goods";
+   if (coeff <= 0) ans = "inferior goods";
 }
-void Elasticity::CrossAnswer()
-{
-   if (coeff > 0) ans = "goods are interchangeable";//товары взаимозамен€емы
-   if (coeff < 0) ans = "mutually complementary goods";//взаимодополн€ющие друг друга товары
-   if (coeff == 0) ans = "goods independent of each other";//независимыми друг от друга товары
+void Elasticity::CrossAnswer() {
+   if (coeff > 0) ans = "goods are interchangeable";
+   if (coeff < 0) ans = "mutually complementary goods";
+   if (coeff == 0) ans = "goods independent of each other";
 }
