5b9477c8e44e5e49d0b74423b2953c881c06bb38
Oleg <ogretoleg@yandex.ru>
2018-04-25 20:31:31 +0300

    fix format code

--- modules/elasticity-of-demand/include/elasticity-of-demand.h
+++ modules/elasticity-of-demand/include/elasticity-of-demand.h
@@ -4,35 +4,27 @@
 #define MODULES_ELASTICITY_OF_DEMAND_INCLUDE_ELASTICITY_OF_DEMAND_H_
 
 #include <string>
-class Elasticity {
+
+class Elasticity 
+{
 public:
+
     Elasticity();
-
-    
-    
-    double PriceElasticity(double Q1,double Q2,double P1,double P2);//коэффициент ценовой эластичности спро­са
-    double IncomeElasticity(double Q1, double Q2, double Y1, double Y2);//коэффициент эластичности спроса по доходу
-    double CrossElasticity(double Q1X, double Q2X, double P1Y, double P2Y);//коэффициент перекрестной эластичности
-
-
+    //коэффициент ценовой эластичности спро­са
+    double PriceElasticity(double Q1, double Q2, double P1, double P2);
+    //коэффициент эластичности спроса по доходу
+     double IncomeElasticity(double Q1, double Q2, double Y1, double Y2);
+    //коэффициент перекрестной эластичности
+     double CrossElasticity(double Q1X, double Q2X, double P1Y, double P2Y);
     void PriceAnswer();
     void IncomeAnswer();
     void CrossAnswer();
-    
-    //double GetQ1();
-    //double GetQ2();
-    //double GetP1();
-    //double GetP2();
-double GetCoeff();
-std::string GetAns();
-    //void SetQ1(const double _Q1);
-    //void SetQ2(const double _Q2);
-    //void SetP1(const double _P1);
-    //void SetP2(const double _P2);
+    double GetCoeff();
+    std::string GetAns();
+
 private:
+
     double coeff;
     std::string ans;
-
 };
-
 #endif  //  MODULES_ELASTICITY_OF_DEMAND_INCLUDE_ELASTICITY_OF_DEMAND_H_
