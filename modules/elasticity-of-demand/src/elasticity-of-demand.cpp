3ac78f03a36d7a3b28b7483a83e98ba375125ac4
Oleg <ogretoleg@yandex.ru>
2018-04-25 22:39:35 +0300

    line ends in cpp

--- modules/elasticity-of-demand/src/elasticity-of-demand.cpp
+++ modules/elasticity-of-demand/src/elasticity-of-demand.cpp
@@ -23,8 +23,8 @@ double Elasticity::PriceElasticity(double Q1,
     return coeff;
 }
 double Elasticity::IncomeElasticity(double Q1,
-    double Q2, 
-    double Y1, 
+    double Q2,
+    double Y1,
     double Y2) {
      // Q1 and Q2 - initial and new volumes of demand;
      // Y1 and Y2 - initial and new income levels
@@ -37,7 +37,7 @@ double Elasticity::CrossElasticity(double Q1X,
     double Q2X,
     double P1Y,
     double P2Y) {
-    // Q1X and Q2X - initial and new volumes of demand for goods X; 
+    // Q1X and Q2X - initial and new volumes of demand for goods X;
     // P2Y and P1Y - initial and new price of goods Y
     coeff =
     ((Q2X - Q1X) / (Q2X + Q1X)) *
