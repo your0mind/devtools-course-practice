fafaf8afab78cdd338511b3a352b713ca00a18f5
KST <tarakan.php@yandex.ru>
2018-04-26 18:24:09 +0300

    fix tab

--- modules/modify-steck/test/test_ModifySteck.cpp
+++ modules/modify-steck/test/test_ModifySteck.cpp
@@ -4,87 +4,87 @@
 #include "include/Modify_Steck.h"
 
 TEST(Tarakanov_Kirill_ModifySteckTest, Can_Create_ModifySteck) {
-	// Arrange
+    // Arrange
 
-	// Act
+    // Act
 
-	// Assert
-	ASSERT_NO_THROW(ModifySteck<int>());
+    // Assert
+    ASSERT_NO_THROW(ModifySteck<int>());
 }
 
 TEST(Tarakanov_Kirill_ModifySteckTest, ModifySteck_Can_Push_Val) {
-	// Arrange
-	ModifySteck<int> *steck = new ModifySteck<int>();
+    // Arrange
+    ModifySteck<int> *steck = new ModifySteck<int>();
 
-	// Act
+    // Act
 
-	// Assert
-	ASSERT_NO_THROW(steck->Push(10));
+    // Assert
+    ASSERT_NO_THROW(steck->Push(10));
 }
 
 TEST(Tarakanov_Kirill_ModifySteckTest, ModifySteck_Is_Empty) {
-	// Arrange
-	ModifySteck<int> *steck = new ModifySteck<int>();
+    // Arrange
+    ModifySteck<int> *steck = new ModifySteck<int>();
 
-	// Act
-	steck->Push(10);
-	bool empty = steck->IsEmpty();
+    // Act
+    steck->Push(10);
+    bool empty = steck->IsEmpty();
 
-	// Assert
-	EXPECT_EQ(empty, 0);
+    // Assert
+    EXPECT_EQ(empty, 0);
 }
 
 TEST(Tarakanov_Kirill_ModifySteckTest, ModifySteck_Can_LookTop) {
-	// Arrange
-	ModifySteck<int> *steck = new ModifySteck<int>();
+    // Arrange
+    ModifySteck<int> *steck = new ModifySteck<int>();
 
-	// Act
-	steck->Push(10);
-	steck->Push(20);
-	int onTop = steck->LookTop();
+    // Act
+    steck->Push(10);
+    steck->Push(20);
+    int onTop = steck->LookTop();
 
-	// Assert
-	EXPECT_EQ(onTop, 20);
+    // Assert
+    EXPECT_EQ(onTop, 20);
 }
 
 TEST(Tarakanov_Kirill_ModifySteckTest, ModifySteck_Can_Pop) {
-	// Arrange
-	ModifySteck<int> *steck = new ModifySteck<int>();
+    // Arrange
+    ModifySteck<int> *steck = new ModifySteck<int>();
 
-	// Act
-	steck->Push(10);
-	steck->Push(-5);
-	steck->Pop();
-	int onTop = steck->LookTop();
+    // Act
+    steck->Push(10);
+    steck->Push(-5);
+    steck->Pop();
+    int onTop = steck->LookTop();
 
-	// Assert
-	EXPECT_EQ(onTop, 10);
+    // Assert
+    EXPECT_EQ(onTop, 10);
 }
 
 TEST(Tarakanov_Kirill_ModifySteckTest, ModifySteck_Can_KnewSize) {
-	// Arrange
-	ModifySteck<int> *steck = new ModifySteck<int>();
+    // Arrange
+    ModifySteck<int> *steck = new ModifySteck<int>();
 
-	// Act
-	steck->Push(10);
-	steck->Push(-5);
-	steck->Push(4);
-	steck->Push(0);
-	steck->Push(3);
+    // Act
+    steck->Push(10);
+    steck->Push(-5);
+    steck->Push(4);
+    steck->Push(0);
+    steck->Push(3);
 
-	// Assert
-	EXPECT_EQ(steck->Size(), 5);
+    // Assert
+    EXPECT_EQ(steck->Size(), 5);
 }
 
 TEST(Tarakanov_Kirill_ModifySteckTest, ModifySteck_Can_FindMin) {
-	// Arrange
-	ModifySteck<int> *steck = new ModifySteck<int>();
+    // Arrange
+    ModifySteck<int> *steck = new ModifySteck<int>();
 
-	// Act
-	steck->Push(10);
-	steck->Push(20);
-	int onTop = steck->FindMin();
+    // Act
+    steck->Push(10);
+    steck->Push(20);
+    int onTop = steck->FindMin();
 
-	// Assert
-	EXPECT_EQ(onTop, 10);
+    // Assert
+    EXPECT_EQ(onTop, 10);
 }
--- modules/modify-steck/test/test_Steck.cpp
+++ modules/modify-steck/test/test_Steck.cpp
@@ -4,104 +4,103 @@
 #include "include/Steck.h"
 
 TEST(Tarakanov_Kirill_SteckTest, Can_Create_Node_withINT) {
-	// Arrange
+    // Arrange
 	
-	// Act
+    // Act
 
-	// Assert
-	ASSERT_NO_THROW(Node<int>());
+    // Assert
+    ASSERT_NO_THROW(Node<int>());
 }
 
 TEST(Tarakanov_Kirill_SteckTest, Can_Create_Node_withFLOAT) {
-	// Arrange
+    // Arrange
 
-	// Act
+    // Act
 
-	// Assert
-	ASSERT_NO_THROW(Node<float>());
+    // Assert
+    ASSERT_NO_THROW(Node<float>());
 }
 
 TEST(Tarakanov_Kirill_SteckTest, Can_Create_Node_withParametrs) {
-	// Arrange
-	int val = 10;
-	Node<int> _node(val, 0);
+    // Arrange
+    int val = 10;
+    Node<int> _node(val, 0);
 
-	// Act
+    // Act
 
-	// Assert
-	ASSERT_NO_THROW(Node<int>(6, &_node));
+    // Assert
+    ASSERT_NO_THROW(Node<int>(6, &_node));
 }
 
 TEST(Tarakanov_Kirill_SteckTest, Can_Create_Steck) {
-	// Arrange
+    // Arrange
 	
-	// Act
+    // Act
 
-	// Assert
-	ASSERT_NO_THROW(Steck<int>());
+    // Assert
+    ASSERT_NO_THROW(Steck<int>());
 }
 
 TEST(Tarakanov_Kirill_SteckTest, Steck_Can_Push_Val) {
-	// Arrange
-	Steck<int> *steck = new Steck<int>();
+    // Arrange
+    Steck<int> *steck = new Steck<int>();
 
-	// Act
+    // Act
 	
-	// Assert
-	ASSERT_NO_THROW(steck->Push(10));
+    // Assert
+    ASSERT_NO_THROW(steck->Push(10));
 }
 
 TEST(Tarakanov_Kirill_SteckTest, Steck_Is_Empty) {
-	// Arrange
-	Steck<int> *steck = new Steck<int>();
+    // Arrange
+    Steck<int> *steck = new Steck<int>();
 
-	// Act
-	steck->Push(10);
-	bool empty = steck->IsEmpty();
+    // Act
+    steck->Push(10);
+    bool empty = steck->IsEmpty();
 
-	// Assert
-	EXPECT_EQ(empty, 0);
+    // Assert
+    EXPECT_EQ(empty, 0);
 }
 
 TEST(Tarakanov_Kirill_SteckTest, Steck_Can_LookTop) {
-	// Arrange
-	Steck<int> *steck = new Steck<int>();
+    // Arrange
+    Steck<int> *steck = new Steck<int>();
 
-	// Act
-	steck->Push(10);
-	steck->Push(20);
-	int onTop = steck->LookTop();
+    // Act
+    steck->Push(10);
+    steck->Push(20);
+    int onTop = steck->LookTop();
 
-	// Assert
-	EXPECT_EQ(onTop, 20);
+    // Assert
+    EXPECT_EQ(onTop, 20);
 }
 
 TEST(Tarakanov_Kirill_SteckTest, Steck_Can_Pop) {
-	// Arrange
-	Steck<int> *steck = new Steck<int>();
+    // Arrange
+    Steck<int> *steck = new Steck<int>();
 
-	// Act
-	steck->Push(10);
-	steck->Push(-5);
-	steck->Pop();
-	int onTop = steck->LookTop();
+    // Act
+    steck->Push(10);
+    steck->Push(-5);
+    steck->Pop();
+    int onTop = steck->LookTop();
 
-	// Assert
-	EXPECT_EQ(onTop, 10);
+    // Assert
+    EXPECT_EQ(onTop, 10);
 }
 
 TEST(Tarakanov_Kirill_SteckTest, Steck_Can_KnewSize) {
-	// Arrange
-	Steck<int> *steck = new Steck<int>();
+    // Arrange
+    Steck<int> *steck = new Steck<int>();
 
-	// Act
-	steck->Push(10);
-	steck->Push(-5);
-	steck->Push(4);
-	steck->Push(0);
-	steck->Push(3);
+    // Act
+    steck->Push(10);
+    steck->Push(-5);
+    steck->Push(4);
+    steck->Push(0);
+    steck->Push(3);
 
-	// Assert
-	EXPECT_EQ(steck->Size(), 5);
+    // Assert
+    EXPECT_EQ(steck->Size(), 5);
 }
-
