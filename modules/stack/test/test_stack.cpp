c2178c60bca4bf1f56e63426b594a5a08b88791b
Alexey Gladyshev <wotpricol@mail.ru>
2018-04-26 13:38:25 +0300

    Fix cpplint

--- modules/stack/test/test_stack.cpp
+++ modules/stack/test/test_stack.cpp
@@ -11,10 +11,10 @@ TEST(Gladyshev_Alexey_StackTest, Always_Correct_Test) {
     int hello = 1;
     int world = 1;
     int assert = 0;
-    
+
     // Act
     assert = hello + world;
-    
+
     // Assert
     EXPECT_EQ(assert, 2);
 }
@@ -23,10 +23,10 @@ TEST(Gladyshev_Alexey_StackTest, Can_Create_Stack_With_My_Size) {
     // Arrange
     TStack<int> stack(47);
     int size = 0;
-    
+
     // Act
     size = stack.GetSize();
-    
+
     // Assert
     EXPECT_EQ(size, 47);
 }
@@ -34,10 +34,10 @@ TEST(Gladyshev_Alexey_StackTest, Can_Create_Stack_With_My_Size) {
 TEST(Gladyshev_Alexey_StackTest, Can_Create_Copy_Stack) {
     // Arrange
     TStack<int> stack(47);
-    
+
     // Act
     TStack<int> copyStack(stack);
-    
+
     // Assert
     EXPECT_EQ(stack.GetSize(), copyStack.GetSize());
 }
@@ -45,10 +45,10 @@ TEST(Gladyshev_Alexey_StackTest, Can_Create_Copy_Stack) {
 TEST(Gladyshev_Alexey_StackTest, Can_Push_Value_On_Stack) {
     // Arrange
     TStack<int> stack(47);
-    
+
     // Act
     stack.Push(47);
-    
+
     // Assert
     EXPECT_EQ(stack.GetSP(), 1);
 }
@@ -56,10 +56,10 @@ TEST(Gladyshev_Alexey_StackTest, Can_Push_Value_On_Stack) {
 TEST(Gladyshev_Alexey_StackTest, Can_Pop_Value_From_Stack) {
     // Arrange
     TStack<int> stack(47);
-    
+
     // Act
     stack.Push(47);
-    
+
     // Assert
     EXPECT_EQ(stack.Pop(), 47);
 }
@@ -67,10 +67,10 @@ TEST(Gladyshev_Alexey_StackTest, Can_Pop_Value_From_Stack) {
 TEST(Gladyshev_Alexey_StackTest, Can_See_Value_From_Top_Of_Stack) {
     // Arrange
     TStack<int> stack(47);
-    
+
     // Act
     stack.Push(47);
-    
+
     // Assert
     EXPECT_EQ(stack.Top(), 47);
     EXPECT_EQ(stack.GetSP(), 1);
@@ -79,7 +79,7 @@ TEST(Gladyshev_Alexey_StackTest, Can_See_Value_From_Top_Of_Stack) {
 TEST(Gladyshev_Alexey_StackTest, Can_Create_Empty_Stack) {
     // Arrange & Act
     TStack<int> stack(47);
-    
+
     // Assert
     EXPECT_TRUE(stack.IsEmpty() == true);
 }
@@ -87,11 +87,11 @@ TEST(Gladyshev_Alexey_StackTest, Can_Create_Empty_Stack) {
 TEST(Gladyshev_Alexey_StackTest, Can_Create_Full_Stack) {
     // Arrange
     TStack<int> stack(5);
-    
+
     // Act
     for (int i = 0; i < 5; i++)
         stack.Push(i);
-    
+
     // Assert
     EXPECT_TRUE(stack.IsFull() == true);
 }
@@ -100,14 +100,14 @@ TEST(Gladyshev_Alexey_StackTest, Compare_Identical_Stacks) {
     // Arrange
     TStack<int> stack1(5);
     TStack<int> stack2(5);
-    
+
     // Act
     for (int i = 0; i < 5; i++)
     {
         stack1.Push(i);
         stack2.Push(i);
     }
-    
+
     // Assert
     EXPECT_TRUE(stack1 == stack2);
 }
@@ -117,11 +117,11 @@ TEST(Gladyshev_Alexey_StackTest, Can_Assign_Stack) {
     TStack<int> stack1(5);
     for (int i = 0; i < 5; i++)
         stack1.Push(i);
-    
+
     // Act
     TStack<int> stack2(47);
     stack2 = stack1;
-    
+
     // Assert
     EXPECT_TRUE(stack1 == stack2);
 }
