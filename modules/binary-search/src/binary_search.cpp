d01179932bcd2e5a163ddae95b9ef8778af4615e
VarvaraPozdeeva <37691481+VarvaraPozdeeva@users.noreply.github.com>
2018-04-27 11:03:39 +0300

    Update binary_search.cpp

--- modules/binary-search/src/binary_search.cpp
+++ modules/binary-search/src/binary_search.cpp
@@ -3,46 +3,36 @@
 #include "include/binary_search.h"
 #include <algorithm>
 
-using namespace std;
-
-BinarySearch::BinarySearch(int mas[], int size)
-{
-	if (mas != nullptr)
-	{
-		masiv = new int[size];
-		for (int i = 0; i < size; i++)
-			masiv[i] = mas[i];
-		center = size / 2;
-		this->size = size;
-	}
+BinarySearch::BinarySearch(int mas[], int size) {
+         if (mas != nullptr) {
+             masiv = new int[size];
+         for (int i = 0; i < size; i++)
+             masiv[i] = mas[i];
+         center = size / 2;
+         this->size = size;
+    }
 }
-
 BinarySearch::~BinarySearch()
 {
-	if(masiv!= nullptr)
-		delete[] masiv;
+     if(masiv!= nullptr)
+          delete[] masiv;
 }
+int BinarySearch::Search(int num) {
+     std::sort (masiv, masiv + size);
+     int resalt = -1;
+     int right = size;
+     int left = 0;
 
-int BinarySearch::Search(int num)
-{
-	sort (masiv, masiv + size);
-
-	int resalt = -1;
-	int right = size;
-	int left = 0;
-
-	while ((left <= right))
-	{
-		center = (left + right) / 2;
-		if (masiv[center] == num)
-		{
-			resalt = center;
-			break;
-		}
-		if (masiv[center] > num)
-			right = center - 1;
-		else
-			left = center + 1;
-	}
-	return resalt;
+     while ((left <= right)) {
+          center = (left + right) / 2;
+     if (masiv[center] == num) {
+          resalt = center;
+          break;
+     }
+     if (masiv[center] > num)
+           right = center - 1;
+     else
+           left = center + 1;
+     }
+     return resalt;
 }
