54cefad30b679ca1890896f3f8c29c6b369b2af2
VarvaraPozdeeva <37691481+VarvaraPozdeeva@users.noreply.github.com>
2018-04-27 11:09:21 +0300

    Update test_binary_search.cpp

--- modules/binary-search/test/test_binary_search.cpp
+++ modules/binary-search/test/test_binary_search.cpp
@@ -5,38 +5,38 @@
 #include "include/binary_search.h"
 
 TEST(Pozdeeva_Varvaral_BinarySearchTest, can_create) {
-	// Arrange
-    int mas[] = { 1,2,3 };
+     // Arrange
+     int mas[] = { 1, 2, 3};
 
-	// Act	
+      // Act
 
-	// Assert
-    ASSERT_NO_THROW(BinarySearch search(mas, 3));
+      // Assert
+     ASSERT_NO_THROW(BinarySearch search(mas, 3));
 }
 TEST(Pozdeeva_Varvaral_BinarySearchTest, can_search) {
-	// Arrange
-    int mas[] = { 1,2,3 };
-    BinarySearch search(mas, 3);
-	// Act	
-    int resalt = search.Search(3);
-	// Assert
-    EXPECT_EQ(resalt , 2);
+     // Arrange
+     int mas[] = { 1 ,2, 3};
+     BinarySearch search(mas, 3);
+     // Act
+     int resalt = search.Search(3);
+      // Assert
+     EXPECT_EQ(resalt , 2);
 }
 TEST(Pozdeeva_Varvaral_BinarySearchTest, can_search_if_num_not_in_mas) {
-	// Arrange
-    int mas[] = { 1,2,3 };
+     // Arrange
+    int mas[] = { 1, 2, 3};
     BinarySearch search(mas, 3);
-	// Act	
+     // Act
     int resalt = search.Search(4);
-	// Assert
+     // Assert
     EXPECT_EQ(resalt, -1);
 }
 TEST(Pozdeeva_Varvaral_BinarySearchTest, can_Search_if_mas_not_sort) {
-	// Arrange
-    int mas[] = { 5,2,3,1,9 };
+    // Arrange
+    int mas[] = { 5, 2, 3, 1, 9};
     BinarySearch search(mas, 5);
-	// Act	
-    int resalt = search.Search(3);
-	// Assert
+     // Act
+     int resalt = search.Search(3);
+      // Assert
     EXPECT_EQ(resalt, 2);
 }
