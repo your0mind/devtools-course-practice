b72b847f056ef4de8f1fa0bb7974f8178bbabc73
Daniil_Bederdinov <palidos@yandex.ru>
2018-04-28 18:25:28 +0300

    Fixed temperature_converter.h and test_temperature_converter

--- modules/temperature-converter/include/temperature_converter.h
+++ modules/temperature-converter/include/temperature_converter.h
@@ -1,26 +1,26 @@
 // Copyright 2018 Bederdinov Daniil
 
-#ifndef MODULES_TEMPERATURE_CONVERTER_INCLUDE_COMPLEX_NUMBER_H_
-#define MODULES_TEMPERATURE_CONVERTER_INCLUDE_COMPLEX_NUMBER_H_
+#ifndef MODULES_TEMPERATURE_CONVERTER_INCLUDE_TEMPERATURE_CONVERTER_H_
+#define MODULES_TEMPERATURE_CONVERTER_INCLUDE_TEMPERATURE_CONVERTER_H_
 #include <iostream>
 
 class TemperatureConverter {
-    public:
+     public:
         TemperatureConverter() {}
         ~TemperatureConverter() {}
 
-        double kelToCel(double);
-        double kelToFahr(double);
-        double kelToNewt(double);
-        double celToKel(double);
-        double celToFahr(double);
-        double celToNewt(double);
-        double fahrToCel(double);
-        double fahrToKel(double);
-        double fahrToNewt(double);
-        double newtToCel(double);
-        double newtToKel(double);
-        double newtToFahr(double);
+        double kelToCel(double kel);
+        double kelToFahr(double kel);
+        double kelToNewt(double kel);
+        double celToKel(double cel);
+        double celToFahr(double cel);
+        double celToNewt(double cel);
+        double fahrToCel(double fahr);
+        double fahrToKel(double fahr);
+        double fahrToNewt(double fahr);
+        double newtToCel(double newt);
+        double newtToKel(double newt);
+        double newtToFahr(double newt);
 };
 
-#endif  // MODULES_TEMPERATURE_CONVERTER_INCLUDE_COMPLEX_NUMBER_H_
+#endif  // MODULES_TEMPERATURE_CONVERTER_INCLUDE_TEMPERATURE_CONVERTER_H_
