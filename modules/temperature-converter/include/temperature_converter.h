de659708193611b582f57f6b6ff591f855c36e83
Daniil_Bederdinov <palidos@yandex.ru>
2018-04-28 18:35:58 +0300

    Fixed temperature_converter.h

--- modules/temperature-converter/include/temperature_converter.h
+++ modules/temperature-converter/include/temperature_converter.h
@@ -5,22 +5,22 @@
 #include <iostream>
 
 class TemperatureConverter {
-     public:
-        TemperatureConverter() {}
-        ~TemperatureConverter() {}
+ public:
+  TemperatureConverter() {}
+  ~TemperatureConverter() {}
 
-        double kelToCel(double kel);
-        double kelToFahr(double kel);
-        double kelToNewt(double kel);
-        double celToKel(double cel);
-        double celToFahr(double cel);
-        double celToNewt(double cel);
-        double fahrToCel(double fahr);
-        double fahrToKel(double fahr);
-        double fahrToNewt(double fahr);
-        double newtToCel(double newt);
-        double newtToKel(double newt);
-        double newtToFahr(double newt);
+  double kelToCel(double kel);
+  double kelToFahr(double kel);
+  double kelToNewt(double kel);
+  double celToKel(double cel);
+  double celToFahr(double cel);
+  double celToNewt(double cel);
+  double fahrToCel(double fahr);
+  double fahrToKel(double fahr);
+  double fahrToNewt(double fahr);
+  double newtToCel(double newt);
+  double newtToKel(double newt);
+  double newtToFahr(double newt);
 };
 
 #endif  // MODULES_TEMPERATURE_CONVERTER_INCLUDE_TEMPERATURE_CONVERTER_H_
