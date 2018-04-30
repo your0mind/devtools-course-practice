a20205411333506302a3f559d249a3c75c285b05
Pavel Gribov <gribovy2010@gmail.com>
2018-04-30 17:36:08 +0300

    Начальная версия интерфейса класса

---
+++ modules/l-metrics-float/include/l-metrics-float.h
@@ -0,0 +1,17 @@
+// Copyright 2017 Gribov Pavel
+
+#ifndef MODULES_L_METRICS_FLOAT_INCLUDE_L_METRICS_FLOAT_H_
+#define MODULES_L_METRICS_FLOAT_INCLUDE_L_METRICS_FLOAT_H_
+
+#include <vector>
+
+class LMetricsFloat {
+ public:
+    static float linf_vec_distance(std::vector<float> vec1, std::vector<float> vec2);
+	static float l1_vec_distance(std::vector<float> vec1, std::vector<float> vec2);
+	static float l2_vec_distance(std::vector<float> vec1, std::vector<float> vec2);
+	static float l3_vec_distance(std::vector<float> vec1, std::vector<float> vec2);
+	static float l4_vec_distance(std::vector<float> vec1, std::vector<float> vec2);
+};
+
+#endif // MODULES_L_METRICS_FLOAT_INCLUDE_L_METRICS_FLOAT_H_
