b039cbcc2539ee95b8636966b42ef6cc68b4b61d
Pavel Gribov <gribovy2010@gmail.com>
2018-05-01 01:27:26 +0300

    Исправление стиля в интерфейсе

--- modules/l-metrics-float/include/l-metrics-float.h
+++ modules/l-metrics-float/include/l-metrics-float.h
@@ -7,13 +7,19 @@
 
 class LMetricsFloat {
  public:
-    static float linf_vec_distance(const std::vector<float>& vec1, const std::vector<float>& vec2);
-    static float l1_vec_distance(const std::vector<float>& vec1, const std::vector<float>& vec2);
-    static float l2_vec_distance(const std::vector<float>& vec1, const std::vector<float>& vec2);
-    static float l3_vec_distance(const std::vector<float>& vec1, const std::vector<float>& vec2);
-    static float l4_vec_distance(const std::vector<float>& vec1, const std::vector<float>& vec2);
+    static float linf_vec_distance(const std::vector<float>& vec1,
+                                   const std::vector<float>& vec2);
+    static float l1_vec_distance(const std::vector<float>& vec1,
+                                 const std::vector<float>& vec2);
+    static float l2_vec_distance(const std::vector<float>& vec1,
+                                 const std::vector<float>& vec2);
+    static float l3_vec_distance(const std::vector<float>& vec1,
+                                 const std::vector<float>& vec2);
+    static float l4_vec_distance(const std::vector<float>& vec1,
+                                 const std::vector<float>& vec2);
  private:
-    static std::vector<float> vec_substract_mod(const std::vector<float> vec1, const std::vector<float> vec2);
+    static std::vector<float> vec_substract_mod(const std::vector<float> vec1,
+                                                const std::vector<float> vec2);
 };
 
-#endif // MODULES_L_METRICS_FLOAT_INCLUDE_L_METRICS_FLOAT_H_
+#endif  // MODULES_L_METRICS_FLOAT_INCLUDE_L_METRICS_FLOAT_H_
