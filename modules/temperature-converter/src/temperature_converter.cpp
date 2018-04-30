569d780eb412b3d7c75c6f35982b395f31dacd8a
Daniil_Bederdinov <palidos@yandex.ru>
2018-04-30 18:42:48 +0300

    Fixed methods arguments names

--- modules/temperature-converter/src/temperature_converter.cpp
+++ modules/temperature-converter/src/temperature_converter.cpp
@@ -2,50 +2,50 @@
 
 #include "include/temperature_converter.h"
 
-double TemperatureConverter::kelvinToCelsius(double kel) {
-    return kel - 273.15;
+double TemperatureConverter::kelvinToCelsius(double kelvin) {
+    return kelvin - 273.15;
 }
 
-double TemperatureConverter::kelvinToFahrenheit(double kel) {
-    return (9.0 / 5) * (kel - 273.15) + 32;
+double TemperatureConverter::kelvinToFahrenheit(double kelvin) {
+    return (9.0 / 5) * (kelvin - 273.15) + 32;
 }
 
-double TemperatureConverter::kelvinToNewton(double kel) {
-    return (kel - 273.15) * 0.33;
+double TemperatureConverter::kelvinToNewton(double kelvin) {
+    return (kelvin - 273.15) * 0.33;
 }
 
-double TemperatureConverter::celsiusToKelvin(double cel) {
-    return cel + 273.15;
+double TemperatureConverter::celsiusToKelvin(double celsius) {
+    return celsius + 273.15;
 }
 
-double TemperatureConverter::celsiusToFahrenheit(double cel) {
-    return cel * (9.0 / 5) + 32;
+double TemperatureConverter::celsiusToFahrenheit(double celsius) {
+    return celsius * (9.0 / 5) + 32;
 }
 
-double TemperatureConverter::celsiusToNewton(double cel) {
-    return cel * 0.33;
+double TemperatureConverter::celsiusToNewton(double celsius) {
+    return celsius * 0.33;
 }
 
-double TemperatureConverter::fahrenheitToCelsius(double fahr) {
-    return (5.0 / 9) * (fahr - 32);
+double TemperatureConverter::fahrenheitToCelsius(double fahrenheit) {
+    return (5.0 / 9) * (fahrenheit - 32);
 }
 
-double TemperatureConverter::fahrenheitToKelvin(double fahr) {
-    return  (5.0 / 9) * (fahr - 32) + 273.15;
+double TemperatureConverter::fahrenheitToKelvin(double fahrenheit) {
+    return  (5.0 / 9) * (fahrenheit - 32) + 273.15;
 }
 
-double TemperatureConverter::fahrenheitToNewton(double fahr) {
-    return (fahr - 32) * 0.183333;
+double TemperatureConverter::fahrenheitToNewton(double fahrenheit) {
+    return (fahrenheit - 32) * 0.183333;
 }
 
-double TemperatureConverter::newtonToCelsius(double newt) {
-    return newt * 3.030303;
+double TemperatureConverter::newtonToCelsius(double newton) {
+    return newton * 3.030303;
 }
 
-double TemperatureConverter::newtonToKelvin(double newt) {
-    return newt * 3.030303 + 273.15;
+double TemperatureConverter::newtonToKelvin(double newton) {
+    return newton * 3.030303 + 273.15;
 }
 
-double TemperatureConverter::newtonToFahrenheit(double newt) {
-    return newt * 5.454545 + 32;
+double TemperatureConverter::newtonToFahrenheit(double newton) {
+    return newton * 5.454545 + 32;
 }
