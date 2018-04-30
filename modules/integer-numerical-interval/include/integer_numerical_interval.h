16e8ebde83b2ac2999c3a049d016c637bbb8103d
Boris Okunev <hait.08@mail.ru>
2018-04-30 17:11:51 +0300

    Added header

---
+++ modules/integer-numerical-interval/include/integer_numerical_interval.h
@@ -0,0 +1,40 @@
+// Copyright 2018 Okunev Boris
+
+#ifndef MODULES_NUMERICAL_INTERVAL_INCLUDE_NUMERICAL_INTERVAL_H_
+#define MODULES_NUMERICAL_INTERVAL_INCLUDE_NUMERICAL_INTERVAL_H_
+
+#include <string>
+#include <vector>
+
+using std::vector;
+
+class IntegerNumericalInterval {
+public:
+    IntegerNumericalInterval();
+    IntegerNumericalInterval(const int leftBorder,const int rightBorder,const bool isIncludeLeft,const bool isIncludeRight);
+    IntegerNumericalInterval(const std::string& interval);
+    IntegerNumericalInterval(const IntegerNumericalInterval& ni);
+
+
+    bool isContainPoints(const vector<int>& points); 
+    bool isHaveOverlapsRange(const IntegerNumericalInterval& ni);
+    bool isContainsRange(const IntegerNumericalInterval& ni);
+
+    vector<int> getAllPoints() const;
+    vector<int> getEndPoints() const;
+
+    bool operator == (const IntegerNumericalInterval& ni);
+    bool operator != (const IntegerNumericalInterval& ni);
+    operator std::string();
+
+private:
+    bool isInitializationCorrect(const int leftBorder, const int rightBorder, const bool isIncludeLeft, const bool isIncludeRight);
+
+private:
+    int leftBorder_; 
+    int rightBorder_;
+    bool isIncludeLeft_;
+    bool isIncludeRight_;
+};
+
+#endif
