b1c4a0a1c538c70251329970d9723c3ae8c25004
Neaph <vnifadyev@gmail.com>
2018-04-30 09:29:25 +0300

    All else blocks were removed from calculating functions

--- modules/length-converter/src/length-converter.cpp
+++ modules/length-converter/src/length-converter.cpp
@@ -7,164 +7,144 @@ LengthConverter::LengthConverter() { }
 LengthConverter::~LengthConverter() { }
 
 double LengthConverter::centimetersToMeters(double centimeters) {
-    if (isCorrect(centimeters)) {
-        return centimeters / 100;
-    } else {
-        return -1;
+    if (!isCorrect(centimeters)) {
+        throw "Error! Invalid argument";
     }
+    return centimeters / 100
 }
 
 double LengthConverter::metersToCentimeters(double meters) {
-    if (isCorrect(meters)) {
-        return meters * 100;
-    } else {
-        return -1;
+    if (!isCorrect(meters)) {
+        throw "Error! Invalid argument";
     }
+    return meters * 100
 }
 
 double LengthConverter::metersToKilometers(double meters) {
-    if (isCorrect(meters)) {
-        return meters / 1000;
-    } else {
-        return -1;
+    if (!isCorrect(meters)) {
+        throw "Error! Invalid argument";
     }
+    return meters / 1000;
 }
 
 double LengthConverter::kilometersToMeters(double kilometers) {
-    if (isCorrect(kilometers)) {
-        return kilometers * 1000;
-    } else {
-        return -1;
+    if (!isCorrect(kilometers)) {
+        throw "Error! Invalid argument";
     }
+    return kilometers * 1000;
 }
 
 double LengthConverter::kilometersToMiles(double kilometers) {
-    if (isCorrect(kilometers)) {
-        return kilometers / 1.609344;
-    } else {
-        return -1;
+    if (!isCorrect(kilometers)) {
+        throw "Error! Invalid argument";
     }
+    return kilometers / 1.609344;
 }
 
 double LengthConverter::milesToKilometers(double miles) {
-    if (isCorrect(miles)) {
-        return miles * 1.609344;
-    } else {
-        return -1;
+    if (!isCorrect(miles)) {
+        throw "Error! Invalid argument";
     }
+    return miles * 1.609344;
 }
 
 double LengthConverter::footToYards(double foot) {
-    if (isCorrect(foot)) {
-        return foot / 3;
-    } else {
-        return -1;
+    if (!isCorrect(foot)) {
+        throw "Error! Invalid argument";
     }
+    return foot / 3;
 }
 
 double LengthConverter::yardsToFoot(double yards) {
-    if (isCorrect(yards)) {
-        return yards * 3;
-    } else {
-        return -1;
+    if (!isCorrect(yards)) {
+        throw "Error! Invalid argument";
     }
+    return yards * 3;
 }
 
 double LengthConverter::footToMeters(double foot) {
-    if (isCorrect(foot)) {
-        return foot / 0.3048;
-    } else {
-        return -1;
+    if (!isCorrect(foot)) {
+        throw "Error! Invalid argument";
     }
+    return foot / 0.3048;
 }
 
 double LengthConverter::metersToFoot(double meters) {
-    if (isCorrect(meters)) {
-        return meters * 0.3048;
-    } else {
-        return -1;
+    if (!isCorrect(meters)) {
+        throw "Error! Invalid argument";
     }
+    return meters * 0.3048;
 }
 
 double LengthConverter::centimetersToInches(double centimeters) {
-    if (isCorrect(centimeters)) {
-        return centimeters / 2.54;
-    } else {
-        return -1;
+    if (!isCorrect(centimeters)) {
+        throw "Error! Invalid argument";
     }
+    return centimeters / 2.54;
 }
 
 double LengthConverter::inchesToCentimeters(double inches) {
-    if (isCorrect(inches)) {
-        return inches * 2.54;
-    } else {
-        return -1;
+    if (!isCorrect(inches)) {
+        throw "Error! Invalid argument";
     }
+    return inches * 2.54;
 }
 
 double LengthConverter::metersToNauticalMiles(double meters) {
-    if (isCorrect(meters)) {
-        return meters / 1852;
-    } else {
-        return -1;
+    if (!isCorrect(meters)) {
+        throw "Error! Invalid argument";
     }
+    return meters / 1852;
 }
 
 double LengthConverter::nauiticalMilesToMeters(double nauticalMiles) {
-    if (isCorrect(nauticalMiles)) {
-        return nauticalMiles * 1852;
-    } else {
-        return -1;
+    if (!isCorrect(nauticalMiles)) {
+        throw "Error! Invalid argument";
     }
+    return nauticalMiles * 1852;
 }
 
 double LengthConverter::kilometersToAstronomicalUnits(double kilometers) {
-    if (isCorrect(kilometers)) {
-        return kilometers / 149597870.7;
-    } else {
-        return -1;
+    if (!isCorrect(kilometers)) {
+        throw "Error! Invalid argument";
     }
+    return kilometers / 149597870.7;
 }
 
 double LengthConverter::astronomicalUnitsToKilometers
        (double astronomicalUnits) {
-    if (isCorrect(astronomicalUnits)) {
-        return astronomicalUnits * 149597870.7;
-    } else {
-        return -1;
+    if (!isCorrect(astronomicalUnits)) {
+        throw "Error! Invalid argument";
     }
+    return astronomicalUnits * 149597870.7;
 }
 
 double LengthConverter::centimetersToSpans(double centimeters) {
-    if (isCorrect(centimeters)) {
-        return centimeters / 17.78;
-    } else {
-        return -1;
+    if (!isCorrect(centimeters)) {
+        throw "Error! Invalid argument";
     }
+    return centimeters / 17.78;
 }
 
 double LengthConverter::spansToCentimeters(double spans) {
-    if (isCorrect(spans)) {
-        return spans * 17.78;
-    } else {
-        return -1;
+    if (!isCorrect(spans)) {
+        throw "Error! Invalid argument";
     }
+    return spans * 17.78;
 }
 
 double LengthConverter::spansToVersts(double spans) {
-    if (isCorrect(spans)) {
-        return spans / 6000;
-    } else {
-        return -1;
+    if (!isCorrect(spans)) {
+        throw "Error! Invalid argument";
     }
+    return spans / 6000;
 }
 
 double LengthConverter::verstsToSpans(double versts) {
-    if (isCorrect(versts)) {
-        return versts * 6000;
-    } else {
-        return -1;
+    if (!isCorrect(versts)) {
+        throw "Error! Invalid argument";
     }
+    return versts * 6000;
 }
 
 bool LengthConverter::isCorrect(double value) {
