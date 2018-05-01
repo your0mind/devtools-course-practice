0f2d72594a16dafb57e0b1eb3ccca1b54ed6bad1
Lagerta <enniluisarita@gmail.com>
2018-05-01 13:36:34 +0300

    Add implementation of constructors, destructor and equality operator

--- modules/priority-queue/include/priority_queue.h
+++ modules/priority-queue/include/priority_queue.h
@@ -51,4 +51,137 @@ private:
 	std::size_t element_count
 };
 
+template<typename T>
+PriorityQueue<T>::PriorityQueue()
+{
+	elements.head = elements.tail = elements.current = nullptr;
+	elements_count = 0;
+}
+
+template<typename T>
+PriorityQueue<T>::PriorityQueue(T v, int priority)
+{
+	elements.head = elements.tail = elements.current = new Node(v, priority);
+	elements_count = 1;
+}
+
+template<typename T>
+PriorityQueue<T>::PriorityQueue(const PriorityQueue<T>& pq)
+{
+	if (pq.elements.head == nullptr)
+	{
+		elements.head = elements.tail = elements.current = nullptr;
+		elements_count = 0;
+	}
+	else {
+		elements.head = new Node(pq.elements.head->value, pq.elements.head->priority);
+		elements_count = pq.element_count;
+
+		elements.current = elements.first;
+		Node* tmp_current = pq.elements.head->next;
+
+		while (tmp_current != nullptr)
+		{
+			elements.current->next = new Node(tmp_current->value, tmp_current->priority);
+			tmp_current = tmp_current->next;
+			elements.current = elements.current->next;
+		}
+		elements.current = elements.head;
+	}
+}
+
+template<typename T>
+PriorityQueue<T>::~PriorityQueue()
+{
+	if (!empty()) {
+		elements.current = elements.head;
+		while (elements.current != nullptr)
+		{
+			Node* tmp_current = elements.current;
+			elements.current = elements.current->next;
+			if (tmp_current != nullptr) delete tmp_current;
+		}
+		elements.head = elements.tail = elements.current = nullptr;
+	}
+}
+
+template<typename T>
+PriorityQueue<T>& PriorityQueue<T>::operator=(const PriorityQueue<T>& pq)
+{
+	if (this != &pq) {
+		if (pq.elements.head == nullptr)
+		{
+			elements.head = elements.tail = elements.current = nullptr;
+			elements_count = 0;
+		}
+		else {
+			elements.head = new Node(pq.elements.head->value, pq.elements.head->priority);
+			elements_count = pq.element_count;
+
+			elements.current = elements.first;
+			Node* tmp_current = pq.elements.head->next;
+
+			while (tmp_current != nullptr)
+			{
+				elements.current->next = new Node(tmp_current->value, tmp_current->priority);
+				tmp_current = tmp_current->next;
+				elements.current = elements.current->next;
+			}
+			elements.current = elements.head;
+		}
+	}
+	return *this;
+}
+
+template<typename T>
+T PriorityQueue<T>::extract_max()
+{
+	return T();
+}
+
+template<typename T>
+void PriorityQueue<T>::delete_max()
+{
+}
+
+template<typename T>
+void PriorityQueue<T>::push(const T & v, int priority)
+{
+}
+
+template<typename T>
+void PriorityQueue<T>::swap(const PriorityQueue<T>& pq)
+{
+}
+
+template<typename T>
+bool PriorityQueue<T>::empty() const
+{
+	return false;
+}
+
+template<typename T>
+std::size_t PriorityQueue<T>::size() const
+{
+	return std::size_t();
+}
+
+template<typename T>
+int PriorityQueue<T>::get_max_priority() const
+{
+	return 0;
+}
+
+template<typename T>
+bool PriorityQueue<T>::operator==(const PriorityQueue<T>& pq) const
+{
+	return false;
+}
+
+template<typename T>
+bool PriorityQueue<T>::operator!=(const PriorityQueue<T>& pq) const
+{
+	return false;
+}
+
 #endif // MODULES_PRIORITY_QUEUE_INCLUDE_PRIORITY_QUEUE_H_
