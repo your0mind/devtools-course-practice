ac7aac41548ad379dc596054830bc111f9a31c66
VikaSannikova <vikasannikova1998@gmail.com>
2018-05-01 16:41:35 +0300

    fix tabs 16:41

--- modules/avl-tree/test/test_sannikova_vika_avl_treee.cpp
+++ modules/avl-tree/test/test_sannikova_vika_avl_treee.cpp
@@ -67,13 +67,49 @@ TEST(Sannikova_Vika_AVLTreeTest, Can_NOT_Find_Val_In_AVL_Tree) {
     EXPECT_EQ(exp_res, res);
 }
 
-TEST(Sannikova_Vika_AVLTreeTest, Can_Remove_From_AVL_Tree) {
+TEST(Sannikova_Vika_AVLTreeTest, Can_Remove_From_AVL_Tree1) {
     // Arrange
     AVL avltree;
     int numbers[11] = { 0, 2, 4, 5, 6, 15, 82, 90, 91, 3 };
     for (int i = 0; i < 12; i++) {
         avltree.insert(avltree.getRoot(), numbers[i]);
     }
+
+    // Act
+    AVLNode* res1 = avltree.search(avltree.getRoot(), 4);
+    avltree.remove(avltree.getRoot(), 4);
+    AVLNode* res2 = avltree.search(avltree.getRoot(), 4);
+
+    // Assert
+    // AVLNode* exp_res = NULL;
+    EXPECT_NE(res1, res2);
+}
+
+TEST(Sannikova_Vika_AVLTreeTest, Can_Remove_From_AVL_Tree2) {
+    // Arrange
+    AVL avltree;
+    int numbers[11] = { 0, 2, 4, 5, 6, 15, 82, 90, 91, 3 };
+    for (int i = 0; i < 12; i++) {
+        avltree.insert(avltree.getRoot(), numbers[i]);
+    }
+
+    // Act
+    AVLNode* res1 = avltree.search(avltree.getRoot(), 15);
+    avltree.remove(avltree.getRoot(), 15);
+    AVLNode* res2 = avltree.search(avltree.getRoot(), 15);
+
+    // Assert
+    // AVLNode* exp_res = NULL;
+    EXPECT_NE(res1, res2);
+}
+
+TEST(Sannikova_Vika_AVLTreeTest, Can_Remove_From_AVL_Tree3) {
+    // Arrange
+    AVL avltree;
+    // int numbers[11] = { 0, 2, 4, 5, 6, 15, 82, 90, 91, 3 };
+    // for (int i = 0; i < 12; i++) {
+       // avltree.insert(avltree.getRoot(), numbers[i]);
+    // }
 
     // Act
     avltree.remove(avltree.getRoot(), 4);
