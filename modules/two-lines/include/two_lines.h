91b0f92dc01b1c55bc08e2e0b1ecc72f0618daf2
AleksandrPanov <sasha1_1997@mail.ru>
2018-05-02 17:55:27 +0400

    added interface

--- modules/two-lines/include/two_lines.h
+++ modules/two-lines/include/two_lines.h
@@ -2,5 +2,23 @@
 
 #ifndef MODULES_TWO_LINES_INCLUDE_TWO_LINES_H_
 #define MODULES_TWO_LINES_INCLUDE_TWO_LINES_H_
+class Line
+{
+public:
+    Line();
+    Line(double x, double y);
+    Line(const Line &l);
 
+    Line& operator=(const Line &l);
+
+    double getX() const;
+    double getY() const;
+    void setX();
+    void setY();
+
+    bool isIntersection(const Line &l) const;
+private:
+    double x;
+    double y;
+};
 #endif  // MODULES_TWO_LINES_INCLUDE_TWO_LINES_H_
--- modules/two-lines/src/two_lines.cpp
+++ modules/two-lines/src/two_lines.cpp
@@ -1,5 +1,3 @@
 // Copyright 2017 Korniakov Kirill
 
 #include "include/two_lines.h"
-
-
