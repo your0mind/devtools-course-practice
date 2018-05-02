53852ae546a4d86af43363f9f69cbbb741ce9d0c
AleksandrPanov <sasha1_1997@mail.ru>
2018-05-02 17:17:40 +0400

    created lab2 project

---
+++ modules/two-lines/src/two_lines.cpp
@@ -0,0 +1,5 @@
+// Copyright 2017 Korniakov Kirill
+
+#include "include/two_lines.h"
+
+
---
+++ modules/two-lines/test/test_panov_aleksander_two_lines.cpp
@@ -0,0 +1,5 @@
+// Copyright 2018 Panov Aleksander
+
+#include <gtest/gtest.h>
+
+#include "include/two_lines.h"
