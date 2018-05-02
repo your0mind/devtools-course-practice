ec40222fcb52a76be98d3c2c400567ebfb647bc4
jkamelin <julia.kamelina@intel.com>
2018-05-02 16:07:38 +0300

    lab2

---
+++ modules/huffman/include/huffman.h
@@ -0,0 +1,50 @@
+// Copyright 2018 Kamelina Julia
+
+#ifndef MODULES_HUFFMAN_INCLUDE_HUFFMAN_H_
+#define MODULES_HUFFMAN_INCLUDE_HUFFMAN_H_
+
+#include <map>
+#include <vector>
+
+using std::map;
+using std::vector;
+
+struct CNode {
+  CNode* left;
+  CNode* right;
+  char* value;
+  char* code;
+  int freq;
+
+  CNode(char* v = 0, int f = 0,
+        CNode* l = 0, CNode* r = 0, char* c = 0) {
+    value = v;
+    freq = f;
+    left = l;
+    right = r;
+    code = c;
+  }
+
+  bool operator() (const CNode* x, const CNode* y) const {
+    return x->freq > y->freq;
+  }
+};
+
+class Huffman {
+ private:
+    CNode* H_tree;
+    vector<CNode*> frequency;
+
+ private:
+    void BuildTree();
+
+ public:
+    Huffman() {
+      H_tree = nullptr;
+    }
+    explicit Huffman(char* text);
+    ~Huffman();
+    map<char, char*> Encode();
+};
+
+#endif  // MODULES_HUFFMAN_INCLUDE_HUFFMAN_H_
---
+++ modules/huffman/test/test_huffman.cpp
@@ -0,0 +1,131 @@
+// Copyright 2018 Kamelina Julia
+
+#include <gtest/gtest.h>
+#include <map>
+#include <utility>
+#include <vector>
+
+#include "include/huffman.h"
+
+using std::make_pair;
+using std::pair;
+
+TEST(Huffman_Tests, Can_Encode) {
+  //  Arrange
+  char* text = "AAAAAAABBBBBBBCCDDDCCDDDCCFFFFF";
+  vector<pair<char, const char*> > exp = {
+    make_pair('A', "01"), make_pair('B', "10"),
+    make_pair('C', "111"), make_pair('D', "00"),
+    make_pair('F', "110")};
+
+  bool flag = true;
+  Huffman huff(text);
+
+  //  Act
+  map<char, char*> enc = huff.Encode();
+  int i = 0;
+  for (auto it = enc.begin(); it != enc.end(); it++, i++) {
+    if ((it->first != exp[i].first) || strcmp(it->second, exp[i].second) != 0) {
+      flag = false;
+    }
+  }
+
+  //  Assert
+  EXPECT_TRUE(flag);
+}
+
+TEST(Huffman_Tests, Can_Encode2) {
+  //  Arrange
+  char* text = "AAAAAABBBBBBBCCDDDCCDDDCCAA";
+  vector<pair<char, const char*> > exp = {
+    make_pair('A', "11"), make_pair('B', "10"),
+    make_pair('C', "00"), make_pair('D', "01")};
+  bool flag = true;
+  Huffman huff(text);
+
+  //  Act
+  map<char, char*> encoded = huff.Encode();
+  int i = 0;
+  for (auto it = encoded.begin(); it != encoded.end(); it++, i++) {
+    if ((it->first != exp[i].first) || strcmp(it->second, exp[i].second) != 0) {
+      flag = false;
+    }
+  }
+
+  //  Assert
+  EXPECT_TRUE(flag);
+}
+
+TEST(Huffman_Tests, Throws_When_Create_Nullptr) {
+  //  Arrange
+  char* text = 0;
+
+  //  Assert
+  EXPECT_ANY_THROW(Huffman huff(text););
+}
+
+TEST(Huffman_Tests, Throws_When_Encode_Nullptr) {
+  //  Arrange
+  Huffman huff;
+
+  //  Assert
+  EXPECT_ANY_THROW(huff.Encode());
+}
+
+TEST(Huffman_Tests, Can_Encode_One_Letter) {
+  //  Arrange
+  char* text = "AAAAAA";
+  Huffman huff(text);
+
+  //  Act
+  map<char, char*> encoded = huff.Encode();
+
+  //  Assert
+  EXPECT_EQ(encoded.size(), 1);
+  EXPECT_EQ(encoded.begin()->first, 'A');
+  EXPECT_EQ(strcmp(encoded.begin()->second, "1"), 0);
+}
+
+TEST(Huffman_Tests, Fano_Conditional) {
+  //  Arrange
+  char* text = "AAAAAAABBBBBBBCCDDDCCDDDCCFFFFF";
+  bool flag = true;
+  Huffman huff(text);
+
+  //  Act
+  map<char, char*> encoded = huff.Encode();
+  size_t size = encoded.size();
+  for (auto it = encoded.begin(); it != encoded.end(); it++) {
+    auto it2 = it;
+    for (int i = 1; i < size - 1; i++) {
+      char* to_copy;
+      char* to_compare;
+      int len;
+      it2++;
+
+      if (it2 == encoded.end()) {
+        break;
+      }
+
+      if (strlen(it->second) > strlen(it2->second)) {
+        len = strlen(it2->second);
+        to_copy = it->second;
+        to_compare = it2->second;
+      } else {
+        len = strlen(it->second);
+        to_copy = it2->second;
+        to_compare = it->second;
+      }
+      char* expr = new char[len];
+      snprintf(expr, len, "%s", to_copy);
+      expr[len] = '\0';
+      if (strcmp(to_compare, expr) == 0) {
+        flag = false;
+        break;
+      }
+    }
+  }
+
+  //  Assert
+  EXPECT_TRUE(flag);
+}
