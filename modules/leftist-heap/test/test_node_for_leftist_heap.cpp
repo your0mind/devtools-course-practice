a640d04cc62d758b17d41278088601eb8febb7a6
Krasikova Ekaterina <katrinkrasikova@mail.ru>
2018-05-04 22:52:37 +0300

    file text_node.cpp was renamed

--- modules/leftist-heap/test/test_node_for_leftist_heap.cpp
+++ modules/leftist-heap/test/test_node_for_leftist_heap.cpp
@@ -4,19 +4,19 @@
 
 #include "include/Node.h"
 
-TEST(NodeTest,
+TEST(NodeForLeftistHeapTest,
     There_is_no_throw_when_create_null_node) {
     EXPECT_NO_THROW(node n);
 }
-TEST(NodeTest, Can_create_null_node) {
+TEST(NodeForLeftistHeapTest, Can_create_null_node) {
     node n;
     EXPECT_EQ(0, n.key);
 }
-TEST(NodeTest,
+TEST(NodeForLeftistHeapTest,
     There_is_no_throw_when_create_not_null_node) {
     EXPECT_NO_THROW(node n(10));
 }
-TEST(NodeTest, Can_create_not_null_node) {
+TEST(NodeForLeftistHeapTest, Can_create_not_null_node) {
     node n(10);
     EXPECT_EQ(10, n.key);
 }
