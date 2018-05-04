6dcec6fa002880e2fe602cb7d808cb15329ff804
Куликова Светлана Александровна <37101627+SylaiseElvenan@users.noreply.github.com>
2018-05-04 15:43:16 +0300

    Update line_plane_intersection.h

--- modules/line-plane-intersection/include/line_plane_intersection.h
+++ modules/line-plane-intersection/include/line_plane_intersection.h
@@ -1,9 +1,9 @@
 // Copyright 2018 Kulikova Svetlana
 
+#include <cmath>
+
 #ifndef MODULES_LINE_PLANE_INTERSECTION_INCLUDE_LINE_PLANE_INTERSECTION_H_
 #define MODULES_LINE_PLANE_INTERSECTION_INCLUDE_LINE_PLANE_INTERSECTION_H_
-
-#include <cmath>
 
 struct xyz {
     double x, y, z;
@@ -46,7 +46,7 @@ class line_plane_intersection {
  public:
     line_plane_intersection();
 
-    xyz PlaneIntersectLine(xyz A, xyz B,xyz C, xyz X, xyz Y);
+    xyz PlaneIntersectLine(xyz A, xyz B, xyz C, xyz X, xyz Y);
     bool GetNotIntersectPlaneLine();
 
  private:
