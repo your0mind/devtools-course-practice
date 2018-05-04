2e85038c0472d7f41e63da5fb4ecacfb37c78548
Imadzuma <mar-gosha98@mail.ru>
2018-05-04 11:50:23 +0300

    Удалены табы

--- modules/dijkstra-algorithm/include/Graph.h
+++ modules/dijkstra-algorithm/include/Graph.h
@@ -2,18 +2,18 @@
 class Graph
 {
 protected:
-	int n;
-	double** graph_matrix;
+    int n;
+    double** graph_matrix;
 public:
-	Graph(); //котруктор по умолчанию
-	Graph(int count_vertex, double** matrix); //конструктор с параметрами
-	~Graph(); //деструктор
-	int GetCount(); //Получить число вершин
-	double GetWeight(int first, int last); //Получить вес ребра
-	void AddVertex(); //Добавить вершину
-	void AddEdge(int first, int last, double weight); //добавить ребро
-	void DeleteEdge(int first, int last); //Удалить ребро
-	void DeleteVertex(int vertex); //Удалить вершину
-	double FindDistance(int start, int finish); //Поиск расстояния от start до finish
+    Graph(); //котруктор по умолчанию
+    Graph(int count_vertex, double** matrix); //конструктор с параметрами
+    ~Graph(); //деструктор
+    int GetCount(); //Получить число вершин
+    double GetWeight(int first, int last); //Получить вес ребра
+    void AddVertex(); //Добавить вершину
+    void AddEdge(int first, int last, double weight); //добавить ребро
+    void DeleteEdge(int first, int last); //Удалить ребро
+    void DeleteVertex(int vertex); //Удалить вершину
+    double FindDistance(int start, int finish); //Поиск расстояния от start до finish
 };
 
