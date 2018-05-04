e17af4729b4f9478e173c7b3c8982c428fab79e4
Krasikova Ekaterina <katrinkrasikova@mail.ru>
2018-05-04 23:19:37 +0300

    file node.cpp was added

--- modules/leftist-heap/include/Node.h
+++ modules/leftist-heap/include/Node.h
@@ -9,15 +9,7 @@ struct node {
     node* parent;
     node* left;
     node* right;
-    node() {
-        key = 0;
-        rank = 1;
-        parent = left = right = 0;
-    }
-    explicit node(int k) {
-        key = k;
-        rank = 1;
-        parent = left = right = 0;
-    }
+    node();
+    explicit node(int k);
 };
 #endif  // MODULES_LEFTIST_HEAP_INCLUDE_NODE_H_
