76bca448e606468667e619862c25077eb99e6093
unknown <v02055020v@gmail.com>
2018-05-05 11:16:05 +0300

    реализован класс RBTree

---
+++ modules/red-black-tree/include/RBTree.h
@@ -0,0 +1,48 @@
+#ifndef RBTREE
+#define RBTREE
+
+typedef int T;
+typedef enum { BLACK, RED } nColor;    //Возможные цвета узлов дерева
+struct Node {
+    Node* left;           //Указатель на "левого" потомка
+    Node* right;          //Указатель на "правого" потомка
+    Node* parent;         //Указатель на "предка"
+    nColor color;      //Цвет узла
+    T data;               //Хранящиеся в узле данные
+
+    Node(Node* _l, Node* _r, Node* _p, nColor _c, T _d) {
+        left = _l; right = _r, parent = _p, color = _c, data = _d;
+    }
+};
+
+class RBTree
+{
+private:
+
+    void deleteTree(Node* x);  //Удаление дерева
+
+protected:
+
+    Node* root;             //Указатель на корень дерева
+
+    void rotateLeft(Node* x);  //"Левый поворот"
+    void rotateRight(Node* x); //"Правый поворот"
+    void insertFixup(Node* x); //Вставка узла с балансировкой
+    void deleteFixup(Node* x); //Удаление узла с балансировкой
+
+public:
+
+    static Node* NIL;
+
+    RBTree();
+    RBTree(T data);
+    RBTree(T datas[], int count);
+    ~RBTree();
+
+    Node* insertNode(T data);     //Вставка узла по значению
+    Node* findNode(T data);       //Поиск узла по значению
+    void deleteNode(T data);      //Удаление узла по значению
+
+};
+
+#endif
---
+++ modules/red-black-tree/test/test_chernenko_valery_RBTree.cpp
@@ -0,0 +1,80 @@
+// Copyright 2018 Chernenko Valery
+
+#include <gtest/gtest.h>
+#include "include/RBTree.h"
+
+TEST(RBTree, can_create_empty_tree) {
+   ASSERT_NO_THROW(RBTree T);
+}
+
+TEST(RBTree, creation_empty_tree_not_added_node) {
+    RBTree T;
+
+    EXPECT_EQ(RBTree::NIL, T.findNode(5));
+}
+
+TEST(RBTree, can_create_tree) {
+    ASSERT_NO_THROW(RBTree T(5));
+}
+
+TEST(RBTree, creation_tree_add_data) {
+    RBTree T(5);
+
+    EXPECT_NE(RBTree::NIL, T.findNode(5));
+}
+
+TEST(RBTree, can_create_tree_from_array){
+    int array[5];
+
+    for (int i = 0; i < 5; i++)
+        array[i] = i * 2;
+
+    ASSERT_NO_THROW(RBTree T(array, 5));
+}
+
+TEST(RBTree, creation_tree_from_array_add_node) {
+    int array[5];
+    bool check = true;
+
+    for (int i = 0; i < 5; i++)
+        array[i] = i * 2;
+    RBTree T(array, 5);
+    for (int i = 0; i < 5; i++)
+        if (RBTree::NIL == T.findNode(i * 2)) check = false;
+
+    EXPECT_TRUE(check);
+}
+
+TEST(RBTree, can_delete_nonexistent_node) {
+    RBTree T(5);
+
+    ASSERT_NO_THROW(T.deleteNode(8));
+}
+
+TEST(RBTree, can_delete_node) {
+    RBTree T;
+
+    T.insertNode(5);
+    T.deleteNode(5);
+
+    EXPECT_EQ(RBTree::NIL, T.findNode(5));
+}
+
+TEST(RBTree, can_add_two_node) {
+    RBTree T;
+
+    T.insertNode(5);
+    T.insertNode(5);
+
+    EXPECT_NE(RBTree::NIL, T.findNode(5));
+}
+
+TEST(RBTree, can_delete_two_node) {
+    RBTree T;
+
+    T.insertNode(5);
+    T.insertNode(5);
+    T.deleteNode(5);
+
+    EXPECT_EQ(RBTree::NIL, T.findNode(5));
+}
