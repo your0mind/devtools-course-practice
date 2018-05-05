46527a58387b7dda47dd42b10e0fa3457c7b30c3
Daniil <haxaji2012@gmail.com>
2018-05-05 19:43:19 +0300

    Fixed spaces

--- modules/prime-numbers/include/PrimeNumbers.h
+++ modules/prime-numbers/include/PrimeNumbers.h
@@ -1,13 +1,17 @@
 // Copyright 2018 Tipanov Daniil
 
-#pragma once
+#ifndef MODULES_PRIME_NUMBERS_INCLUDE_PRIME_NUMBERS_H_
+#define MODULES_PRIME_NUMBERS_INCLUDE_PRIME_NUMBERS_H_
+
 #include <iostream>
 #include <cmath>
 #include <vector>
 
 class primeNumbers {
-public:
+ public:
     primeNumbers();
     ~primeNumbers();
     std::vector<int> findPrimeNums(int min, int max);
 };
+
+#endif
