a46c2e0361d7df9ed6a7b6c1d07bea4e1e4c4068
Alexander Romanov <al.romanov2012@yandex.ru>
2018-05-05 23:47:30 +0300

    fixed tabulation in q_sort_test

--- modules/quick_sort/test/test_q_sort.cpp
+++ modules/quick_sort/test/test_q_sort.cpp
@@ -18,17 +18,17 @@ TEST(QuickSortTest, Cant_Sort_Empty_Array) {
 }
 
 TEST(QuickSortTest, Cant_Sort_Negative_index) {
-	// Arrange
-	std::vector<int> arr = { 2, 1 };
-	// Assert
-	EXPECT_ANY_THROW(sort::quickSort(arr, -1, 1););
+    // Arrange
+    std::vector<int> arr = { 2, 1 };
+    // Assert
+    EXPECT_ANY_THROW(sort::quickSort(arr, -1, 1););
 }
 
 TEST(QuickSortTest, Can_Sort_Smoke) {
-	// Arrange
-	std::vector<int> arr = {2, 1};
-	// Act
-	sort::quickSort(arr, 0, 1);
-	// Assert
-	EXPECT_TRUE(arr[0] < arr[1]);
+    // Arrange
+    std::vector<int> arr = {2, 1};
+    // Act
+    sort::quickSort(arr, 0, 1);
+    // Assert
+    EXPECT_TRUE(arr[0] < arr[1]);
 }
