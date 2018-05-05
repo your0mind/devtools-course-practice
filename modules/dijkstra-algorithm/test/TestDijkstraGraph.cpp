2e4fb3e61319e854fbc7e970e46d2e6295aee6a5
Imadzuma <36950027+Imadzuma@users.noreply.github.com>
2018-05-05 15:14:33 +0300

    Update TestDijkstraGraph.cpp

--- modules/dijkstra-algorithm/test/TestDijkstraGraph.cpp
+++ modules/dijkstra-algorithm/test/TestDijkstraGraph.cpp
@@ -437,3 +437,21 @@ TEST(Graph, can_equate_graphs) {
     // Assert
     ASSERT_NO_THROW(g1 = g2);
 }
+
+TEST(Graph, can_equate_itself) {
+    // Arrange
+    double** matrix = new double*[3];
+    matrix[0] = new double[3];
+    matrix[1] = new double[3];
+    matrix[2] = new double[3];
+    matrix[0][0] = 0; matrix[0][1] = 1; matrix[0][2] = 2;
+    matrix[1][0] = 1; matrix[1][1] = 0; matrix[1][2] = 3;
+    matrix[2][0] = 2; matrix[2][1] = 3; matrix[2][2] = 4;
+
+    // Act
+    Graph g1(3, matrix);
+
+    // Assert
+    ASSERT_NO_THROW(g1 = g1);
+}
+
