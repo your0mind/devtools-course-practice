8bbc69342b5aa5ca1a4c178edb2ee597554d819d
Куликова Светлана Александровна <37101627+SylaiseElvenan@users.noreply.github.com>
2018-05-05 13:22:46 +0300

    Update line_plane_intersection.h

--- modules/line-plane-intersection/include/line_plane_intersection.h
+++ modules/line-plane-intersection/include/line_plane_intersection.h
@@ -13,14 +13,14 @@ class line_plane_intersection {
  public:
     line_plane_intersection();
 
-    xyz PlaneIntersectLine(dot A, dot B, dot C, dot X, dot Y);
+    dot PlaneIntersectLine(dot A, dot B, dot C, dot X, dot Y);
     bool GetNotIntersectPlaneLine();
 
  private:
     bool NotIntersectPlaneLine;
 
-    xyz CreateVector(dot A, dot B);
-    xyz VectorProduct(dot A, dot B);
+    dot CreateVector(dot A, dot B);
+    dot VectorProduct(dot A, dot B);
     double DotProduct(dot A, dot B);
     bool IncorrectLine(dot A, dot B);
     bool IncorrectPlane(dot A, dot B, dot C);
