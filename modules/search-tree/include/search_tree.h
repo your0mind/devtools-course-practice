ffa4736e9ec44f2df1f12cf5087f849e36da0e3f
KseniaGorelova <32457353+KseniaGorelova@users.noreply.github.com>
2018-05-07 15:55:09 +0300

    Update search_tree.h

--- modules/search-tree/include/search_tree.h
+++ modules/search-tree/include/search_tree.h
@@ -13,10 +13,6 @@ struct Tree {
 class SearchTree {
  public:
     void AddTree(int val, Tree** tr);
-    Tree * FindMin(Tree * tr);
-    Tree * FindMax(Tree * tr);
-    int NumberOfNodes(Tree* tr);
-    int ListCount(Tree* nd);
     Tree * Search(Tree * tr, int key);
     void DestroyBTree(Tree * tr);
 };
