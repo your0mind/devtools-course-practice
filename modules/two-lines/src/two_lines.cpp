e33ddf6ec15dbb694376d6e6d66def090cff66f3
AleksandrPanov <sasha1_1997@mail.ru>
2018-05-08 11:37:49 +0400

    changed checkProjection

--- modules/two-lines/src/two_lines.cpp
+++ modules/two-lines/src/two_lines.cpp
@@ -3,6 +3,10 @@
 #include "include/two_lines.h"
 #include <algorithm>
 
+using std::swap;
+using std::max;
+using std::min;
+
 Point::Point() : x(0), y(0) {}
 
 Point::Point(double _x, double _y) : x(_x), y(_y) {}
@@ -13,14 +17,12 @@ inline double orientedArea(Point a, Point b, Point c) {
 
 inline bool checkProjection(double startLine1_coord, double endLine1_coord,
     double startLine2_coord, double endLine2_coord) {
-
-    if (startLine1_coord > endLine1_coord)
-        std::swap(startLine1_coord, endLine1_coord);
-    if (startLine2_coord > endLine2_coord)
-        std::swap(startLine2_coord, endLine2_coord);
-
-    return std::max(startLine1_coord, startLine2_coord)
-        <= std::min(endLine1_coord, endLine2_coord);
+    if (startLine1_coord > endLine1_coord) 
+        swap(startLine1_coord, endLine1_coord);
+    if (startLine2_coord > endLine2_coord) 
+        swap(startLine2_coord, endLine2_coord);
+    return (max(startLine1_coord, startLine2_coord)
+        <= min(endLine1_coord, endLine2_coord));
 }
 
 bool intersect(Point startLine1, Point endLine1,
