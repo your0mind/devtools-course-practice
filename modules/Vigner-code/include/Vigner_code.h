c1324b7c00871ac5cda4b517d3abc64fcfbade6b
GusevAlexandr <gusevsasha1996@mail.ru>
2018-05-09 20:57:17 +0300

    fixed

--- modules/Vigner-code/include/Vigner_code.h
+++ modules/Vigner-code/include/Vigner_code.h
@@ -6,11 +6,10 @@
 #include <string>
 
 class Vigner_code {
-public:
+ public:
     Vigner_code();
     int Keycode(char s);
     std::string Encode(std::string Text, std::string Key);
     std::string Decode(std::string Text, std::string Key);
 };
-
-#endif // MODULES_VIGNER_CODE_INCLUDE_VIGNER_CODE_H_
+#endif  // MODULES_VIGNER_CODE_INCLUDE_VIGNER_CODE_H_
