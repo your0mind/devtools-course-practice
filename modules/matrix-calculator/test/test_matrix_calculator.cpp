adc42f8507495b7ccbda4bbd899911a1fcba7802
FedorovaOlga <36547568+FedorovaOlga@users.noreply.github.com>
2018-05-10 16:13:11 +0300

    fix for review
    renamed variables

--- modules/matrix-calculator/test/test_matrix_calculator.cpp
+++ modules/matrix-calculator/test/test_matrix_calculator.cpp
@@ -5,17 +5,17 @@
 #include "include/matrix_calculator.h"
 
 TEST(MatrixCalculatorTest, can_create_default_and_with_sizes) {
-    MatrixCalculator a;
-    MatrixCalculator b(1, 1);
+    MatrixCalculator matrix1;
+    MatrixCalculator matrix2(1, 1);
 
-    EXPECT_EQ(a, b);
+    EXPECT_EQ(matrix1, matrix2);
 }
 
 TEST(MatrixCalculatorTest, can_create_copy) {
-    MatrixCalculator a(2, 2);
-    MatrixCalculator b(a);
+    MatrixCalculator matrix1(2, 2);
+    MatrixCalculator matrix2(matrix1);
 
-    EXPECT_EQ(a, b);
+    EXPECT_EQ(matrix1, matrix2);
 }
 
 TEST(MatrixCalculatorTest, cant_create_with_negativ_sizes) {
@@ -27,13 +27,13 @@ TEST(MatrixCalculatorTest, correct_set) {
     for (size_t i = 0; i < vect.size(); i++) {
         vect[i][i] = 3;
     }
-    MatrixCalculator a(1, 2);
-    MatrixCalculator b(a);
+    MatrixCalculator matrix1(1, 2);
+    MatrixCalculator matrix2(matrix1);
 
-    a.SetMatrix(vect);
-    b.SetMatrix(vect);
+	matrix1.SetMatrix(vect);
+	matrix2.SetMatrix(vect);
 
-    EXPECT_EQ(a, b);
+    EXPECT_EQ(matrix1, matrix2);
 }
 
 TEST(MatrixCalculatorTest, size_error_in_set) {
@@ -41,34 +41,34 @@ TEST(MatrixCalculatorTest, size_error_in_set) {
     vect[0] = std::vector<double>(1);
     vect[1] = std::vector<double>(3);
 
-    MatrixCalculator a;
+    MatrixCalculator matrix1;
 
-    EXPECT_ANY_THROW(a.SetMatrix(vect));
+    EXPECT_ANY_THROW(matrix1.SetMatrix(vect));
 }
 
 TEST(MatrixCalculatorTest, correct_are_sizes_equal) {
     std::vector<std::vector<double>> vect1(3, std::vector<double>(3, 0.0));
     std::vector<std::vector<double>> vect2(3, std::vector<double>(5, 0.0));
 
-    MatrixCalculator a;
-    MatrixCalculator b;
-    a.SetMatrix(vect1);
-    b.SetMatrix(vect2);
+    MatrixCalculator matrix1;
+    MatrixCalculator matrix2;
+	matrix1.SetMatrix(vect1);
+	matrix2.SetMatrix(vect2);
 
-    EXPECT_FALSE(a.AreSizesEqual(b));
+    EXPECT_FALSE(matrix1.AreSizesEqual(matrix2));
 }
 
 TEST(MatrixCalculatorTest, correct_equal_operator) {
     std::vector<std::vector<double>> vect1(1, std::vector<double>(1, 1));
     std::vector<std::vector<double>> vect2(2, std::vector<double>(2, 2));
 
-    MatrixCalculator a, b;
-    a.SetMatrix(vect1);
-    b.SetMatrix(vect2);
+    MatrixCalculator matrix1, matrix2;
+	matrix1.SetMatrix(vect1);
+	matrix2.SetMatrix(vect2);
 
-    a = b;
+	matrix1 = matrix2;
 
-    EXPECT_EQ(a, b);
+    EXPECT_EQ(matrix1, matrix2);
 }
 
 TEST(MatrixCalculatorTest, correct_plus_operator) {
@@ -76,12 +76,12 @@ TEST(MatrixCalculatorTest, correct_plus_operator) {
     std::vector<std::vector<double>> vect2(1, std::vector<double>(1, 2));
     std::vector<std::vector<double>> vect3(1, std::vector<double>(1, 3));
 
-    MatrixCalculator a, b, sum, real_res;
-    a.SetMatrix(vect1);
-    b.SetMatrix(vect2);
+    MatrixCalculator matrix1, matrix2, sum, real_res;
+	matrix1.SetMatrix(vect1);
+	matrix2.SetMatrix(vect2);
     real_res.SetMatrix(vect3);
 
-    sum = a + b;
+    sum = matrix1 + matrix2;
 
     EXPECT_EQ(sum, real_res);
 }
@@ -91,12 +91,12 @@ TEST(MatrixCalculatorTest, correct_minus_operator) {
     std::vector<std::vector<double>> vect2(1, std::vector<double>(1, 2));
     std::vector<std::vector<double>> vect3(1, std::vector<double>(1, -1));
 
-    MatrixCalculator a, b, res, real_res;
-    a.SetMatrix(vect1);
-    b.SetMatrix(vect2);
+    MatrixCalculator matrix1, matrix2, res, real_res;
+	matrix1.SetMatrix(vect1);
+	matrix2.SetMatrix(vect2);
     real_res.SetMatrix(vect3);
 
-    res = a - b;
+    res = matrix1 - matrix2;
 
     EXPECT_EQ(res, real_res);
 }
@@ -106,12 +106,12 @@ TEST(MatrixCalculatorTest, correct_multiply_operator) {
     std::vector<std::vector<double>> vect2(1, std::vector<double>(1, 2));
     std::vector<std::vector<double>> vect3(1, std::vector<double>(1, 2));
 
-    MatrixCalculator a, b, res, real_res;
-    a.SetMatrix(vect1);
-    b.SetMatrix(vect2);
+    MatrixCalculator matrix1, matrix2, res, real_res;
+	matrix1.SetMatrix(vect1);
+	matrix2.SetMatrix(vect2);
     real_res.SetMatrix(vect3);
 
-    res = a * b;
+    res = matrix1 * matrix2;
 
     EXPECT_EQ(res, real_res);
 }
@@ -120,33 +120,33 @@ TEST(MatrixCalculatorTest, operator_plus_error_with_not_equal_size) {
     std::vector<std::vector<double>> vect1(1, std::vector<double>(1, 1));
     std::vector<std::vector<double>> vect2(1, std::vector<double>(2, 2));
 
-    MatrixCalculator a, b;
-    a.SetMatrix(vect1);
-    b.SetMatrix(vect2);
+    MatrixCalculator matrix1, matrix2;
+	matrix1.SetMatrix(vect1);
+	matrix2.SetMatrix(vect2);
 
-    EXPECT_ANY_THROW(a + b);
+    EXPECT_ANY_THROW(matrix1 + matrix2);
 }
 
 TEST(MatrixCalculatorTest, operator_minus_error_with_not_equal_size) {
     std::vector<std::vector<double>> vect1(1, std::vector<double>(1, 1));
     std::vector<std::vector<double>> vect2(1, std::vector<double>(2, 2));
 
-    MatrixCalculator a, b;
-    a.SetMatrix(vect1);
-    b.SetMatrix(vect2);
+    MatrixCalculator matrix1, matrix2;
+	matrix1.SetMatrix(vect1);
+	matrix2.SetMatrix(vect2);
 
-    EXPECT_ANY_THROW(a - b);
+    EXPECT_ANY_THROW(matrix1 - matrix2);
 }
 
 TEST(MatrixCalculatorTest, operator_multiply_error_with_wrong_size) {
     std::vector<std::vector<double>> vect1(1, std::vector<double>(2, 1));
     std::vector<std::vector<double>> vect2(3, std::vector<double>(2, 2));
 
-    MatrixCalculator a, b;
-    a.SetMatrix(vect1);
-    b.SetMatrix(vect2);
+    MatrixCalculator matrix1, matrix2;
+	matrix1.SetMatrix(vect1);
+	matrix2.SetMatrix(vect2);
 
-    EXPECT_ANY_THROW(a * b);
+    EXPECT_ANY_THROW(matrix1 * matrix2);
 }
 
 TEST(MatrixCalculatorTest, correct_determinant) {
@@ -155,10 +155,10 @@ TEST(MatrixCalculatorTest, correct_determinant) {
     { 3, 4 } };
     double real_res = -2;
 
-    MatrixCalculator a;
-    a.SetMatrix(vect);
+    MatrixCalculator matrix1;
+	matrix1.SetMatrix(vect);
 
-    EXPECT_EQ(a.determinant(), real_res);
+    EXPECT_EQ(matrix1.determinant(), real_res);
 }
 
 TEST(MatrixCalculatorTest, correct_determinant_with_zero_row) {
@@ -168,10 +168,10 @@ TEST(MatrixCalculatorTest, correct_determinant_with_zero_row) {
     { 5, 3, 1 } };
     double real_res = 0.0;
 
-    MatrixCalculator a;
-    a.SetMatrix(vect);
+    MatrixCalculator matrix1;
+	matrix1.SetMatrix(vect);
 
-    EXPECT_EQ(a.determinant(), real_res);
+    EXPECT_EQ(matrix1.determinant(), real_res);
 }
 
 TEST(MatrixCalculatorTest, determinant_error_with_not_square_matrix) {
@@ -180,10 +180,10 @@ TEST(MatrixCalculatorTest, determinant_error_with_not_square_matrix) {
     { 1, 3 },
     { 3, 4 } };
 
-    MatrixCalculator a;
-    a.SetMatrix(vect);
+    MatrixCalculator matrix1;
+	matrix1.SetMatrix(vect);
 
-    EXPECT_ANY_THROW(a.determinant());
+    EXPECT_ANY_THROW(matrix1.determinant());
 }
 
 TEST(MatrixCalculatorTest, inverse_error_with_not_square_matrix) {
@@ -192,10 +192,10 @@ TEST(MatrixCalculatorTest, inverse_error_with_not_square_matrix) {
     { 1, 3 },
     { 3, 4 } };
 
-    MatrixCalculator a;
-    a.SetMatrix(vect);
+    MatrixCalculator matrix1;
+	matrix1.SetMatrix(vect);
 
-    EXPECT_ANY_THROW(a.InverseMatrix());
+    EXPECT_ANY_THROW(matrix1.InverseMatrix());
 }
 
 TEST(MatrixCalculatorTest, inverse_error_with_zero_determinant) {
@@ -203,10 +203,10 @@ TEST(MatrixCalculatorTest, inverse_error_with_zero_determinant) {
     { 1, 2 },
     { 1, 2 } };
 
-    MatrixCalculator a;
-    a.SetMatrix(vect);
+    MatrixCalculator matrix1;
+	matrix1.SetMatrix(vect);
 
-    EXPECT_ANY_THROW(a.InverseMatrix());
+    EXPECT_ANY_THROW(matrix1.InverseMatrix());
 }
 
 TEST(MatrixCalculatorTest, correct_inverse) {
@@ -217,9 +217,9 @@ TEST(MatrixCalculatorTest, correct_inverse) {
     { 3, -2 },
     { -1, 1 } };
 
-    MatrixCalculator a, real_res;
-    a.SetMatrix(vect1);
+    MatrixCalculator matrix1, real_res;
+	matrix1.SetMatrix(vect1);
     real_res.SetMatrix(vect2);
 
-    EXPECT_EQ(a.InverseMatrix(), real_res);
+    EXPECT_EQ(matrix1.InverseMatrix(), real_res);
 }
