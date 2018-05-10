4652d1b11d48ea342b3be8c06aa4b97fce7d781a
GusevAlexandr <gusevsasha1996@mail.ru>
2018-05-10 16:58:53 +0300

    try fixe

--- modules/Vigner-code/include/Vigner_code.h
+++ modules/Vigner-code/include/Vigner_code.h
@@ -7,11 +7,11 @@
 #define MODULES_VIGNER_CODE_INCLUDE_VIGNER_CODE_H_
 
 class Vigner_code {
- public:
-    Vigner_code();
-    int Keycode(char s);
-    std::string Encode(std::string Text, std::string Key);
-    std::string Decode(std::string Text, std::string Key);
+public:
+	Vigner_code();
+	int Keycode(char s);
+	std::string Encode(std::string Text, std::string Key);
+	std::string Decode(std::string Text, std::string Key);
 };
 
-#endif  // MODULES_VIGNER_CODE_INCLUDE_VIGNER_CODE_H_ 
+#endif  // MODULES_VIGNER_CODE_INCLUDE_VIGNER_CODE_H_
--- modules/Vigner-code/src/Vigner_code.cpp
+++ modules/Vigner-code/src/Vigner_code.cpp
@@ -11,44 +11,44 @@ Vigner_code::Vigner_code() {
 }
 
 int Vigner_code::Keycode(char s) {
-    for (unsigned int k = 0; k < symbols.length(); k++)
-    if (s == symbols[k])
-        c = k;
-    return c;
+	for (unsigned int k = 0; k < symbols.length(); k++)
+		if (s == symbols[k])
+			c = k;
+	return c;
 }
 
 std::string Vigner_code::Encode(std::string Text, std::string Key) {
-    std::string result;
-    for (i = 0; i < Text.length(); i++) {
-        if (j >= Key.length())
-            j = 0;
-        Keycode(Text[i]);
-        sum = c;
-        Keycode(Key[j]);
-        sum = sum + c + 1;
-        if (sum > 51)
-            sum = sum - 52;
-        result += symbols[sum];
-        j++;
-    }
-    j = 0;
-    return result;
+	std::string result;
+	for (i = 0; i < Text.length(); i++) {
+		if (j >= Key.length())
+			j = 0;
+		Keycode(Text[i]);
+		sum = c;
+		Keycode(Key[j]);
+		sum = sum + c + 1;
+		if (sum > 51)
+			sum = sum - 52;
+		result += symbols[sum];
+		j++;
+	}
+	j = 0;
+	return result;
 }
 
 std::string Vigner_code::Decode(std::string Text, std::string Key) {
-    std::string result;
-    for (i = 0; i < Text.length(); i++) {
-        if (j >= Key.length())
-            j = 0;
-        Keycode(Text[i]);
-        sum = c;
-        Keycode(Key[j]);
-        sum = sum - c - 1;
-        if (sum < 0)
-            sum = sum + 52;
-        result += symbols[sum];
-        j++;
-    }
-    j = 0;
-    return result;
+	std::string result;
+	for (i = 0; i < Text.length(); i++) {
+		if (j >= Key.length())
+			j = 0;
+		Keycode(Text[i]);
+		sum = c;
+		Keycode(Key[j]);
+		sum = sum - c - 1;
+		if (sum < 0)
+			sum = sum + 52;
+		result += symbols[sum];
+		j++;
+	}
+	j = 0;
+	return result;
 }
