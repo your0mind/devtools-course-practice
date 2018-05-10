a8a6e8a0f04539ccd6eb963f60152b7e70c10045
GusevAlexandr <gusevsasha1996@mail.ru>
2018-05-10 17:51:52 +0300

    add string

--- modules/Vigner-code/test/test_gusev_alexandr_Vigner_code.cpp
+++ modules/Vigner-code/test/test_gusev_alexandr_Vigner_code.cpp
@@ -1,8 +1,8 @@
 // Copyright 2018 Gusev Alexandr
 
 #include <gtest/gtest.h>
-#include "include/Vigner_code.h"
 #include <string>
+#include "include/Vigner_code.h"
 
 TEST(Gusev_Alexandr_VignerCodeTest, Can_Get_Symbol) {
     // Arrange
@@ -17,22 +17,22 @@ TEST(Gusev_Alexandr_VignerCodeTest, Can_Get_Symbol) {
 TEST(Gusev_Alexandr_VignerCodeTest, Can_Encode_Symbol) {
     // Arrange
     Vigner_code vigner;
-	std::string Text = "abcABC";
-	std::string Key = "abc";
+    std::string Text = "abcABC";
+    std::string Key = "abc";
     // Act
-	std::string res = vigner.Encode(Text, Key);
+    std::string res = vigner.Encode(Text, Key);
     // Assert
-	std::string ans = "bdfBDF";
+    std::string ans = "bdfBDF";
     EXPECT_EQ(res, ans);
 }
 TEST(Gusev_Alexandr_VignerCodeTest, Can_Decode_Symbol) {
     // Arrange
     Vigner_code vigner;
-	std::string Text = "bdfBDF";
-	std::string Key = "abc";
+    std::string Text = "bdfBDF";
+    std::string Key = "abc";
     // Act
-	std::string res = vigner.Decode(Text, Key);
+    std::string res = vigner.Decode(Text, Key);
     // Assert
-	std::string ans = "abcABC";
+    std::string ans = "abcABC";
     EXPECT_EQ(res, ans);
 }
