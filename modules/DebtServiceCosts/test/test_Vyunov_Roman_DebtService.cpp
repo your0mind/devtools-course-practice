a1559ae3f6fb2d270b1a236acaaf656257b46f67
Вьюнов Роман <vyunov.rom@mail.ru>
2018-05-11 10:21:32 +0300

    Code style fixed

--- modules/DebtServiceCosts/test/test_Vyunov_Roman_DebtService.cpp
+++ modules/DebtServiceCosts/test/test_Vyunov_Roman_DebtService.cpp
@@ -1,4 +1,4 @@
-//Copyright 2018 Vyunov Roman
+// Copyright 2018 Vyunov Roman
 
 #include <gtest/gtest.h>
 
@@ -12,5 +12,5 @@ TEST(Vyunov_Roman_DebtService_test, Can_Create_Zero) {
 
 
     // Assert
-    ASSERT_ANY_THROW(new DebtService(-1,2,3));
+    ASSERT_ANY_THROW(new DebtService(-1, 2, 3));
 }
