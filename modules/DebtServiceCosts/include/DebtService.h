3a895298f6da850ff15838bfd18da4ec9395f705
Вьюнов Роман <vyunov.rom@mail.ru>
2018-05-11 10:03:02 +0300

    Code style fixed

--- modules/DebtServiceCosts/include/DebtService.h
+++ modules/DebtServiceCosts/include/DebtService.h
@@ -1,21 +1,21 @@
-//Copyright 2018 Vyunov Roman
+// Copyright 2018 Vyunov Roman
 
-#ifndef DEBT_SERVICE_H_
-#define DEBT_SERVICE_H_
+#ifndef MODULES_DEBTSERVICECOSTS_INCLUDE_DEBTSERVICE_H_
+#define MODULES_DEBTSERVICECOSTS_INCLUDE_DEBTSERVICE_H_
 
 class DebtService {
-private:
+ private:
     int loan_amount_;
     int loan_term_;
     int loan_rate_;
 
-public:
-    //DebtService ():loan_amount_(0), loan_term_(0), loan_rate_(0) {}
+ public:
+    // DebtService ():loan_amount_(0), loan_term_(0), loan_rate_(0) {}
     // DebtService& operator= (const DebtService& ds);
     // DebtService& operator= (const DebtService&& ds);
-    DebtService (int loan_amount, int loan_term, int loan_rate);
+    DebtService(int loan_amount, int loan_term, int loan_rate);
 
-    //virtual ~DebtService ();
+    // virtual ~DebtService ();
 };
 
-#endif
+#endif // MODULES_DEBTSERVICECOSTS_INCLUDE_DEBTSERVICE_H_
