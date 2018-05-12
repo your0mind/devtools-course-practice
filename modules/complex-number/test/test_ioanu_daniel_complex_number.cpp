7dee99f05585287b0d71a0354940e933bbfc6100
Даниел Иоану <d3n1el@live.com>
2018-05-12 23:41:42 +0300

    Edit test sections

--- modules/complex-number/test/test_ioanu_daniel_complex_number.cpp
+++ modules/complex-number/test/test_ioanu_daniel_complex_number.cpp
@@ -21,10 +21,13 @@ TEST(Ioanu_Daniel_ComplexNumberTest, Can_Division_Imag) {
     // Arrange
     ComplexNumber z1(0.0, 20.0);
     ComplexNumber z2(0.0, 10.0);
-    ComplexNumber res(2.0, 0.0);
+	ComplexNumber answer(2.0, 0.0);
+	
+	// Act
+    ComplexNumber res = z1 / z2;
 
     // Assert
-    EXPECT_EQ(z1 / z2, res);
+    EXPECT_EQ(res, answer);
 }
 
 TEST(Ioanu_Daniel_ComplexNumberTest, Are_Numbers_Equal) {
@@ -34,11 +37,16 @@ TEST(Ioanu_Daniel_ComplexNumberTest, Are_Numbers_Equal) {
     ComplexNumber z3(9.0, -3.0);
     ComplexNumber z4(3.0, -3.0);
 
-    // Act & Assert
-    EXPECT_EQ(z1 / z2, z3 / z4);
+    // Act
+	ComplexNumber res1 = z1 / z2;
+	ComplexNumber res2 = z3 / z4;
+	
+	// Assert
+    EXPECT_EQ(res1, res2);
 }
 
 TEST(Ioanu_Daniel_ComplexNumberTest, Numbers_Not_Equal) {
+    // Arrange
     ComplexNumber z1(12.0, 3.0);
     ComplexNumber z2(3.0, 8.0);
 
@@ -47,14 +55,12 @@ TEST(Ioanu_Daniel_ComplexNumberTest, Numbers_Not_Equal) {
 }
 
 TEST(Ioanu_Daniel_ComplexNumberTest, Not_Dividable_By_Zero) {
+    // Arrange
     double r = 23.01;
     double i = 42.02;
-
-    // Arrange
     ComplexNumber z1(r, i);
     ComplexNumber zero(0, 0);
-    ComplexNumber res;
 
     // Act & Assert
-    EXPECT_ANY_THROW(res = z1 / zero);
+    EXPECT_ANY_THROW(z1 / zero);
 }
