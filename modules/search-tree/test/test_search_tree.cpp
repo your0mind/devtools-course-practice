0207cfec134f8e143add1521bf62f06a36bf6c13
KseniaGorelova <32457353+KseniaGorelova@users.noreply.github.com>
2018-05-13 17:54:50 +0300

    Update test_search_tree.cpp

--- modules/search-tree/test/test_search_tree.cpp
+++ modules/search-tree/test/test_search_tree.cpp
@@ -15,17 +15,17 @@ TEST(Gorelova_Ksenia_SearchTree_Test, can_delete_empty_tree) {
 
 TEST(Gorelova_Ksenia_SearchTree_Test, can_insert_element_in_empty_tree) {
     SearchTree * tree = new SearchTree();
-    double key = 11.0;
+    int key = 11;
     Tree * tr = nullptr;
-    ASSERT_NO_THROW(tree->AddTree(key, &tr));
+    ASSERT_NO_THROW(tree->AddTree(key, tr));
     delete tree;
 }
 
 TEST(Gorelova_Ksenia_SearchTree_Test, can_search_element) {
     SearchTree * tree = new SearchTree();
-    double key = 11.0;
+    int key = 11;
     Tree * tr = nullptr;
-    tree->AddTree(key, &tr);
+    tree->AddTree(key, tr);
     ASSERT_NO_THROW(tree->Search(tr, key));
     delete tree;
 }
