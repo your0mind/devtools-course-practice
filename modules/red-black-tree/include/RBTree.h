12e31dc92521ecf4e675b12a0c0b2a9fb9f816c6
unknown <v02055020v@gmail.com>
2018-05-13 20:23:57 +0300

    Исправлены тесты снова

--- modules/red-black-tree/include/RBTree.h
+++ modules/red-black-tree/include/RBTree.h
@@ -13,7 +13,8 @@ struct Node {
     int count;  /*Count of Elements*/
 
     Node(Node* _l, Node* _r, Node* _p, nColor _c, T _d, int _count = 1) {
-        left = _l; right = _r; parent = _p; color = _c; data = _d; count = _count;
+        left = _l; right = _r; parent = _p; 
+        color = _c; data = _d; count = _count;
     }
 };
 
