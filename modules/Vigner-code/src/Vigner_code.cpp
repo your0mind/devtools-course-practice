5205d7ffee6c0c4037545c55f613755708fceab3
GusevAlexandr <gusevsasha1996@mail.ru>
2018-05-13 08:22:27 +0300

    changes

--- modules/Vigner-code/src/Vigner_code.cpp
+++ modules/Vigner-code/src/Vigner_code.cpp
@@ -4,8 +4,6 @@
 #include <string>
 
 std::string symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-unsigned int i, j;
-int c, sum;
 
 Vigner_code::Vigner_code() {
 }
@@ -13,19 +11,18 @@ Vigner_code::Vigner_code() {
 int Vigner_code::Keycode(char s) {
     for (unsigned int k = 0; k < symbols.length(); k++)
         if (s == symbols[k])
-            c = k;
-    return c;
+    return k;
 }
 
 std::string Vigner_code::Encode(std::string Text, std::string Key) {
     std::string result;
+    unsigned int i, j = 0;
+    int sum;
     for (i = 0; i < Text.length(); i++) {
         if (j >= Key.length())
             j = 0;
-        Keycode(Text[i]);
-        sum = c;
-        Keycode(Key[j]);
-        sum = sum + c + 1;
+        sum = Keycode(Text[i]);
+        sum = sum + Keycode(Key[j]) + 1;
         if (sum > 51)
             sum = sum - 52;
         result += symbols[sum];
@@ -37,13 +34,13 @@ std::string Vigner_code::Encode(std::string Text, std::string Key) {
 
 std::string Vigner_code::Decode(std::string Text, std::string Key) {
     std::string result;
+    unsigned int i, j = 0;
+    int sum;
     for (i = 0; i < Text.length(); i++) {
         if (j >= Key.length())
             j = 0;
-        Keycode(Text[i]);
-        sum = c;
-        Keycode(Key[j]);
-        sum = sum - c - 1;
+        sum = Keycode(Text[i]);
+        sum = sum - Keycode(Key[j]) - 1;
         if (sum < 0)
             sum = sum + 52;
         result += symbols[sum];
--- modules/Vigner-code/test/test_gusev_alexandr_Vigner_code.cpp
+++ modules/Vigner-code/test/test_gusev_alexandr_Vigner_code.cpp
@@ -8,10 +8,11 @@ TEST(Gusev_Alexandr_VignerCodeTest, Can_Get_Symbol) {
     // Arrange
     Vigner_code vigner;
     char sym = 'e';
-    // Act
-    int res = vigner.Keycode(sym);
-    // Assert
+    int res;
     int ans = 4;
+    // Act
+    res = vigner.Keycode(sym);
+    // Assert
     EXPECT_EQ(res, ans);
 }
 TEST(Gusev_Alexandr_VignerCodeTest, Can_Encode_Symbol) {
@@ -19,10 +20,11 @@ TEST(Gusev_Alexandr_VignerCodeTest, Can_Encode_Symbol) {
     Vigner_code vigner;
     std::string Text = "abcABC";
     std::string Key = "abc";
-    // Act
-    std::string res = vigner.Encode(Text, Key);
-    // Assert
+    std::string res;
     std::string ans = "bdfBDF";
+    // Act
+    res = vigner.Encode(Text, Key);
+    // Assert
     EXPECT_EQ(res, ans);
 }
 TEST(Gusev_Alexandr_VignerCodeTest, Can_Decode_Symbol) {
@@ -30,10 +32,11 @@ TEST(Gusev_Alexandr_VignerCodeTest, Can_Decode_Symbol) {
     Vigner_code vigner;
     std::string Text = "bdfBDF";
     std::string Key = "abc";
-    // Act
-    std::string res = vigner.Decode(Text, Key);
-    // Assert
+    std::string res;
     std::string ans = "abcABC";
+    // Act
+    res = vigner.Decode(Text, Key);
+    // Assert
     EXPECT_EQ(res, ans);
 }
 TEST(Gusev_Alexandr_VignerCodeTest, Encode_and_Decode_Symbol) {
@@ -41,9 +44,10 @@ TEST(Gusev_Alexandr_VignerCodeTest, Encode_and_Decode_Symbol) {
     Vigner_code vigner;
     std::string Text = "ajffAZBtC";
     std::string Key = "abcZs";
+    std::string res, res2;
     // Act
-    std::string res = vigner.Encode(Text, Key);
-    std::string res2 = vigner.Decode(res, Key);
+    res = vigner.Encode(Text, Key);
+    res2 = vigner.Decode(res, Key);
     // Assert
     EXPECT_EQ(res2, Text);
 }
