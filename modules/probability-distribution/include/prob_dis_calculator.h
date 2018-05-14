8bb31db2f2a996cd99b4b1926e29966698d6ebb5
Oleg <ogretoleg@yandex.ru>
2018-05-14 16:18:00 +0300

    fix for style

--- modules/probability-distribution/include/prob_dis_calculator.h
+++ modules/probability-distribution/include/prob_dis_calculator.h
@@ -1,7 +1,7 @@
 // Copyright 2018 Lalykin Oleg
 
-#ifndef MODULES_PROB_DIS_CALCULATOR_INCLUDE_PROB_DIS_CALCULATOR_H_
-#define MODULES_PROB_DIS_CALCULATOR_INCLUDE_PROB_DIS_CALCULATOR_H_
+#ifndef MODULES_PROBABILITY_DISTRIBUTION_INCLUDE_PROB_DIS_CALCULATOR_H_
+#define MODULES_PROBABILITY_DISTRIBUTION_INCLUDE_PROB_DIS_CALCULATOR_H_
 
 #include <string>
 #include <vector>
@@ -11,19 +11,17 @@ class ProbDisCalculator {
     ProbDisCalculator();
     std::string operator()(int argc, const char** argv);
 
-    
  private:
     void help(const char* appname, const char* message = "");
-    bool validateNumberOfArguments(int argc, const char** argv,int n);
+    bool validateNumberOfArguments(int argc, const char** argv, int n);
     std::string message_;
     typedef struct {
         int n;
-        std::vector<double> values ;
-        std::vector<double> probabilities ;
+        std::vector<double> values;
+        std::vector<double> probabilities;
         char operation;
         unsigned char level;
     } Arguments;
-
 };
 
-#endif  // MODULES_PROB_DIS_CALCULATOR_INCLUDE_PROB_DIS_CALCULATOR_H_
+#endif  // MODULES_PROBABILITY_DISTRIBUTION_INCLUDE_PROB_DIS_CALCULATOR_H_
