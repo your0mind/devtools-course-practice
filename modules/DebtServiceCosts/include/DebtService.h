d7ba26f29f16c31e05b21b75d02aedb225734941
Вьюнов Роман <vyunov.rom@mail.ru>
2018-05-15 14:11:12 +0300

    Some fixtures added

--- modules/DebtServiceCosts/include/DebtService.h
+++ modules/DebtServiceCosts/include/DebtService.h
@@ -3,23 +3,32 @@
 #ifndef MODULES_DEBTSERVICECOSTS_INCLUDE_DEBTSERVICE_H_
 #define MODULES_DEBTSERVICECOSTS_INCLUDE_DEBTSERVICE_H_
 
-enum DebtServiceType {ONE_PAYMENT_AT_THE_END, PRINCIPAL_DEBT_ONE_PAYMENT_AT_THE_END};
+#include <math.h>
+
+enum DebtServiceType {ONE_PAYMENT_AT_THE_END,
+                      PRINCIPAL_DEBT_ONE_PAYMENT_AT_THE_END,
+                      EQUAL_ANNUAL_PAYMENTS};
 
 class DebtService {
  private:
     float loan_amount_;  // Сумма займа
-    float loan_term_;  // Срок займа в единицах времени
+    int loan_term_;  // Срок займа в единицах времени
+    int curr_term_;  // Сколько времени прошло от начала займа
     float loan_rate_;  // Процент займа за единицу времени
     float loan_balance_;  // Остаток по займу
     DebtServiceType service_type_;  //тип обслуживания займа
+
  public:
-    DebtService():loan_amount_(0), loan_term_(0), loan_rate_(0),
+    DebtService():loan_amount_(0), loan_term_(0), curr_term_(0), loan_rate_(0),
         loan_balance_(0), service_type_(ONE_PAYMENT_AT_THE_END) {}
-    DebtService(float loan_amount, float loan_term, float loan_rate, DebtServiceType dt);
+    DebtService(float loan_amount, int loan_term, float loan_rate, DebtServiceType dt);
     DebtService(const DebtService& ds);
     DebtService& operator= (const DebtService& ds);
 
-
+    float GetTotalPayout();
+    void MakingPayment(float pay_amnt);
+    void SetNextTime();
+    float GetBalance();
     ~DebtService() {}
 };
 
--- modules/DebtServiceCosts/src/DebtService.cpp
+++ modules/DebtServiceCosts/src/DebtService.cpp
@@ -2,7 +2,7 @@
 
 #include "../modules/DebtServiceCosts/include/DebtService.h"
 
-DebtService::DebtService(float loan_amount, float loan_term, float loan_rate, DebtServiceType dt) {
+DebtService::DebtService(float loan_amount, int loan_term, float loan_rate, DebtServiceType dt) {
     if (loan_amount < 0) {
         throw "Incorrect amount";
     } else {
@@ -21,7 +21,7 @@ DebtService::DebtService(float loan_amount, float loan_term, float loan_rate, De
     } else {
         loan_rate_ = loan_rate;
     }
-
+    curr_term_ = 0;
     service_type_ = dt;
 }
 
@@ -31,13 +31,58 @@ DebtService::DebtService(const DebtService &ds) {
     loan_rate_ = ds.loan_rate_;
     loan_term_ = ds.loan_term_;
     service_type_ = ds.service_type_;
+    curr_term_ = ds.curr_term_;
 }
-DebtService& DebtService::operator= (const DebtService& dt){
+
+DebtService& DebtService::operator= (const DebtService& dt) {
     loan_amount_ = dt.loan_amount_;
     loan_balance_ = dt.loan_balance_;
     loan_rate_ = dt.loan_rate_;
     loan_term_ = dt.loan_term_;
     service_type_ = dt.service_type_;
+    curr_term_ = dt.curr_term_;
 
     return *this;
+}
+
+float DebtService::GetTotalPayout() {
+    switch (service_type_) {
+    case ONE_PAYMENT_AT_THE_END:
+        return loan_amount_ * pow(1 + loan_rate_, loan_term_);
+        break;
+    case PRINCIPAL_DEBT_ONE_PAYMENT_AT_THE_END:
+        return loan_amount_ * (1 + loan_term_ * loan_rate_);
+        break;
+    case EQUAL_ANNUAL_PAYMENTS:
+        return loan_amount_ * (1 + loan_term_ * loan_rate_) - loan_amount_ * (loan_rate_ * (loan_term_ - 1)/2);
+        break;
+    default:
+        throw "Wrong service type";
+        break;
+    }
+}
+
+void DebtService::MakingPayment(float pay_amnt){
+    switch (service_type_) {
+    case ONE_PAYMENT_AT_THE_END:
+        if(pay_amnt == loan_balance_)
+            loan_balance_ = 0;
+        else
+            throw "You must make one pay equal balance";
+        break;
+    case PRINCIPAL_DEBT_ONE_PAYMENT_AT_THE_END:
+        // TODO;
+        break;
+    case EQUAL_ANNUAL_PAYMENTS:
+
+        break;
+    }
+}
+
+void DebtService::SetNextTime(){
+    curr_term_ += 1;
+}
+
+float DebtService::GetBalance(){
+    return loan_balance_;
 }
