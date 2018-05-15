4a81899fb070cb4c9832c60d2a6ab0773bed451b
SolarSystem23 <piccione-mail@yandex.ru>
2018-05-15 22:43:07 +0300

    Add RadixSorter

--- modules/radix-sort/test/test_radix_sort.cpp
+++ modules/radix-sort/test/test_radix_sort.cpp
@@ -41,3 +41,100 @@ TEST_F(RadixSortTest, Throw_When_Pass_Reverse_Range) {
     // Arrange & Act & Assert
     EXPECT_THROW(radixSort(data.end(), data.begin()), std::logic_error);
 }
+
+TEST_F(RadixSortTest, Create_Radix_Sorter) {
+  // AAA
+  EXPECT_NO_THROW(RadixSorter rs);
+}
+
+TEST_F(RadixSortTest, Create_Radix_Sorter_From_Vector) {
+  // Arrange & Act
+  std::vector<int> v = {1, 4, 5};
+
+  // Assert
+  EXPECT_NO_THROW(RadixSorter rs(v));
+}
+
+TEST_F(RadixSortTest, Check_Get_Data) {
+  // Arrange & Act
+  std::vector<int> v = {1, 4, 5};
+
+  // Act
+  RadixSorter rs(v);
+
+  // Assert
+  EXPECT_EQ(v, rs.get_data());
+}
+
+TEST_F(RadixSortTest, Create_Radix_Sorter_From_Vector_Iterators) {
+  // Arrange
+  std::vector<int> v = {1, 5, 6, 1, 6, -2};
+
+  // Act
+  RadixSorter rs(v.begin(), v.end());
+
+  // Assert
+  EXPECT_EQ(rs.get_data(), v);
+}
+
+TEST_F(RadixSortTest, Create_Radix_Sorter_Move) {
+  // Arrange
+  std::vector<int> v = {1, 5, 6, 1, 6, -2};
+
+  // Act
+  RadixSorter rs(v.begin(), v.end());
+  RadixSorter mrs(std::move(rs));
+
+  // Assert
+  EXPECT_EQ(mrs.get_data(), v);
+}
+
+TEST_F(RadixSortTest, Create_Radix_Sorter_Copy) {
+  // Arrange
+  std::vector<int> v = {1, 5, 6, 1, 6, -2};
+
+  // Act
+  RadixSorter rs(v.begin(), v.end());
+  RadixSorter crs(rs);
+
+  // Assert
+  EXPECT_EQ(crs.get_data(), v);
+}
+
+TEST_F(RadixSortTest, Assign_Radix_Sorter) {
+  // Arrange
+  std::vector<int> v = {1, 5, 0};
+  RadixSorter rs(v);
+  RadixSorter rs2;
+
+  // Act
+  rs2 = rs;
+
+  // Assert
+  EXPECT_EQ(rs2.get_data(), v);
+}
+
+TEST_F(RadixSortTest, Move_Assign_Radix_Sorter) {
+  // Arrange
+  std::vector<int> v = {1, 5, 0};
+  RadixSorter rs(v);
+  RadixSorter rs2;
+
+  // Act
+  rs2 = std::move(rs);
+
+  // Assert
+  EXPECT_EQ(rs2.get_data(), v);
+}
+
+TEST_F(RadixSortTest, Check_Sort_Radix_Sorter) {
+  // Arrange
+  std::vector<int> v = {-3, 5, 1, 5, 10};
+  RadixSorter rs(v);
+
+  // Act
+  auto sort_v = rs.sort();
+
+  // Assert
+  EXPECT_TRUE(std::is_sorted(sort_v.begin(), sort_v.end()));
+}
