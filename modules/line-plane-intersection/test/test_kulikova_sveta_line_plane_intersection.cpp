41626bf3420245dfa641b5bbc2b33d1a3eca31ab
Куликова Светлана Александровна <37101627+SylaiseElvenan@users.noreply.github.com>
2018-05-16 10:33:18 +0300

    Update test_kulikova_sveta_line_plane_intersection.cpp

--- modules/line-plane-intersection/test/test_kulikova_sveta_line_plane_intersection.cpp
+++ modules/line-plane-intersection/test/test_kulikova_sveta_line_plane_intersection.cpp
@@ -14,7 +14,7 @@ TEST(Kulikova_Sveta_LinePlaneIntersection_Test, Intersect1) {
     dot EQ = { -2, 3, -5 };
     dot res;
     double epsilon = 1e-6;
-    line_plane_intersection F;
+    LinePlaneIntersection F;
 
     // Act
     res = F.PlaneIntersectLine(Plane1, Plane2, Plane3, Line1, Line2);
@@ -35,7 +35,7 @@ TEST(Kulikova_Sveta_LinePlaneIntersection_Test, Intersect2) {
     dot EQ = { -2, 1, 1 };
     dot res;
     double epsilon = 1e-6;
-    line_plane_intersection F;
+    LinePlaneIntersection F;
 
     // Act
     res = F.PlaneIntersectLine(Plane1, Plane2, Plane3, Line1, Line2);
@@ -54,7 +54,7 @@ TEST(Kulikova_Sveta_LinePlaneIntersection_Test, NotIntersect) {
     dot Plane2 = { -0.5, 0, 0 };
     dot Plane3 = { 1, 0, 1 };
     dot res;
-    line_plane_intersection F;
+    LinePlaneIntersection F;
 
     // Act
     res = F.PlaneIntersectLine(Plane1, Plane2, Plane3, Line1, Line2);
@@ -70,7 +70,7 @@ TEST(Kulikova_Sveta_LinePlaneIntersection_Test, IncorrectLine) {
     dot Plane1 = { 0, 1, 0 };
     dot Plane2 = { -0.5, 0, 0 };
     dot Plane3 = { 1, 0, 1 };
-    line_plane_intersection F;
+    LinePlaneIntersection F;
 
     // Act & Assert
     EXPECT_ANY_THROW(F.PlaneIntersectLine(Plane1,
@@ -84,7 +84,7 @@ TEST(Kulikova_Sveta_LinePlaneIntersection_Test, IncorrectPlaneA) {
     dot Plane1 = { 0, 0, 0 };
     dot Plane2 = { 0, 0, 0 };
     dot Plane3 = { 0, 1, 0 };
-    line_plane_intersection F;
+    LinePlaneIntersection F;
 
     // Act & Assert
     EXPECT_ANY_THROW(F.PlaneIntersectLine(Plane1,
@@ -98,7 +98,7 @@ TEST(Kulikova_Sveta_LinePlaneIntersection_Test, IncorrectPlaneB) {
     dot Plane1 = { 0, 0, 0 };
     dot Plane2 = { 0, 1, 0 };
     dot Plane3 = { 0, 0, 0 };
-    line_plane_intersection F;
+    LinePlaneIntersection F;
 
     // Act & Assert
     EXPECT_ANY_THROW(F.PlaneIntersectLine(Plane1,
@@ -113,7 +113,7 @@ TEST(Kulikova_Sveta_LinePlaneIntersection_Test, IncorrectPlaneC) {
     dot Plane2 = { 0, 0, 0 };
     dot Plane3 = { 0, 0, 0 };
     dot res;
-    line_plane_intersection F;
+    LinePlaneIntersection F;
 
     // Act & Assert
     EXPECT_ANY_THROW(F.PlaneIntersectLine(Plane1,
