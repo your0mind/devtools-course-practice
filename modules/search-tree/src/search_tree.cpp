23f8a1c170b3db65017deeef3cb71571ccfe9f8d
KseniaGorelova <32457353+KseniaGorelova@users.noreply.github.com>
2018-05-17 11:59:49 +0300

    Update search_tree.cpp

--- modules/search-tree/src/search_tree.cpp
+++ modules/search-tree/src/search_tree.cpp
@@ -6,34 +6,34 @@ SearchTree::SearchTree(Tree * pRoot) {
     this->pRoot = pRoot;
 }
 
-void SearchTree::AddTree(int val, Tree * tr) {
-    if (tr == nullptr) {
-        tr = new Tree;
-        tr->value = val;
-        tr->left = tr->right = nullptr;
+void SearchTree::AddTree(int val, Tree * pRoot) {
+    if (pRoot == nullptr) {
+        pRoot = new Tree;
+        pRoot->value = val;
+        pRoot->left = pRoot->right = nullptr;
         return;
     }
-    if (val > tr->value)
-        AddTree(val, tr->right);
+    if (val > pRoot->value)
+        AddTree(val, pRoot->right);
     else
-        AddTree(val, tr->left);
+        AddTree(val, pRoot->left);
 }
 
-Tree * SearchTree::Search(Tree * tr, int key) {
-    if (tr == nullptr)
+Tree * SearchTree::Search(Tree * pRoot, int key) {
+    if (pRoot == nullptr)
         return nullptr;
-    if (tr->value == key)
-        return tr;
-    if (key < tr->value)
-        return Search(tr->left, key);
+    if (pRoot->value == key)
+        return pRoot;
+    if (key < pRoot->value)
+        return Search(pRoot->left, key);
     else
-        return Search(tr->right, key);
+        return Search(pRoot->right, key);
 }
 
-void SearchTree::DestroyBTree(Tree * tr) {
-    if (tr != nullptr) {
-        DestroyBTree(tr->left);
-        DestroyBTree(tr->right);
-        delete(tr);
+void SearchTree::DestroyBTree(Tree * pRoot) {
+    if (pRoot != nullptr) {
+        DestroyBTree(pRoot->left);
+        DestroyBTree(pRoot->right);
+        delete(pRoot);
     }
 }
