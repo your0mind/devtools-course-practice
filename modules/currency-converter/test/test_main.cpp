a10b192788b6f1b5a67800956a6bb9770af1f708
KST <tarakan.php@yandex.ru>
2018-05-17 22:53:34 +0300

    Revert "Merge branch 'TarakanovK-lab2' of https://github.com/KSTconsult/devtools-course-practice into TarakanovK-lab2"
    This reverts commit f67be236b7a4e8b0350c027046d71eb161d0aab7, reversing
    changes made to cdeb16d5fec84f962ddc0c9045c083d9a88acb76.

--- modules/currency-converter/test/test_main.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2018 Grishin Anton
-
-#include <gtest/gtest.h>
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
--- modules/queue/src/queue.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright 2018 Roman Kudalin
-
-// Methods implementation is located in header file
-// (include/queue.h) because of using templates
-
