38b2f29c38c31c7fe3917919ff5be1ae81498764
Вьюнов Роман <vyunov.rom@mail.ru>
2018-05-18 22:15:21 +0300

    Fixes added in test(code style)

--- modules/DebtServiceCosts/test/test_Vyunov_Roman_DebtService.cpp
+++ modules/DebtServiceCosts/test/test_Vyunov_Roman_DebtService.cpp
@@ -5,38 +5,25 @@
 #include "../modules/DebtServiceCosts/include/DebtService.h"
 
 TEST(Vyunov_Roman_DebtService_test, Can_Throw_Exception_Incorrect_Amount) {
-    // Arrange
-
-    // Act
-
-    // Assert
+    // Act & Assert
     ASSERT_ANY_THROW(new DebtService(-1.0, 2.0, 3.0, ONE_PAYMENT_AT_THE_END));
 }
 
 TEST(Vyunov_Roman_DebtService_test, Can_Throw_Exception_Incorrect_Term) {
-    // Arrange
-
-    // Act
-
-    // Assert
+    // Act & Assert
     ASSERT_ANY_THROW(new DebtService(1.0, -2.0, 3.0, ONE_PAYMENT_AT_THE_END));
 }
 
 TEST(Vyunov_Roman_DebtService_test, Can_Throw_Exception_Incorrect_Rate) {
-    // Arrange
-
-    // Act
-
-    // Assert
+    // Act & Assert
     ASSERT_ANY_THROW(new DebtService(1.0, 2.0, -3.0, ONE_PAYMENT_AT_THE_END));
 }
 
 TEST(Vyunov_Roman_DebtService_test, Copy_constructor_work_correctly) {
     // Arrange
     DebtService ds1(1, 1, 1, ONE_PAYMENT_AT_THE_END);
-    DebtService ds2(ds1);
     // Act
-
+    DebtService ds2(ds1);
     // Assert
     ASSERT_EQ(ds1, ds2);
 }
@@ -55,18 +42,14 @@ TEST(Vyunov_Roman_DebtService_test, Equal_operator_return_correct_value) {
     // Arrange
     DebtService ds1(1, 1, 1, ONE_PAYMENT_AT_THE_END);
     DebtService ds2(1, 2, 3, EQUAL_ANNUAL_PAYMENTS);
-    // Act
-
-    // Assert
+    // Act & Assert
     ASSERT_EQ(&ds1, &(ds1 = ds2));
 }
 
 TEST(Vyunov_Roman_DebtService_test, GetTotalPayout_for_ONE_PAYMENT_AT_THE_END) {
     // Arrange
     DebtService ds1(1, 1, 1, ONE_PAYMENT_AT_THE_END);
-    // Act
-
-    // Assert
+    // Act & Assert
     ASSERT_EQ(ds1.GetTotalPayout(), 2);
 }
 
@@ -74,18 +57,14 @@ TEST(Vyunov_Roman_DebtService_test,
      GetTotalPayout_for_PRINCIPAL_DEBT_ONE_PAYMENT_AT_THE_END) {
     // Arrange
     DebtService ds1(1, 1, 1, PRINCIPAL_DEBT_ONE_PAYMENT_AT_THE_END);
-    // Act
-
-    // Assert
+    // Act & Assert
     ASSERT_EQ(ds1.GetTotalPayout(), 2);
 }
 
 TEST(Vyunov_Roman_DebtService_test, GetTotalPayout_for_EQUAL_ANNUAL_PAYMENTS) {
     // Arrange
     DebtService ds1(1, 1, 1, EQUAL_ANNUAL_PAYMENTS);
-    // Act
-
-    // Assert
+    // Act & Assert
     ASSERT_EQ(ds1.GetTotalPayout(), 2);
 }
 
@@ -102,9 +81,7 @@ TEST(Vyunov_Roman_DebtService_test,
      MakingPayment_throw_exception_for_ONE_PAYMENT_AT_THE_END) {
     // Arrange
     DebtService ds1(1, 1, 1, ONE_PAYMENT_AT_THE_END);
-    // Act
-
-    // Assert
+    // Act & Assert
     ASSERT_ANY_THROW(ds1.MakingPayment(1));
 }
 
@@ -132,9 +109,7 @@ TEST(Vyunov_Roman_DebtService_test,
      MakingPayment_throw_exception_for_PRINCIPAL_DEBT_ONE_PAYMENT_AT_THE_END) {
     // Arrange
     DebtService ds1(1, 1, 1, PRINCIPAL_DEBT_ONE_PAYMENT_AT_THE_END);
-    // Act
-
-    // Assert
+    // Act & Assert
     ASSERT_ANY_THROW(ds1.MakingPayment(1));
 }
 
@@ -152,8 +127,6 @@ TEST(Vyunov_Roman_DebtService_test,
      MakingPayment_throw_exception_for_EQUAL_ANNUAL_PAYMENTS) {
     // Arrange
     DebtService ds1(1, 1, 1, EQUAL_ANNUAL_PAYMENTS);
-    // Act
-
-    // Assert
+    // Act & Assert
     ASSERT_ANY_THROW(ds1.MakingPayment(1));
 }
