ce8cfe86b5735e8ccb2f036114ca1af021112170
Dvorjanchikov_Evgeniy <dreamcather1997@yandex.ru>
2018-05-21 17:48:31 +0300

    Fix

--- modules/area_calculation/src/area_calculation.cpp
+++ modules/area_calculation/src/area_calculation.cpp
@@ -1,28 +1,27 @@
 #include <math.h>
 #include "include/area_calculation.h"
 
-AreaCalculation::AreaCalculation(int _size, double * x_mas, double * y_mas){
+AreaCalculation::AreaCalculation(int _size, double * x_mas, double * y_mas) {
     size = _size;
     area = 0;
     x_coordinate = new double[size];
     y_coordinate = new double[size];
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         x_coordinate[i] = x_mas[i];
         y_coordinate[i] = y_mas[i];
     }
 
 }
 
-double AreaCalculation::GetArea(){
+double AreaCalculation::GetArea() {
     if(area > 0 )
     return area;
-    else
-    {
+    else {
         Transposition();
         double up_area =0;
         double down_area = 0;
         int cur_step = first;
-        while (cur_step != last){
+        while (cur_step != last) {
             int tmp = cur_step;
             if (cur_step != size-1)
                 cur_step++;
@@ -31,7 +30,7 @@ double AreaCalculation::GetArea(){
             up_area += AreaTrapetion(x_coordinate[tmp], x_coordinate[cur_step], y_coordinate[tmp], y_coordinate[cur_step]);
         }
         cur_step = last;
-        while (cur_step != first){
+        while (cur_step != first) {
             int tmp = cur_step;
             if (cur_step != size-1)
                 cur_step++;
@@ -44,29 +43,28 @@ double AreaCalculation::GetArea(){
     }
 }
 
-double * AreaCalculation::GetX(){
+double * AreaCalculation::GetX() {
     return x_coordinate;
 }
 
-double * AreaCalculation::GetY(){
+double * AreaCalculation::GetY() {
     return y_coordinate;
 }
 
-int AreaCalculation::GetSize(){
+int AreaCalculation::GetSize() {
     return size;
 }
 
-AreaCalculation::~AreaCalculation(){
+AreaCalculation::~AreaCalculation() {
     delete(x_coordinate);
     delete(y_coordinate);
 }
 
-int AreaCalculation::FindStart(){
+int AreaCalculation::FindStart() {
     int start = 0;
     int min = x_coordinate[0];
-    for (int i = 1; i < size; i++){
-        if (x_coordinate[i] < min)
-        {
+    for (int i = 1; i < size; i++) {
+        if (x_coordinate[i] < min) {
             start = i;
             min = x_coordinate[i];
         }
@@ -74,12 +72,12 @@ int AreaCalculation::FindStart(){
     return start;
 }
 
-int AreaCalculation::FindEnd(){
+int AreaCalculation::FindEnd() {
     int end = 0;
     int max = x_coordinate[0];
-    for (int i = 1; i < size; i++){
+    for (int i = 1; i < size; i++) {
         if (x_coordinate[i] > max)
-        {
+         {
             end = i;
             max = x_coordinate[i];
         }
@@ -87,32 +85,32 @@ int AreaCalculation::FindEnd(){
     return end;
 }
 
-void AreaCalculation::Transposition(){
+void AreaCalculation::Transposition() {
     first = FindStart();
     last = FindEnd();
     double x_shift;
-    if (x_coordinate[first] < 0){
+    if (x_coordinate[first] < 0) {
         x_shift = fabs(x_coordinate[first]);
-        for (int i = 0;i < size;i++){
+        for (int i = 0;i < size;i++) {
             x_coordinate[i] += x_shift;
         }
     }
     double y_min = y_coordinate[0];
-    for (int i = 1; i < size; i++){
-        if (y_coordinate[i] < y_min){
+    for (int i = 1; i < size; i++) {
+        if (y_coordinate[i] < y_min) {
             y_min = y_coordinate[i];
         }
     }
     double y_shift;
-    if (y_min < 0){
+    if (y_min < 0) {
         y_shift = fabs(y_min);
-        for (int i = 0;i < size;i++){
+        for (int i = 0;i < size;i++) {
             y_coordinate[i] += y_shift;
         }
     }
 
 }
 
-double AreaCalculation::AreaTrapetion(double _first_x, double _second_x, double _first_y, double _second_y){
+double AreaCalculation::AreaTrapetion(double _first_x, double _second_x, double _first_y, double _second_y) {
     return (_first_y + _second_y)*(fabs(_first_x - _second_x)/2);
 }
