cb55738caae0c1042c73606304b49c289401fe3e
Dvorjanchikov_Evgeniy <dreamcather1997@yandex.ru>
2018-05-22 21:48:37 +0300

    Fix

--- modules/area_calculation/test/test_dvorjanchikov_evgeniy_area_calculation.cpp
+++ modules/area_calculation/test/test_dvorjanchikov_evgeniy_area_calculation.cpp
@@ -80,6 +80,20 @@ TEST(Dvorjanchikov_Evgeniy_Area_Calculation_Test, Area_test_2) {
     // Assert
     EXPECT_EQ(res, A.GetArea());
 }
+TEST(Dvorjanchikov_Evgeniy_Area_Calculation_Test, Area_test_3) {
+    // Arrange
+    int _size = 8;
+    double x_mas[8]{ -1, -1, -2, -2, -3, -3, -4, -4};
+    double y_mas[8]{ -3, -2, -2, -1, -1, -2, -2, -3};
+    double res = 4;
+
+    // Act
+    AreaCalculation A(_size, x_mas, y_mas);
+    A.GetArea();
+
+    // Assert
+    EXPECT_EQ(res, A.GetArea());
+}
 TEST(Dvorjanchikov_Evgeniy_Area_Calculation_Test, Zero_area_test_1) {
     // Arrange
     int _size = 4;
