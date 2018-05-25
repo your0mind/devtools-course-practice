8fdebf49fe1bc74a17c42f64eb37bd5e27182725
Usova Marina A <oppabang@mail.ru>
2018-05-25 17:54:58 +0300

    add gtests

--- modules/bignum-arithmetic/test/test_big_number.cpp
+++ modules/bignum-arithmetic/test/test_big_number.cpp
@@ -72,7 +72,7 @@ TEST_F(BigNumberTest, Can_Add_Big_Numbers) {
     EXPECT_EQ(expected_z, z);
 }
 
-TEST_F(BigNumberTest, Can_Add_Big_Numbers_With_Different_Signs) {
+TEST_F(BigNumberTest, Can_Add_Big_Numbers_With_Different_Signs_1) {
     // Arrange
     BigNumber z1("-23345678909876543578876543");
     BigNumber z2("56787654345678909876543");
@@ -83,6 +83,32 @@ TEST_F(BigNumberTest, Can_Add_Big_Numbers_With_Different_Signs) {
     // Assert
     BigNumber expected_z("-23288891255530864669000000");
     EXPECT_EQ(expected_z, z);
+}
+
+TEST_F(BigNumberTest, Can_Add_Big_Numbers_With_Different_Signs_2) {
+  // Arrange
+  BigNumber z1("56787654345678909876543");
+  BigNumber z2("-23345678909876543578876543");
+
+  // Act
+  BigNumber z = z1 + z2;
+
+  // Assert
+  BigNumber expected_z("-23288891255530864669000000");
+  EXPECT_EQ(expected_z, z);
+}
+
+TEST_F(BigNumberTest, Can_Add_Big_Negative_Numbers) {
+  // Arrange
+  BigNumber z1("-46782382032574853849803565");
+  BigNumber z2("-257485384980356552284513453423525");
+
+  // Act
+  BigNumber z = z1 + z2;
+
+  // Assert
+  BigNumber expected_z("-257485431762738584859367303227090");
+  EXPECT_EQ(expected_z, z);
 }
 
 TEST_F(BigNumberTest, Can_Difference_Big_Numbers) {
@@ -98,7 +124,7 @@ TEST_F(BigNumberTest, Can_Difference_Big_Numbers) {
     EXPECT_EQ(expected_z, z);
 }
 
-TEST_F(BigNumberTest, Can_Difference_Big_Numbers_With_Different_Signs) {
+TEST_F(BigNumberTest, Can_Difference_Big_Numbers_With_Different_Signs_1) {
     // Arrange
     BigNumber z1("-5247952962546546524265");
     BigNumber z2("9856528520685476802455354435464");
@@ -111,17 +137,82 @@ TEST_F(BigNumberTest, Can_Difference_Big_Numbers_With_Different_Signs) {
     EXPECT_EQ(expected_z, z);
 }
 
+TEST_F(BigNumberTest, Can_Difference_Big_Numbers_With_Different_Signs_2) {
+  // Arrange
+  BigNumber z1("9856528520685476802455354435464");
+  BigNumber z2("-5247952962546546524265");
+
+  // Act
+  BigNumber z = z1 - z2;
+
+  // Assert
+  BigNumber expected_z("9856528525933429765001900959729");
+  EXPECT_EQ(expected_z, z);
+}
+
+TEST_F(BigNumberTest, Can_Difference_Big_Negative_Numbers) {
+  // Arrange
+  BigNumber z1("-4563746954394453687695");
+  BigNumber z2("-9856528520685476802455354435464");
+
+  // Act
+  BigNumber z = z1 - z2;
+
+  // Assert
+  BigNumber expected_z("9856528516121729848060900747769");
+  EXPECT_EQ(expected_z, z);
+}
+
 TEST_F(BigNumberTest, Can_Multiplication_Big_Numbers) {
     // Arrange
-    BigNumber z1("-9856528516121729848060900747");
+    BigNumber z1("9856528516121729848060900747");
     BigNumber z2("575398440289675084055");
 
     // Act
     BigNumber z = z1 * z2;
 
     // Assert
-    BigNumber expected_z("-5671431134847148931073745831167678482646237289085");
+    BigNumber expected_z("5671431134847148931073745831167678482646237289085");
     EXPECT_EQ(expected_z, z);
+}
+
+TEST_F(BigNumberTest, Can_Multiplication_Big_Numbers_With_Different_Signs_1) {
+  // Arrange
+  BigNumber z1("-9856528516121729848060900747");
+  BigNumber z2("575398440289675084055");
+
+  // Act
+  BigNumber z = z1 * z2;
+
+  // Assert
+  BigNumber expected_z("-5671431134847148931073745831167678482646237289085");
+  EXPECT_EQ(expected_z, z);
+}
+
+TEST_F(BigNumberTest, Can_Multiplication_Big_Numbers_With_Different_Signs_2) {
+  // Arrange
+  BigNumber z1("575398440289675084055");
+  BigNumber z2("-9856528516121729848060900747");
+
+  // Act
+  BigNumber z = z1 * z2;
+
+  // Assert
+  BigNumber expected_z("-5671431134847148931073745831167678482646237289085");
+  EXPECT_EQ(expected_z, z);
+}
+
+TEST_F(BigNumberTest, Can_Multiplication_Big_Negative_Numbers) {
+  // Arrange
+  BigNumber z1("-9856528516121729848060900747");
+  BigNumber z2("-575398440289675084055");
+
+  // Act
+  BigNumber z = z1 * z2;
+
+  // Assert
+  BigNumber expected_z("5671431134847148931073745831167678482646237289085");
+  EXPECT_EQ(expected_z, z);
 }
 
 TEST_F(BigNumberTest, Can_Division_Big_Numbers) {
@@ -138,16 +229,58 @@ TEST_F(BigNumberTest, Can_Division_Big_Numbers) {
     EXPECT_EQ(z, expected_z);
 }
 
+TEST_F(BigNumberTest, Can_Division_Big_Numbers_With_Different_Signs_1) {
+  // Arrange
+  BigNumber z1("-9469812971537775525694720659749323356437443739413630");
+  BigNumber z2("2175639854932426546456455546245");
+
+  // Act
+  BigNumber z = z1 / z2;
+
+  // Assert
+  BigNumber expected_z("-4352656507035673676774");
+  std::string res = expected_z.getResultInString();
+  EXPECT_EQ(z, expected_z);
+}
+
+TEST_F(BigNumberTest, Can_Division_Big_Numbers_With_Different_Signs_2) {
+  // Arrange
+  BigNumber z1("9469812971537775525694720659749323356437443739413630");
+  BigNumber z2("-2175639854932426546456455546245");
+
+  // Act
+  BigNumber z = z1 / z2;
+
+  // Assert
+  BigNumber expected_z("-4352656507035673676774");
+  std::string res = expected_z.getResultInString();
+  EXPECT_EQ(z, expected_z);
+}
+
+TEST_F(BigNumberTest, Can_Division_Big_Negative_Numbers) {
+  // Arrange
+  BigNumber z1("-9469812971537775525694720659749323356437443739413630");
+  BigNumber z2("-2175639854932426546456455546245");
+
+  // Act
+  BigNumber z = z1 / z2;
+
+  // Assert
+  BigNumber expected_z("4352656507035673676774");
+  std::string res = expected_z.getResultInString();
+  EXPECT_EQ(z, expected_z);
+}
+
 TEST_F(BigNumberTest, Can_Division_Big_Numbers_With_Rounded) {
     // Arrange
-    BigNumber z1("9469812971537775525696306765");
-    BigNumber z2("78987654323456567654");
+    BigNumber z1("78987654323456567654");
+    BigNumber z2("9469812971537775525696306765");
 
     // Act
     BigNumber z = z1 / z2;
 
     // Assert
-    BigNumber expected_z("119889785");
+    BigNumber expected_z("0");
     EXPECT_EQ(z, expected_z);
 }
 
@@ -206,6 +339,25 @@ TEST_F(BigNumberTest, Can_Comparison_Numbers) {
     // Act & Assert
     EXPECT_TRUE(z2 > z1);
 }
+
+TEST_F(BigNumberTest, Can_Comparison_Numbers_With_Different_Signs_1) {
+  // Arrange
+  BigNumber z1("2175639854935");
+  BigNumber z2("-2175639854932426546456455546244");
+
+  // Act & Assert
+  EXPECT_FALSE(z2 > z1);
+}
+
+TEST_F(BigNumberTest, Can_Comparison_Numbers_With_Different_Signs_2) {
+  // Arrange
+  BigNumber z1("-2454356352251244");
+  BigNumber z2("217563985676544935");
+
+  // Act & Assert
+  EXPECT_TRUE(z2 > z1);
+}
+
 
 TEST_F(BigNumberTest, Can_Return_Result_In_String) {
     // Arrange
