c95c3056d64c9fb7f8f7f1535e6444e2b4f552ab
Azarius <osh1308@gmail.com>
2018-05-26 16:46:42 +0300

    fix typos

--- modules/dheap/include/dheap.h
+++ modules/dheap/include/dheap.h
@@ -1,16 +1,19 @@
 // Copyright 2018 Lipatov Igor
 
 #include <algorithm>
+#include <vector>
+#include <utility>
 
-#ifndef DEVTOOLS_DHEAP_H
-#define DEVTOOLS_DHEAP_H
+#ifndef MODULES_DHEAP_INCLUDE_DHEAP_H_
+#define MODULES_DHEAP_INCLUDE_DHEAP_H_
 
-#endif //DEVTOOLS_DHEAP_H
+#endif  // MODULES_DHEAP_INCLUDE_DHEAP_H_
 
 typedef std::pair<int, int> Vertex;
 
 class Dheap {
 public:
+
     Dheap(int d, int elemCount);
     bool isEmpty();
     Vertex pop();
@@ -18,10 +21,11 @@ public:
     void decreaseKey(int vNum, int lVal);
     int getVal(int vNum);
 private:
+
     int elemCount;
     int d;
-    std::vector<int> storage;  //vertex number
-    std::vector<int> points; //(length,pos) [2 * vNum] indexing
+    std::vector<int> storage;  //  vertex number
+    std::vector<int> points; //  (length,pos) [2 * vNum] indexing
     int key(int i);
     void swap(int iFir, int iSec);
     int up(int i);
