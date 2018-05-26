0532f28aff1aca1ac5626442750dec589a7c47fc
Azarius <osh1308@gmail.com>
2018-05-26 17:04:20 +0300

    fix more typos

--- modules/dheap/test/test_lipatov_igor_dheap.cpp
+++ modules/dheap/test/test_lipatov_igor_dheap.cpp
@@ -1,9 +1,11 @@
+// Copyright 2018 Lipatov Igor
+
 #include <gtest/gtest.h>
 #include "include/dheap.h"
 
 TEST(Lipatov_Igor_DHeapTest, CanConstruct) {
     // Arrange
-    Dheap dh(3,10);
+    Dheap dh(3, 10);
     // Act
     Vertex tmp;
     for (int i = 0; i < 10; i++) {
@@ -12,13 +14,13 @@ TEST(Lipatov_Igor_DHeapTest, CanConstruct) {
         dh.push(tmp);
     }
     // Assert
-    EXPECT_EQ(dh.getVal(9),81);
+    EXPECT_EQ(dh.getVal(9), 81);
 }
 
 TEST(Lipatov_Igor_DHeapTest, CantUseEmpty) {
     // Arrange
-    Dheap dh(3,10);
+    Dheap dh(3, 10);
     // Act
     // Assert
-    EXPECT_EQ(dh.pop(),Vertex(-1,-1));
+    EXPECT_EQ(dh.pop(),Vertex(-1, -1));
 }
