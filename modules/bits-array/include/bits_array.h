e5cd3bc1cec93957a7a5cd1d92e630bd333b4b27
Gachiman <aglikov@bk.ru>
2018-05-26 15:33:31 +0300

    v1.3.1

--- modules/bits-array/include/bits_array.h
+++ modules/bits-array/include/bits_array.h
@@ -29,8 +29,8 @@ class BitsArray {
     int  MemLen;  // к-во эл-тов Мем для представления бит.поля
 
                  // методы реализации
-    int   GetMemIndex(const int n);  // индекс в pМем для бита n
-    unsigned int GetMemMask(const int n);  // битовая маска для бита n
+    int   GetMemIndex(const int n) const;  // индекс в pМем для бита n
+    unsigned int GetMemMask(const int n) const;  // битовая маска для бита n
 };
 
 #endif  // MODULES_BITS_ARRAY_INCLUDE_BITS_ARRAY_H_
