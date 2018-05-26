bb24bf9d88be143fa2e6fc75e19b90b2fef0ab6f
Azarius <osh1308@gmail.com>
2018-05-26 20:42:47 +0300

    Add more tests.

--- modules/dheap/test/test_lipatov_igor_dheap.cpp
+++ modules/dheap/test/test_lipatov_igor_dheap.cpp
@@ -24,3 +24,34 @@ TEST(Lipatov_Igor_DHeapTest, CantUseEmpty) {
     // Assert
     EXPECT_EQ(dh.pop(), Vertex(-1, -1));
 }
+
+TEST(Lipatov_Igor_DHeapTest, CanGetMin) {
+    // Arrange
+    Dheap dh(3, 10);
+    // Act
+    Vertex tmp;
+    for (int i = 0; i < 10; i++) {
+        tmp.first = i;
+        tmp.second = i * i;
+        dh.push(tmp);
+    }
+    // Assert
+    EXPECT_EQ(dh.pop(), Vertex(0, 0));
+}
+
+TEST(Lipatov_Igor_DHeapTest, NoDBugs) {
+    // Arrange
+    Dheap dh1(3, 10);
+    Dheap dh2(4, 10);
+
+    // Act
+    Vertex tmp;
+    for (int i = 0; i < 10; i++) {
+        tmp.first = i;
+        tmp.second = i * i;
+        dh1.push(tmp);
+        dh2.push(tmp);
+    }
+    // Assert
+    EXPECT_EQ(dh1.pop(), dh2.pop());
+}
