e9b6c76beec1beaefaefb6fb01dddff658320731
Denis Kiselev <denis.kiselev.1997@gmail.com>
2018-05-30 01:50:04 +0400

    Lab3 done

---
+++ modules/bits-array/src/bits_calculator.cpp
@@ -0,0 +1,116 @@
+// Copyright 2018 Kiselev Denis
+
+#include <sstream>
+
+#include "include/bits_calculator.h"
+
+BitsCalculator::BitsCalculator() : message_("") {}
+
+void BitsCalculator::help(const char* appname, const char* message) {
+    message_ =
+        std::string(message) +
+          "This is a bits calculator application.\n\n" +
+          "Please provide arguments in the following format:\n\n"+
+
+          "  $ " + appname + " <bits1> <bits2> <operation>\n\n" +
+
+          "Where all arguments are bit sequences, " +
+          "and <operation> is one of '&', '|'.\n";
+}
+
+bool BitsCalculator::validateNumberOfArguments(int argc, const char** argv) {
+    if (argc == 1) {
+        help(argv[0]);
+        return false;
+    }
+    else if (argc != 4) {
+        help(argv[0], "ERROR: Should be 3 arguments.\n\n");
+        return false;
+    }
+    return true;
+}
+
+BitsArray* parseBits(const char* arg) {
+    int size = 0;
+
+    while (arg[size++] != '\0') {}
+
+    BitsArray* array = new BitsArray(--size);
+    for (--size; size >= 0; --size) {
+        switch (arg[size]) {
+            case '0' :
+                break;
+                
+            case '1' :
+                array->SetBit(size);
+                break;
+
+            default :
+                throw std::string("Wrong bits format!");
+        }
+    }
+
+    return array;
+}
+
+char parseOperation(const char* arg) {
+    char op;
+    
+    if (arg[1] != '\0') {
+        throw std::string("Wrong operation format!");
+    }
+    else {
+        switch (arg[0]) {
+            case '&' :
+                op = '&';
+                break;
+
+            case '|' :
+                op = '|';
+                break;
+
+            default :
+                throw std::string("Wrong operation format!");
+        }
+    }
+
+    return op;
+}
+
+std::string BitsCalculator::operator()(int argc, const char** argv) {
+    Arguments args;
+
+    if (!validateNumberOfArguments(argc, argv)) {
+        return message_;
+    }
+    try {
+        args.bits_first     = parseBits(argv[1]);
+        args.bits_second    = parseBits(argv[2]);
+        args.operation      = parseOperation(argv[3]);
+    }
+    catch(std::string& str) {
+        return str;
+    }
+
+    BitsArray result(1);
+    std::ostringstream stream;
+    switch (args.operation) {
+        case '&':
+            result = BitsArray(*args.bits_first & *args.bits_second);
+            break;
+        case '|' :
+            result = BitsArray(*args.bits_first | *args.bits_second);
+            break;
+    }
+
+    stream << "Result: ";
+    int size = result.GetLength();
+    for (int i = 0; i < size; ++i) {
+        stream << result.GetBit(i);
+    }
+    stream << std::endl;
+
+    message_ = stream.str();
+
+    return message_;
+}
---
+++ modules/bits-array/test/test_bits_calculator.cpp
@@ -0,0 +1,90 @@
+// Copyright 2018 Kiselev Denis
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+
+#include "include/bits_calculator.h"
+
+using ::testing::internal::RE;
+using std::vector;
+using std::string;
+
+class BitsCalculatorTest : public ::testing::Test {
+ protected:
+    // virtual void SetUp() {}
+
+    void Act(vector<string> args_) {
+        vector<const char*> options;
+
+        options.push_back("appname");
+        for (size_t i = 0; i < args_.size(); ++i) {
+            options.push_back(args_[i].c_str());
+        }
+
+        const char** argv = &options.front();
+        int argc = static_cast<int>(args_.size()) + 1;
+
+        output_ = app_(argc, argv);
+    }
+
+    void Assert(std::string expected) {
+        EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
+    }
+
+ private:
+    BitsCalculator app_;
+    string output_;
+};
+
+TEST_F(BitsCalculatorTest, Do_Print_Help_Without_Arguments) {
+    vector<string> args = {};
+
+    Act(args);
+
+    Assert("This is a bits calculator application\\..*");
+}
+
+TEST_F(BitsCalculatorTest, Is_Checking_Number_Of_Arguments) {
+    vector<string> args = {"1", "2"};
+
+    Act(args);
+
+    Assert("ERROR: Should be 3 arguments\\..*");
+}
+
+TEST_F(BitsCalculatorTest, Can_Detect_Wrong_Bits_Format) {
+    vector<string> args = {"0100", "0112", "&"};
+
+    Act(args);
+
+    Assert("Wrong bits format!");
+}
+
+TEST_F(BitsCalculatorTest, Can_Detect_Wrong_Operation_Format) {
+    vector<string> args = {"0100", "0110", "+"};
+
+    Act(args);
+
+    Assert("Wrong operation format!");
+}
+
+TEST_F(BitsCalculatorTest, And_Operation_Is_Correct) {
+    vector<string> args = {"01001", "0110", "&"};
+
+    Act(args);
+
+    Assert("Result: 01000");
+}
+
+TEST_F(BitsCalculatorTest, Or_Operation_Is_Correct) {
+    vector<string> args = {"01001", "0110", "|"};
+
+    Act(args);
+
+    Assert("Result: 01101");
+}
